4c4c80e07f95c05c66a187d22b5b3463cb72b425
Jossy Solá <jossysola@gmail.com>
2024-10-06 01:13:25 -0600

    Works on custom hook and makes first implementation to test on React component

--- app/hooks/custom.tsx
+++ app/hooks/custom.tsx
@@ -1,21 +1,57 @@
 import { useState } from "react";
 
-export function useCreate () {
+export function useCreateTable () {
     const [ numColumns, setNumColumns ] = useState<number>(0);
     const [ numRows, setNumRows ] = useState<number>(0);
     const [ columns, setColumns ] = useState<Array<Array<React.JSX.Element>>>([]);
     const [ rows, setRows ] = useState<Array<Array<React.JSX.Element>>>([]);
 
     const addColumn = () => {
-        // If no cell, create row with one cell
-        // If one row, add column to existing row
-        // If 1+ rows, add columns for each existing row
+        // If empty, create row with one cell
+        if (numColumns === 0 && numRows === 0) {
+            setRows([
+                [
+                    <thead>
+                        <tr>
+                            <th scope="col"><input type="text" name={`${numRows}-${numColumns}`}/></th>
+                        </tr>
+                    </thead>
+                ]
+            ])
+            setNumColumns(1);
+            setNumRows(1);
+            return;
+        }
+        // If not empty, add column to each existing row
+
     }
 
     const addRow = () => {
-        // If no cell, create row with one cell
-        // If one row, create another row below
-        // If 1+ columns, create row with X number of columns
+        // If empty, create row with one cell
+        if (numColumns === 0 && numRows === 0) {
+            setRows([
+                [
+                    <thead>
+                        <tr>
+                            <th scope="col"><input type="text" name={`${numRows}-${numColumns}`}/></th>
+                        </tr>
+                    </thead>
+                ]
+            ])
+            setNumColumns(1);
+            setNumRows(1);
+            return;
+        }
+        // If not empty, create row with X number of columns
+
     }
-    
+
+    const deleteColumn = (index: number) => {
+
+    }
+
+    const deleteRow = (index: number) => {
+
+    }
+    return {rows, addColumn, addRow}
 }
--- app/ui/organisms/new_dyn_table.tsx
+++ app/ui/organisms/new_dyn_table.tsx
@@ -1,14 +1,19 @@
 'use client'
+import { useCreateTable } from "@/app/hooks/custom"
 
 export default function NewDynamicTable(): React.JSX.Element {
+    const { rows, addColumn, addRow } = useCreateTable();
 
     return (
         <>
             <form>
                 <input type="text" name="name" id="name" placeholder="Untitled table" defaultValue="Untitled table"/>
                 <table>
+                    {rows}
                 </table>
             </form>
+            
+            <button type="button" onClick={(e) => addColumn()}>Add Column</button>
         </>
     )
 }
