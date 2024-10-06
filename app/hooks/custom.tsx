5837a407378fc7d5463cd2356b6e15f471278149
Jossy Solá <jossysola@gmail.com>
2024-10-06 00:34:58 -0600

    Adds comments for steps to code

--- app/hooks/custom.tsx
+++ app/hooks/custom.tsx
@@ -7,11 +7,15 @@ export function useCreate () {
     const [ rows, setRows ] = useState<Array<Array<React.JSX.Element>>>([]);
 
     const addColumn = () => {
-
+        // If no cell, create row with one cell
+        // If one row, add column to existing row
+        // If 1+ rows, add columns for each existing row
     }
 
     const addRow = () => {
-        
+        // If no cell, create row with one cell
+        // If one row, create another row below
+        // If 1+ columns, create row with X number of columns
     }
     
 }
