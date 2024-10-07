3c8a975bcc487ffaf1640c74fa5f863c47f51fca
Jossy Solá <jossysola@gmail.com>
2024-10-06 23:21:47 -0600

    Adds comments and sets the -ref- hooks for the DOM nodes to be manipulated

--- app/hooks/custom.tsx
+++ app/hooks/custom.tsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { v4 as uuidv4 } from 'uuid'
 
 export function useCreateTable () {
     const [ numColumns, setNumColumns ] = useState<number>(0);
     const [ numRows, setNumRows ] = useState<number>(0);
-    const [ columns, setColumns ] = useState<Array<Array<React.JSX.Element>>>([]);
-    const [ rows, setRows ] = useState<Array<Array<React.JSX.Element>>>([]);
+    const [ columns, setColumns ] = useState<Array<React.JSX.Element>>([]);
+    const [ rows, setRows ] = useState<Array<React.JSX.Element>>([]);
+    // Reference to the <thead> targeting columns. Only one element available as there is only one row of columns
+    const columnsRef = useRef<(HTMLTableSectionElement | null)>();
+    // Reference to each <thead> targeting a row. Array of nodes as there can be multiple rows.
+    const rowsRef = useRef<(HTMLTableSectionElement | null)[]>([]);
 
     const addColumn = () => {
         // If empty, create row with one cell
         if (numColumns === 0 && numRows === 0) {
             setRows([
-                [
-                    <thead>
-                        <tr>
-                            <th scope="col"><input type="text" name={`${numRows}-${numColumns}`}/></th>
-                        </tr>
-                    </thead>
-                ]
+                <thead 
+                ref={(element) => {
+                    columnsRef.current = element;
+                }}
+                key={uuidv4()}>
+                    <tr>
+                        <th scope="col"><input type="text" name={`${numRows}-${numColumns}`}/></th>
+                    </tr>
+                </thead>
             ])
             setNumColumns(1);
             setNumRows(1);
@@ -30,13 +37,15 @@ export function useCreateTable () {
         // If empty, create row with one cell
         if (numColumns === 0 && numRows === 0) {
             setRows([
-                [
-                    <thead>
-                        <tr>
-                            <th scope="col"><input type="text" name={`${numRows}-${numColumns}`}/></th>
-                        </tr>
-                    </thead>
-                ]
+                <thead
+                ref={(element) => {
+                    rowsRef.current[0] = element;
+                }}
+                key={uuidv4()}>
+                    <tr>
+                        <th scope="col"><input type="text" name={`${numRows}-${numColumns}`}/></th>
+                    </tr>
+                </thead>
             ])
             setNumColumns(1);
             setNumRows(1);
