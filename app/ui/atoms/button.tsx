bd1d7c1f10c1d494061eed1c1a69d869cfbbbc6d
Jossy Solá <jossysola@gmail.com>
2024-10-07 01:06:05 -0600

    Populates SubmitButton component

--- app/ui/atoms/button.tsx
+++ app/ui/atoms/button.tsx
@@ -11,6 +11,29 @@ export function ActionButton({callback, text}: {
     )
 }
 
-export function SubmitButton() {
-
+export function SubmitButton({name, value, text, disabled, form, formaction, formenctype, formmethod, formnovalidate, formtarget}: {
+    name: string,
+    value: string,
+    text: string,
+    disabled?: boolean,
+    form?: string,
+    formaction?: string,
+    formenctype?: "application/x-www-form-urlencoded" | "multipart/form-data" | "text/plain",
+    formmethod?: "post" | "get" | "dialog",
+    formnovalidate?: boolean,
+    formtarget?: "_self" | "_blank" | "_parent" | "_top",
+}) {
+    return (
+        <button 
+        name={name}
+        value={value}
+        type="submit"
+        disabled={disabled}
+        form={form}
+        formAction={formaction}
+        formEncType={formenctype}
+        formMethod={formmethod}
+        formNoValidate={formnovalidate}
+        formTarget={formtarget}>{text}</button>
+    )
 }
