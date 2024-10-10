208465f63220228daa2c06f4b494e29257dfa7b3
Jossy Solá <jossysola@gmail.com>
2024-10-09 23:28:11 -0600

    Completes refactoring of custom hook

--- app/hooks/custom.tsx
+++ app/hooks/custom.tsx
@@ -1,46 +1,49 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { v4 as uuidv4 } from 'uuid'
 
 export function useHTMLTable() {
     const [ numColumns, setNumColumns ] = useState<number>(0);
     const [ numRows, setNumRows ] = useState<number>(0);
     const [ columns, setColumns ] = useState<Array<React.JSX.Element>>([]);
-    const [ rows, setRows ] = useState<Array<React.JSX.Element>>([]);
+    const [ rows, setRows ] = useState<Array<Array<React.JSX.Element>>>([]);
 
     const addColumn = (value?: string) => {
         setColumns(prev => {
             return [...prev, <th scope="col" key={uuidv4()}><input type="text" placeholder={value ? value : ''}/></th>]
         })
-
         if (rows) {
-
+            setRows(prev => {
+                return prev.map(row => [
+                    ...row,
+                    <td key={uuidv4()}><input type="text" placeholder={value ? value : ''}/></td>
+                ])
+            })
         }
-
         setNumColumns(prev => prev + 1);
     }
+
     const addRow = (value?: string) => {
         if (numColumns === 0) {
             return addColumn(value);
         }
         setRows(prev => {
-            let elements = [];
-            
-            for (let i = 0; i < numColumns; i++) {
-                if (i === 0) {
-                    elements.push(<th key={uuidv4()} scope="row"><input type="text" placeholder={value ? value : ''}/></th>);
-                    continue;
-                }
-                elements.push(<td key={uuidv4()}><input type="text" placeholder={value ? value : ''}/></td>)
-            }
             return [
                 ...prev,
-                <tr key={uuidv4()}>
-                    {elements}
-                </tr>
+                columns.map((el, index) => {
+                    if (index === 0) {
+                        return <th key={uuidv4()} scope="row"><input type="text" placeholder={value ? value : ''}/></th>
+                    }
+                    return <td key={uuidv4()}><input type="text" placeholder={value ? value : ''}/></td>
+                })
             ]
         })
         setNumRows(prev => prev + 1);
     }
 
-    return {columns, rows, addColumn, addRow}
+    return {
+        columns, 
+        rows, 
+        addColumn, 
+        addRow
+    }
 }
--- app/ui/atoms/table.tsx
+++ app/ui/atoms/table.tsx
@@ -1,7 +1,9 @@
 'use client'
+import { v4 as uuidv4 } from 'uuid'
+
 export default function Table({columns, rows} : {
     columns: Array<React.JSX.Element>,
-    rows: Array<React.JSX.Element>,
+    rows: Array<Array<React.JSX.Element>>,
 }) {
     return (
         <table>
@@ -11,7 +13,9 @@ export default function Table({columns, rows} : {
                 </tr>
             </thead>
             <tbody>
-                {rows}
+                {rows.map((row) => {
+                    return <tr key={uuidv4()}>{row}</tr>
+                })}
             </tbody>
         </table>
     )
