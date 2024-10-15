49374a34082af267a1d675b04e3e8b683887962a
Jossy Solá <jossysola@gmail.com>
2024-10-14 18:49:43 -0600

    Creates an array of mocks for table items

---
+++ app/lib/mock.ts
@@ -0,0 +1,102 @@
+const items = [
+    {
+        name: 'Hugo Huante',
+        disabled: 'true',
+        range: {
+            from: '13:00',
+            to: '22:00',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: 'Jorge Salas',
+        disabled: 'false',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        avoidRows:[],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+    {
+        name: '',
+        disabled: '',
+        range: {
+            from: '',
+            to: '',
+        },
+        avoidColumns: [],
+        avoidRows: [],
+    },
+]
