c60e033b58d13c09cea1e1eee5bc0780cd399ae0
Jossy Solá <jossysola@gmail.com>
2024-10-19 11:13:51 -0600

    Resets item.tsx component. Comments out code on classes.ts. Installs SaSS

--- app/ui/atoms/item.tsx
+++ app/ui/atoms/item.tsx
@@ -2,39 +2,9 @@
 
 import { SubmitButton } from "./button";
 
-export default function Item_Preferences({
-    name = 'Unknown',
-    disabled = false,
-    range = {
-        from: '',
-        to: '',
-    },
-    avoidColumns = [],
-    avoidRows = [],
-}) {
+export default function Item () {
     return (
         <form>
-            <label htmlFor="name">Name:</label>
-            <input type="text" name="name" id="name" defaultValue={name} required/>
-
-            <label><input type="radio" name="disabled" value="false" /> Enable</label>
-            <label><input type="radio" name="disabled" value="true" /> Disable</label>
-
-            <fieldset>
-                <legend>Preferred Range:</legend>
-                <label htmlFor="from">From:</label>
-                <input type="text" name="from" id="from" defaultValue={range.from}/>
-                <label htmlFor="to">To:</label>
-                <input type="text" name="to" id="to" defaultValue={range.to}/>
-            </fieldset>
-
-            <label htmlFor="avoidColumn">Avoid this column:</label>
-            <input type="text" name="avoidColumn" id="avoidColumn"/>
-
-            <label htmlFor="avoidRow">Avoid this row:</label>
-            <input type="text" name="avoidRow" id="avoidRow"/>
-
-            <SubmitButton name="save" value="save" text="Save"/>
         </form>
     )
 }
