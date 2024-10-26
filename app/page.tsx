dc2af7cdb8288303bca337c0d44bd883b3dcd575
Jossy Solá <jossysola@gmail.com>
2024-10-25 23:06:27 -0600

    Adds experimental PPR Partial Prerendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import NewDynamicTable from "./ui/organisms/dynamic-table";
 
+export const experimental_ppr = true;
+
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
