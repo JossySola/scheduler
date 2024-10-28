bad8d406f5e5a654c3af991c3a75145800bdca86
Jossy Solá <jossysola@gmail.com>
2024-10-27 18:12:51 -0600

    Adds <h1> to see changes in route navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import NewDynamicTable from "./ui/organisms/dynamic-table";
 
 export const experimental_ppr = true;
 
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <NewDynamicTable/>
+    <h1>Root Page.tsx</h1>
     </div>
   );
 }
