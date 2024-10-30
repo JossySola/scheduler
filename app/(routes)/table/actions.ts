d608b60d4752ff3e44068ad9b6880f49fa613269
Jossy Solá <jossysola@gmail.com>
2024-10-30 01:14:11 -0600

    Reestructures file structure

---
+++ app/(routes)/table/actions.ts
@@ -0,0 +1,16 @@
+'use server'
+import pool from "@/app/lib/mocks/db"
+
+export async function printAction (formData: FormData) {
+    console.log(formData)
+}
+
+export async function SaveActionPOOL (formData: FormData) {
+
+}
+export async function SaveAction (formData: FormData) {
+
+}
+export async function CancelAction () {
+
+}
--- app/(routes)/table/actions/actions.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-export async function printAction (formData: FormData) {
-    const data = Object.fromEntries(formData.entries());
-
-    console.log(data)
-}
