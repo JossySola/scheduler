60d7ef71f3b74e7031bc148b1095b5999e9ee5bb
Jossy Solá <jossysola@gmail.com>
2024-11-07 00:16:28 -0600

    Refactors FormDataToQuery function. Structures the table id adding the UUID without hyphens + table name + date timestamp. Corrects queries syntax expected by PostgreSQL. Uses Regex to skip formData keys that belongs to the table name and action IDs. Implements two separate functions: createColumnsQuery and createRowsQuery.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,14 +3,17 @@ import { v4 as uuidv4 } from 'uuid';
 
 export function FormDataToQuery (formData: FormData) {
     // Query Object to be returned
+    const name = formData.get('table-name')?.toString().replaceAll(' ', '');
     let result: Result_FormDataToQuery = {
-        id: `_${uuidv4().replace(/-/g, '')}_${formData.get('table-name')}_${Date.now().toString()}`,
+        id: `_${uuidv4().replace(/-/g, '')}_${name}_${Date.now().toString()}`,
         columns: '',
-        columnsParam: '',
+        columnsParams: '',
         rows: [],
     };
     const entries_values = formData.values();
     const entries_keys = formData.keys();
+    const regexCol = /[A-Z][0]/;
+    const regexAction = /[\$](ACTION_)/;
     
     const createColumnsQuery = () => {
         // For each element, if the index is greater than 1 and is less than the number of columns
@@ -18,23 +21,53 @@ export function FormDataToQuery (formData: FormData) {
         let columnArray: Array<FormDataEntryValue> = [];
         let paramsArray: Array<FormDataEntryValue> = [];
         let count = 0;
-        for (const entry of entries_keys) {
-            if (count > 1) {
-                columnArray.push(`${entry} VARCHAR(30)`);
-                paramsArray.push(`${entry}`);
+        let notColumns = 0;
+        for (const key of entries_keys) {
+            if (key.match(regexCol)) {
+                columnArray.push(`"${key}" VARCHAR(30)`);
+                paramsArray.push(`"${key}"`);
+            } else if (key.match(regexAction) || key.match('table-name')) {
+                notColumns++;
             }
             count++;
         }
         if (columnArray && paramsArray) {
             result.columns = columnArray.join(', ');
-            result.columnsParam = paramsArray.join(', ');
+            result.columnsParams = paramsArray.join(', ');
+        }
+        const numOfColumns = paramsArray.length;
+
+        return {
+            columnArray,
+            paramsArray,
+            numOfColumns,
+            notColumns,
         }
     }
-    createColumnsQuery();
+    const { numOfColumns, notColumns } = createColumnsQuery();
 
     const createRowsQuery = () => {
+        let index = 0;
+        let tempArray: Array<string | null> = [];
 
+        for (const value of entries_values) {
+            if (index < notColumns) {
+                index++;
+                continue;
+            }
+            tempArray.push(value ? `${value}` : `${null}`);
+
+            if (tempArray.length === numOfColumns) {
+                result.rows.push([...tempArray])
+                tempArray = [];
+            }
+            index++;
+        }
+
+        if (tempArray.length > 0) {
+            result.rows.push([...tempArray])
+        }
     }
-    
+    createRowsQuery();
     return result;
 }
