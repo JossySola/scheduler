83bd2862b9a2519d58e622c5e94f0bdc75c6b165
Jossy Solá <jossysola@gmail.com>
2024-11-20 00:40:49 -0600

    Creates API directory. Begins with table/[id] route handler

---
+++ app/api/table/[id]/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import pool from "@/app/lib/mocks/db";
+
+export async function GET (
+    request: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const id = (await params).id;
+    const data = await pool.query(`
+        SELECT * FROM ${id}
+        ORDER BY _num;
+    `);
+    return NextResponse.json(data);
+}
