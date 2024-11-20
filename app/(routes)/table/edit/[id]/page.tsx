65b75d1b5a95390d73a8b0e38c1af992ac878846
Jossy Solá <jossysola@gmail.com>
2024-11-20 00:37:28 -0600

    Uses custom hook useTableById. Returns JSX depending on fetch status

--- app/(routes)/table/edit/[id]/page.tsx
+++ app/(routes)/table/edit/[id]/page.tsx
@@ -1,16 +1,20 @@
-export default async function Page ({ params }: {
-    params: { id: string }
+'use client'
+import { useTableById } from "@/app/hooks/custom";
+import TablePanel from "@/app/ui/molecules/mol-table-panel";
+
+export default function Page ({ params }: {
+    params: Promise<{ id: string }>
 }) {
-    const id = (await params).id
-    const res = await fetch(`http://localhost:3000/api/edit?id=${id}`, {
-        method: 'GET',
-        cache: 'no-store',
-    });
-    const data = await res.json();
-    // console.log(data)
+    const { data, error, isLoading } = useTableById({ params });
     // http://localhost:3000/table/edit/_8eea43e99cbd4fafb6ca50bc098f48f0_untitled_1731303485221
+    
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error loading data</p>;
+    
     return (
-        <>
-        </>
+        <form>
+            <input name="table-name" id="table-name" type="text" defaultValue="Untitled" placeholder="Untitled" />
+            <TablePanel rowsProp={data} />
+        </form>
     )
 }
