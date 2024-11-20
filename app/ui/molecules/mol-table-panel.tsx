72064b61aa645eeb9211133160eb0f3ff35e0eca
Jossy Solá <jossysola@gmail.com>
2024-11-20 00:43:56 -0600

    Accepts a prop from page belonging to SWR fetched data. Uses useEffect to display the data

--- app/ui/molecules/mol-table-panel.tsx
+++ app/ui/molecules/mol-table-panel.tsx
@@ -2,15 +2,22 @@
 import { useHTMLTable } from "@/app/hooks/custom";
 import DynamicTable from "./mol-table-dynamic";
 import { ActionButton } from "../atoms/atom-button";
+import { useEffect } from "react";
 
-export default function TablePanel () {
-    const { addRow, addColumn, removeRow, removeColumn, rows, handleInputChange } = useHTMLTable();
-    const onlyRows = rows.toSpliced(0, 1);
-    const onlyColumns = rows.toSpliced(1, rows.length)[0];
+export default function TablePanel ({ rowsProp }: {
+    rowsProp?: Array<Array<{id: string, value: string}>>,
+}) {
+    const { rows, columns, setRows, addRow, addColumn, removeRow, removeColumn, handleInputChange} = useHTMLTable();
+    
+    useEffect(() => {
+        if (rowsProp) {
+            setRows(rowsProp);
+        }
+    }, [rowsProp])
     
     return (
         <>
-            <DynamicTable rows={onlyRows} columns={onlyColumns} handleInputChange={handleInputChange} />
+            <DynamicTable rows={rows} columns={columns} handleInputChange={handleInputChange} />
             <ActionButton callback={addRow} text={"-Add row"}/>
             <ActionButton callback={addColumn} text={"-Add column"}/>
             <ActionButton callback={removeRow} text={"-Remove row"}/>
