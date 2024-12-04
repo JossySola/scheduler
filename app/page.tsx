e21e2f10618f39499ad638539a46f7804e6868a5
Jossy Solá <jossysola@gmail.com>
2024-12-04 10:51:43 -0600

    Wraps the app with <GoogleReCaptchaProvider>

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import TablePanel from "./ui/molecules/mol-table-panel";
 
 export const experimental_ppr = true;
 
-export default function Home() {
+export default function App () {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1>Root Page.tsx</h1>
-      <TablePanel />
-    </div>
+    <GoogleReCaptchaProvider reCaptchaKey="6LfEx5EqAAAAAN3Ri6bU8BynXkRlTqh6l6mHbl4t">
+      <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1>Root Page.tsx</h1>
+        <TablePanel />
+      </div>
+    </GoogleReCaptchaProvider>
   );
 }
