493b2323aa67e947d247b635b76a0cff17c41e3a
Jossy Solá <jossysola@gmail.com>
2024-12-03 18:47:55 -0600

    Uses useMemo to memoize rows and columns. Levels down handleInputChange to <DynamicTable> component

--- app/ui/molecules/mol-table-panel.tsx
+++ app/ui/molecules/mol-table-panel.tsx
@@ -2,22 +2,25 @@
 import { useHTMLTable } from "@/app/hooks/custom";
 import DynamicTable from "./mol-table-dynamic";
 import { ActionButton } from "../atoms/atom-button";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 export default function TablePanel ({ rowsProp }: {
     rowsProp?: Array<Array<{id: string, value: string}>>,
 }) {
-    const { rows, columns, setRows, addRow, addColumn, removeRow, removeColumn, handleInputChange} = useHTMLTable();
+    const { rows, columns, setRows, addRow, addColumn, removeRow, removeColumn} = useHTMLTable();
     
     useEffect(() => {
         if (rowsProp) {
             setRows(rowsProp);
         }
     }, [rowsProp])
+
+    const memoizedRows = useMemo(() => rows, [rows]);
+    const memoizedColumns = useMemo(() => columns, [columns]);
     
     return (
         <>
-            <DynamicTable rows={rows} columns={columns} handleInputChange={handleInputChange} />
+            <DynamicTable rows={memoizedRows} columns={memoizedColumns} setRows={setRows}/>
             <ActionButton callback={addRow} text={"-Add row"}/>
             <ActionButton callback={addColumn} text={"-Add column"}/>
             <ActionButton callback={removeRow} text={"-Remove row"}/>
