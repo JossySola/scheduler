e79dabd3a570117966063366218d777576c3e84b
Jossy Solá <jossysola@gmail.com>
2024-12-03 19:03:18 -0600

    Removes defaultValue. Removes <form>

--- app/ui/organisms/org-table-save-form.tsx
+++ app/ui/organisms/org-table-save-form.tsx
@@ -1,25 +1,11 @@
-'use client'
-import { SaveAction } from "@/app/(routes)/table/create/actions"
 import TablePanel from "../molecules/mol-table-panel"
-import { SubmitButton } from "../atoms/atom-button"
-import { Action_State } from "@/app/lib/definitions"
-import { useActionState } from "react"
 
 export default function SaveTableForm() {
-    const initialState: Action_State = {
-        message: null,
-        errors: {}
-    }
-    const [state, formAction, isPending] = useActionState(SaveAction, initialState);
     
     return (
-        <form action={formAction} id="new-table" className="flex flex-col" aria-describedby="form-error">
-            <input name="table-name" id="table-name" type="text" defaultValue="Untitled" placeholder="Untitled" />
+        <>
+            <input name="table-name" id="table-name" type="text" placeholder="Untitled" />
             <TablePanel />
-            <SubmitButton text="save" isSubmitting={isPending ? true : false} />
-            <div id="form-error" aria-live="polite" aria-atomic="true">
-                {state?.message && <p>{state?.message}</p>}
-            </div>
-        </form>
+        </>
     )
 }
