bf35bbd3bb678daa702290ede2012b8db7677d65
Jossy Solá <jossysola@gmail.com>
2024-12-06 01:23:15 -0600

    Renders component conditionally based on the Window object's availability. Uses custom hook useReCAPTCHA and uses onSubmit attribute on <form>

--- app/ui/molecules/mol-signup.tsx
+++ app/ui/molecules/mol-signup.tsx
@@ -1,35 +1,40 @@
 'use client'
-import { SignUpAction } from "@/app/(routes)/signup/actions";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import Link from "next/link";
-import ReCAPTCHA from "../atoms/ReCAPTCHA";
+import { SubmitButton } from "../atoms/atom-button";
+import useReCAPTCHA from "@/app/lib/recaptcha";
 
 
-export default function SignUp () {
+export default function SignUp_Conditioned () {
+    const { isSubmitting, windowIsLoaded, executeCAPTCHA } = useReCAPTCHA('signup');
+    
+    if (!windowIsLoaded) {
+        return <p>Loading...</p>
+    } else {
+        return (
+            <section>
+                <form onSubmit={executeCAPTCHA} method="POST" className="flex-col">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" minLength={3} maxLength={30} required />
 
-    return (
-        <GoogleReCaptchaProvider reCaptchaKey="6LfEx5EqAAAAAN3Ri6bU8BynXkRlTqh6l6mHbl4t">
-            <form className="flex-col">
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" minLength={3} maxLength={30} required />
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" minLength={3} maxLength={15} required />
 
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" minLength={3} maxLength={15} required />
+                    <label htmlFor="birthday">Birthday:</label>
+                    <input type="date" id="birthday" name="birthday" required />
 
-                <label htmlFor="birthday">Birthday:</label>
-                <input type="date" id="birthday" name="birthday" required />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="example@domain.com" required />
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="example@domain.com" required />
+                    <label htmlFor="password">Set a password:</label>
+                    <input type="password" id="password" name="password" minLength={8} required />
 
-                <label htmlFor="password">Set a password:</label>
-                <input type="password" id="password" name="password" minLength={8} required />
-
-                <label htmlFor="confirmpwd">Confirm password:</label>
-                <input type="password" id="confirmpwd" name="confirmpwd" minLength={8} required />
-                <ReCAPTCHA text="Sign Up" formAction={SignUpAction} action="signup"/>
-            </form>
-            <Link href="#">Forgot Password</Link>
-        </GoogleReCaptchaProvider>
-    )
+                    <label htmlFor="confirmpwd">Confirm password:</label>
+                    <input type="password" id="confirmpwd" name="confirmpwd" minLength={8} required />
+                    
+                    <SubmitButton text="Sign Up" />
+                </form>
+                <Link href="#">Forgot Password</Link>
+            </section>
+        )
+    }
 }
