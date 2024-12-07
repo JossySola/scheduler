75df5d4813aca77c241bf1f39bbd302970e3f6df
Jossy Solá <jossysola@gmail.com>
2024-12-07 03:08:59 -0600

    Implements fetch to verify CAPTCHA token

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,10 +1,19 @@
-import { NextRequest } from "next/server";
 import "server-only";
+import { NextRequest } from "next/server";
 
 export async function POST (
     request: NextRequest
 ) {
-    console.log(request)
+    const req = await request.json();
+    const verify = await fetch("https://www.google.com/recaptcha/api/siteverify", {
+        method: 'POST',
+        body: new URLSearchParams({
+            secret: `${process.env.POSTGRES_GOOGLE_SECRET}`,
+            response: `${req['token']}`,
+        })
+    })
+    const verification = await verify.json();
+    console.log(verification)
 
-    return true
+    return Response.json({ req })
 }
