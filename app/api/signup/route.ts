09c02ec9b52f6c20601fad100f219ba572968581
Jossy Solá <jossysola@gmail.com>
2024-12-08 13:13:08 -0600

    Uses reCAPTCHA TypeScript type and cheks if response success is false

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,9 +1,11 @@
 import "server-only";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { reCAPTCHAResponse, ReturnReCAPTCHAError } from "@/app/lib/recaptcha/server-recaptcha";
 
 export async function POST (
     request: NextRequest
 ) {
+    
     const req = await request.json();
     const verify = await fetch("https://www.google.com/recaptcha/api/siteverify", {
         method: 'POST',
@@ -12,8 +14,22 @@ export async function POST (
             response: `${req['token']}`,
         })
     })
-    const verification = await verify.json();
-    console.log(verification)
+    const verification: reCAPTCHAResponse = await verify.json();
+    
+    if (verification.success === false) {
+        const error = verification['error-codes'][0];
+        return ReturnReCAPTCHAError(error);
+    }
+    /*
+    {
+        success: true,
+        challenge_ts: '2024-12-07T08:57:07Z',
+        hostname: 'localhost',
+        score: 0.9,
+        action: 'signup'
+    }
 
+    { success: false, 'error-codes': [ 'invalid-input-response' ] }
+    */
     return Response.json({ req })
 }
