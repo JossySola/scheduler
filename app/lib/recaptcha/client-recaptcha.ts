c6b2dbff28896d2cfe70c447b57e4d3f06be53b3
Jossy Solá <jossysola@gmail.com>
2024-12-08 13:15:46 -0600

    Creates in separation client and server reCAPTCHA utils

---
+++ app/lib/recaptcha/client-recaptcha.ts
@@ -0,0 +1,50 @@
+import { useState, useEffect, BaseSyntheticEvent } from "react";
+import { isInputValid, arePasswordsConfirmed } from "../utils";
+import { redirect } from "next/navigation";
+
+export default function useReCAPTCHA () {
+    'use client'
+    const [ windowIsLoaded, setWindowIsLoaded ] = useState<Window | null>(null);
+    const [ isSubmitting, setIsSubmitting ] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (window) {
+            setWindowIsLoaded(window);
+        }
+    })
+
+    const signupCAPTCHA = async (event: BaseSyntheticEvent<Event & EventTarget & HTMLFormElement & EventTarget>) => {
+        event.preventDefault();
+
+        await window.grecaptcha.ready(() => {
+            window.grecaptcha.execute('6LfEx5EqAAAAAN3Ri6bU8BynXkRlTqh6l6mHbl4t', { action: 'signup'}).then(async (token: string) => {
+                const formData = new FormData(event.target);
+                formData.append('recaptcha_token', token);
+
+                if (isInputValid(formData).ok && arePasswordsConfirmed(formData)) {
+                    await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/signup`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            token: formData.get('recaptcha_token'),
+                            name: formData.get('name'),
+                            username: formData.get('username'),
+                            birthday: formData.get('birthday'),
+                            email: formData.get("email"),
+                            password: formData.get("password"),
+                        })
+                    })
+                }
+            })
+        })
+        redirect("/signup");
+    }
+
+    return {
+        isSubmitting,
+        windowIsLoaded,
+        signupCAPTCHA,
+    }
+}
