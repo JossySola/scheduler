3486732242d891f31b8b2da348e9029d97305e35
Jossy Solá <jossysola@gmail.com>
2024-12-15 02:47:22 -0600

    Uses Zod schema and sets try...catch statement

--- auth.ts
+++ auth.ts
@@ -1,30 +1,41 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import * as argon2 from "argon2"
+import { logInSchema } from "./app/lib/zod";
+import { ZodError } from "zod";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Credentials({
             credentials: {
-                email: {},
                 username: {},
                 password: {},
             },
             authorize: async (credentials) => {
-                let user = null;
-                const hashedPassword = await argon2.hash(credentials.password);
+                try {
+                    let user = null;
+                    const { username, password } = await logInSchema.parseAsync(credentials);
 
-                // logic to verify if the user exists on DB
+                    const hashedPassword = await argon2.hash(credentials.password);
 
-                if (!user) {
-                    // No user found, so this is their first attempt to login
-                    // Optionally, this is also the place you could do a user registration
-                    throw new Error("Invalid credentials.");
+                    // logic to verify if the user exists on DB
+
+                    if (!user) {
+                        // No user found, so this is their first attempt to login
+                        // Optionally, this is also the place you could do a user registration
+                        throw new Error("Invalid credentials.");
+                    }
+
+                    // return the user object with their profile data
+                    return user;
+
+                } catch (error) {
+                    if (error instanceof ZodError) {
+                        // Return 'null' to indicate that the credentials are invalid
+                        return null;
+                    }
                 }
-
-                // return the user object with their profile data
-                return user;
-            }
-        })
+            },
+        }),
     ],
 })
