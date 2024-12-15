3ab753fe845850b60f3821848262af9c263e57ae
Jossy Solá <jossysola@gmail.com>
2024-12-15 02:46:23 -0600

    Sets basic Zod schema for Log In

--- app/lib/zod.ts
+++ app/lib/zod.ts
@@ -1,5 +1,10 @@
 import { object, string } from "zod";
 
 export const logInSchema = object({
-    
+    username: string({ required_error: "Username or e-mail required" })
+    .min(1, "Username or e-mail required"),
+    password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
 })
