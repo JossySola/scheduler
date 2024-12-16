1b3c2d2c65b825376075caca5c421227cb4db5bc
Jossy Solá <jossysola@gmail.com>
2024-12-15 22:35:41 -0600

    Adds 'label' and 'type' keys to credentials. Replaces zod parse into a Promise.all

--- auth.ts
+++ auth.ts
@@ -1,25 +1,29 @@
+import "server-only"
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import * as argon2 from "argon2"
 import { logInSchema } from "./app/lib/zod";
 import { ZodError } from "zod";
+import { getUserFromDb } from "./app/lib/utils";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Credentials({
             credentials: {
-                username: {},
-                password: {},
+                username: { label: "Username or e-mail", type: "text" },
+                password: { label: "Password", type: "password" },
             },
             authorize: async (credentials) => {
                 try {
                     let user = null;
-                    const { username, password } = await logInSchema.parseAsync(credentials);
-
-                    const hashedPassword = await argon2.hash(credentials.password);
+                    const [ username, email, password ] = await Promise.all([
+                        logInSchema.username.parseAsync(credentials.username),
+                        logInSchema.email.parseAsync(credentials.username),
+                        logInSchema.password.parseAsync(credentials.password),
+                    ]);
 
                     // logic to verify if the user exists on DB
-
+                    user = await getUserFromDb(username, email, password);
                     if (!user) {
                         // No user found, so this is their first attempt to login
                         // Optionally, this is also the place you could do a user registration
@@ -28,8 +32,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
                     // return the user object with their profile data
                     return user;
-
                 } catch (error) {
+                    console.error(error);
                     if (error instanceof ZodError) {
                         // Return 'null' to indicate that the credentials are invalid
                         return null;
@@ -38,4 +42,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             },
         }),
     ],
+    pages: {
+        signIn: "/login"
+    },
+    debug: true,
 })
