234f707c529e95c1ad090699eb85908a16b7cd14
Jossy Solá <jossysola@gmail.com>
2024-12-17 12:16:11 -0600

    Copy-pastes the function recently tested with Vitest

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,6 +2,7 @@
 import "server-only"
 import { Result_FormDataToQuery } from "./definitions";
 import { v4 as uuidv4 } from 'uuid';
+import crypto from "crypto";
 import pool from "./mocks/db";
 
 export async function FormDataToQuery (formData: FormData) {
@@ -162,7 +163,33 @@ export async function arePasswordsConfirmed (formData: FormData) {
 }
 
 export async function isPasswordPwned (password: string) {
-    
+    try {
+        // API requires password to be hashed with SHA1
+        const hashed = crypto.createHash('sha1').update(password).digest('hex');
+        // "range" endpoint requires only the first 5 characters of the hashed password
+        const range = hashed.slice(0,5);
+        // Slice gives the remaining hashed password after the string index 5
+        const suffixToCheck = hashed.slice(5).toUpperCase();
+        const response = await fetch(`https://api.pwnedpasswords.com/range/${range}`);
+        // The API returns a plain text response, not a JSON
+        const text = await response.text();
+        // For each \n break, convert the plain text into an Array
+        const lines = text.split('\n');
+
+        for (const line of lines) {
+            // The format of each line is divided by a ":", the left part is the suffix and the right part is the count
+            const [hashSuffix, count] = line.split(':');
+            // If the rest of the hashed password is found on the exposed list
+            if (hashSuffix === suffixToCheck) {
+                // return the count part as a number
+                return parseInt(count, 10);
+            }
+        }
+        // If the remaining part of the hashed password is not found on the list, return 0
+        return 0;
+    } catch (error) {
+        throw error;
+    }
 }
 
 export async function getUserFromDb (username: string, email: string, password: string) {
