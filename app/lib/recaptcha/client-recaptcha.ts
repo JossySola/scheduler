4029847cc5c50737a9957e9bc82d38956e09492f
Jossy Solá <jossysola@gmail.com>
2024-12-18 11:28:13 -0600

    Implements isInputValid function again and returns messages corresponding to failure of the next checks: input validation, password exposition, and password confirmation

--- app/lib/recaptcha/client-recaptcha.ts
+++ app/lib/recaptcha/client-recaptcha.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, BaseSyntheticEvent } from "react";
-import { arePasswordsConfirmed, isPasswordPwned } from "../utils";
+import { arePasswordsConfirmed, isInputValid, isPasswordPwned } from "../utils";
 
 export default function useReCAPTCHA () {
     'use client'
@@ -21,19 +21,19 @@ export default function useReCAPTCHA () {
                 window.grecaptcha.execute('6LfEx5EqAAAAAN3Ri6bU8BynXkRlTqh6l6mHbl4t', { action: 'signup'}).then(async (grecaptchaToken: string) => {
                     const formData = new FormData(event.target);
                     formData.append('recaptcha_token', grecaptchaToken);
-
+                    
                     const token = formData.get('recaptcha_token')?.toString();
                     const name = formData.get('name')?.toString();
                     const username = formData.get('username')?.toString();
                     const birthday = formData.get('birthday')?.toString();
                     const email = formData.get('email')?.toString();
                     const password = formData.get('password')?.toString();
-
                     
                     const passwordIsNotExposed = await isPasswordPwned(password!);
                     const passwordIsConfirmed = await arePasswordsConfirmed(formData);
+                    const inputIsValid = await isInputValid(formData);
                     
-                    if (passwordIsNotExposed && passwordIsConfirmed) {
+                    if (inputIsValid.ok && passwordIsNotExposed === 0 && passwordIsConfirmed) {
                         const response = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/signup`, {
                             method: 'POST',
                             headers: {
@@ -53,7 +53,16 @@ export default function useReCAPTCHA () {
                             setError(res.statusText);
                         }
                     } else {
-                        setError("Invalid input.")
+                        if (passwordIsNotExposed > 0) {
+                            setError('Upon a password verification, unfortunately this password has been exposed in a data breach. For security reasons, please choose another password.');
+                            return;
+                        } else if (passwordIsConfirmed === false) {
+                            setError('The password confirmation is likely to have an error');
+                            return;
+                        } else {
+                            setError(inputIsValid.message);
+                            return;
+                        }
                     }
                 })
             })
