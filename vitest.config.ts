1db61a92f9459624ad3eabb215537197ca3a65f0
Jossy Solá <jossysola@gmail.com>
2024-12-27 01:06:09 -0600

    Removes the environment property, so it is set specifically in the workspace configuration instead of globally

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         reporters: ['verbose'],
-        environment: 'jsdom',
         globals: true,
     }
 })
