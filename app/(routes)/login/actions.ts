5e91cd2c399ae24cfc32111c20fe845d92a9aa27
Jossy Solá <jossysola@gmail.com>
2025-01-05 14:29:30 -0600

    Passes data from formData to signIn and configures redirection

--- app/(routes)/login/actions.ts
+++ app/(routes)/login/actions.ts
@@ -4,19 +4,17 @@ import { signIn } from "@/auth";
 export async function LogInAction (prevState: { message: string }, formData: FormData) {
     const username = formData.get('username');
     const password = formData.get('password');
-    try {
-        await signIn("credentials", { 
-            redirect: true, 
-            redirectTo: '/dashboard',
-            username,
-            password
-        });
+    
+    const result = await signIn("credentials", {
+        redirect: true,
+        redirectTo: '/dashboard',
+        username,
+        password,
+    });
+
+    if (!result) {
         return {
-            message: "Successful"
-        }
-    } catch (error) {
-        return {
-            message: "Invalid credentials"
+            message: "Invalid credentials (action)"
         }
     }
 }
