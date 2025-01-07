5cdf9fb8d380a3789a88dab08305c0e9f5a17d24
Jossy Solá <jossysola@gmail.com>
2025-01-06 22:02:30 -0600

    Creates individual components for each form element used in signup

--- app/ui/atoms/atom-button.tsx
+++ app/ui/atoms/atom-button.tsx
@@ -1,4 +1,4 @@
-export function ActionButton({callback, text}: {
+export function Button({callback, text}: {
     callback: () => void,
     text: string | null,
 }) {
@@ -13,7 +13,7 @@ export function SubmitButton({text, disabled, form, formaction, formenctype, for
     text: string,
     disabled?: boolean,
     form?: string,
-    formaction?: string | ((formData: FormData) => void | Promise<unknown>),
+    formaction?: string | ((formData: FormData) => void | Promise<void>),
     formenctype?: "application/x-www-form-urlencoded" | "multipart/form-data" | "text/plain",
     formmethod?: "post" | "get" | "dialog",
     formnovalidate?: boolean,
@@ -39,4 +39,73 @@ export function SubmitButton({text, disabled, form, formaction, formenctype, for
     {text}{isSubmitting ? "Submitting...": null}{isSubmitted ? "Submitted!" : null}
     </button> 
         
+}
+
+export function ActionButton({action, text, disabled, form, formaction, formenctype, formmethod, formnovalidate, formtarget, isSubmitting = false, isSubmitted = false, onClick, onSubmit}: {
+    action: string,
+    text: string,
+    disabled?: boolean,
+    form?: string,
+    formaction?: string | ((formData: FormData) => void | Promise<void>),
+    formenctype?: "application/x-www-form-urlencoded" | "multipart/form-data" | "text/plain",
+    formmethod?: "post" | "get" | "dialog",
+    formnovalidate?: boolean,
+    formtarget?: "_self" | "_blank" | "_parent" | "_top",
+    isSubmitting?: boolean,
+    isSubmitted?: boolean,
+    onClick?: () => void,
+    onSubmit?: () => void,
+}) {
+    "use client"
+    return <button
+            disabled={disabled}
+            form={form}
+            formAction={formaction}
+            formEncType={formenctype}
+            formMethod={formmethod}
+            formNoValidate={formnovalidate}
+            formTarget={formtarget}
+            onClick={async (e) => {
+                e.preventDefault();
+                const key = '6LfEx5EqAAAAAN3Ri6bU8BynXkRlTqh6l6mHbl4t';
+                if (!key) {
+                    console.error("Google reCAPTCHA key is missing.");
+                }
+                const formElement = (e.currentTarget as HTMLButtonElement).form;
+                
+                console.log(formElement)
+                try {
+                    await window.grecaptcha.ready(() => {
+                        window.grecaptcha
+                        .execute(key, { action })
+                        .then(async (token: string) => {
+                            const request = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/grecaptcha`, {
+                                method: 'POST',
+                                headers: {
+                                    "Content-Type": "application/json"
+                                },
+                                body: JSON.stringify({
+                                    token
+                                })
+                            })
+                            const response = await request.json();
+                            if (response.status !== 200) {
+                                throw Error;
+                            }
+                            
+                            formElement?.submit();
+                        })
+                        .catch(() => {
+                            console.error("Error executing reCAPTCHA");
+                        })
+                    })
+                } catch (error) {
+                    console.error(error);
+                }
+
+            }}
+            onSubmit={onSubmit}
+            >
+            {text}
+    </button>       
 }
---
+++ app/ui/atoms/atom-form-input-name.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useState } from "react"
+
+export default function FormInputName () {
+    const [ name, setName ] = useState<string>('');
+
+    return (
+        <>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" value={name} minLength={3} maxLength={30} autoComplete="name" required
+            onChange={(e => {
+                e.preventDefault();
+                setName(e.target.value);
+            })} />
+        </>
+    )
+}
