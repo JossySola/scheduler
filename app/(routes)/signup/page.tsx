f9326a9e7207948048151f0b5667e0117330bf18
Jossy Solá <jossysola@gmail.com>
2025-01-06 21:54:33 -0600

    Changes the name of child component

--- app/(routes)/signup/page.tsx
+++ app/(routes)/signup/page.tsx
@@ -1,9 +1,9 @@
-import SignUp_Conditioned from "@/app/ui/organisms/org-signup";
+import SignUp from "@/app/ui/organisms/org-signup";
 
 export default async function Page () {
     return (
         <main>
-            <SignUp_Conditioned />
+            <SignUp />
         </main>
     )
 }
