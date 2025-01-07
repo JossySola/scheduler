4fa2d5e9ecfa5c11e38dbd9db2153a6bfe5ec01c
Jossy Solá <jossysola@gmail.com>
2025-01-06 22:05:57 -0600

    Creates components for a form fielset containing inputs dedicated to the user's basic information and a fieldset where the user confirms the email with the token. It also creates the component for the parent form element

--- app/ui/molecules/mol-confirm-email.tsx
+++ app/ui/molecules/mol-confirm-email.tsx
@@ -1,30 +1,30 @@
-import handleConfirmation from "@/app/(routes)/signup/actions";
-import { sendEmailConfirmation } from "@/app/lib/utils-server";
-import { useActionState, useState } from "react"
+"use client"
+import { useActionState, useEffect, useState } from "react";
+import { confirmEmailAction } from "@/app/(routes)/signup/actions";
+import { ActionButton } from "../atoms/atom-button";
 
-export default function ConfirmEmail (form: { email: string }) {
-    const [sent, setSent] = useState<boolean>(false);
-    const [state, formAction, pending] = useActionState(handleConfirmation, { message: ''});
+export default function ConfirmEmail () {
+    const [confirmState, confirmAction, pending] = useActionState(confirmEmailAction, { ok: false, message: ''});
+    const [ windowReady, setWindowReady ] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (window) {
+            setWindowReady(true);
+        }
+    }, []);
+
+    if (!windowReady) {
+        return <p>Loading...</p>
+    } else {
+        return (
+            <fieldset>
+                <h3>We've sent you an e-mail with a code, please type it here to verify your account</h3>
+                <h4>Hemos enviado un e-mail que contiene un código, por favor ingresalo aquí para verificar tu cuenta</h4>
     
-    return (
-        <section>
-            <h3>We've sent you an e-mail with a code, please type it here to verify your account</h3>
-            <h4>Hemos enviado un e-mail que contiene un código, por favor ingresalo aquí para verificar tu cuenta</h4>
-            <form action={formAction}>
-                <input type="hidden" id="email" name="email" value={form.email}></input>
-                <input maxLength={6} id="confirmation-token" name="confirmation-token"></input>
-                <button type="submit">{pending ? 'Sending...' : 'Confirm'}</button>
-            </form>
-            <p>{state.message}</p>
-
-            <button type="button" onClick={(event) => {
-                event.preventDefault();
-                sendEmailConfirmation(`${form.email}`);
-                setSent(true);
-            }}>Resend code</button>
-            {
-                sent ? <p>A new code has been sent!</p> : null
-            }
-        </section>
-    )
+                <input minLength={6} maxLength={6} id="confirmation-token" name="confirmation-token" required />
+                <ActionButton action="confirm_email" form="register" text="Confirm" formaction={ confirmAction } disabled={ pending } />   
+            
+            </fieldset>
+        )
+    }
 }
---
+++ app/ui/molecules/mol-signup-form.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useState } from "react";
+import BasicInformation from "./mol-signup-information";
+import ConfirmEmail from "./mol-confirm-email";
+
+export default function SignupForm () {
+    const [ validated , setValidated ] = useState<boolean>(false);
+
+    return (
+        <form name="register" id="register">
+            <BasicInformation validated={validated} setValidated={setValidated} />
+            <ConfirmEmail />
+        </form>
+    )
+}
