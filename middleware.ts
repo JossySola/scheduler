a5f86691c26aa9df363a21fc3d7e572efae2772b
Jossy Solá <jossysola@gmail.com>
2025-01-08 15:08:49 -0600

    Removes the Google reCAPTCHA verification logic as it is going to be handled by the /api/grecaptcha endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,23 @@
-import { NextResponse, NextRequest } from 'next/server'
-import { reCAPTCHAResponse, VerificationResponse } from './app/lib/recaptcha/server-recaptcha';
-import { ReturnReCAPTCHAError } from './app/lib/recaptcha/server-recaptcha';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware (request: NextRequest) {
-    if (request.nextUrl.pathname.startsWith('/api/signup')) {
-        const googleResponse: VerificationResponse = await GoogleVerification(request);    
-        if (googleResponse.status !== 200) {
-            return NextResponse.json(googleResponse);
+    const secret = process.env.NEXTAUTH_SECRET;
+    const token = await getToken({
+        req: request,
+        secret: secret
+    });
+    
+    if (!['/','/login', '/signup'].includes(request.nextUrl.pathname)) {
+        if (!token) {
+            return NextResponse.redirect(new URL("/login", request.url));
         }
-        return NextResponse.next();
-    }
-    if (request.nextUrl.pathname.startsWith('/dashboard')) {
-        
+    } else if (['/login', '/signup', '/reset'].includes(request.nextUrl.pathname)) {
+        if (token) {
+            return NextResponse.redirect(new URL("/dashboard", request.url));
+        }
     }
 }
 
 export const config = {
     matcher: ['/api', '/dashboard'],
-}
-
-const GoogleVerification = async (request: NextRequest) => {
-    const req = await request.json();
-    const verify = await fetch("https://www.google.com/recaptcha/api/siteverify", {
-        method: 'POST',
-        body: new URLSearchParams({
-            secret: `${process.env.POSTGRES_GOOGLE_SECRET}`,
-            response: `${req['token']}`,
-        })
-    })
-    const verification: reCAPTCHAResponse = await verify.json();
-    
-    if (verification.success === false) {
-        return ReturnReCAPTCHAError(verification['error-codes'][0]);
-    }
-    if (verification.score < 0.8) {
-        return {
-            status: 401,
-            statusText: 'Requires human verification'
-        }
-    }
-    return {
-        status: 200,
-        statusText: 'Verified!'
-    }
 }
