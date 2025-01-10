1b8abbcde11820e1de598cef434e00449b049489
Jossy Solá <jossysola@gmail.com>
2025-01-10 01:10:10 -0600

    Refactors and adds console messages to debug issue

--- app/api/argon/user/route.ts
+++ app/api/argon/user/route.ts
@@ -2,32 +2,38 @@ import { getUserFromDb } from "@/app/lib/utils-server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST (request: NextRequest) {
+    console.error("[/api/argon/user/POST] Starting...")
     const { username, password } = await request.json();
-    
+    console.error("[/api/argon/user/POST] Parsed username:", username)
+    console.error("[/api/argon/user/POST] Parsed password:", password)
     if (!username || !password) {
+        console.error("[/api/argon/user/POST] There is data missing, exiting...")
         return NextResponse.json({
             status: 409,
-            statusText: 'Missing username or password (route)',
+            statusText: 'Missing username or password',
         })
     }
 
-    try {
-        const user = await getUserFromDb(username, password);
-        
-        if (!user) {
-            return NextResponse.json({
-                status: 401,
-                statusText: 'Invalid credentials (route)',
+    const user = await getUserFromDb(username, password);
+    console.error("[/api/argon/user/POST] Result from getUserFromDb:", user)
+    
+    if (!user.ok) {
+        console.error("[/api/argon/user/POST] User ok property is not true")
+        if (user.provider) {
+            console.error("[/api/argon/user/POST] There is a provider property:", user.provider);
+            console.error("[/api/argon/user/POST] Exiting...")
+            throw new Error(`${user.message}`, {
+                cause: {
+                    details: `${user.provider}`
+                }
             })
         }
-        return NextResponse.json({
-            status: 200,
-            user: user,
-        })
-    } catch (error) {
-        return NextResponse.json({
-            status: 400,
-            statusText: 'Unexpected error (route)'
-        })
+        console.error("[/api/argon/user/POST] Exiting...")
+        throw new Error(`${user.message}`)
     }
+    console.error("[/api/argon/user/POST] Exiting...")
+    return NextResponse.json({
+        status: 200,
+        user: user,
+    })
 }
