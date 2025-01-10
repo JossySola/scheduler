acd90e7d43d8c51dc30619724be8b951222bfc76
Jossy Solá <jossysola@gmail.com>
2025-01-10 01:18:27 -0600

    Refactors component. Leaves parent <form> and inside, it renders two child components, one of which depending on the validation of the user's data

--- app/ui/molecules/mol-signup-form.tsx
+++ app/ui/molecules/mol-signup-form.tsx
@@ -2,16 +2,20 @@
 import { useState } from "react";
 import BasicInformation from "./mol-signup-information";
 import ConfirmEmail from "./mol-confirm-email";
+import Link from "next/link";
 
 export default function SignupForm () {
     const [ validated , setValidated ] = useState<boolean>(false);
 
     return (
-        <form name="register" id="register">
-            <BasicInformation validated={validated} setValidated={setValidated} />
-            {
-                validated ? <ConfirmEmail /> : null
-            }
-        </form>
+        <>
+            <form name="register" id="register">
+                <BasicInformation validated={validated} setValidated={setValidated} />
+                {
+                    validated ? <ConfirmEmail /> : <p>Or if you already have an account, <Link href={"/login"}>Login</Link></p>
+                }
+            </form>
+        </>
+        
     )
 }
