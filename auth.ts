18341746f03337b24acf8af6dd969f07dbf35cd6
Jossy Solá <jossysola@gmail.com>
2025-01-10 01:19:32 -0600

    Refactors authorize function and adds console logs to debug. Implementation of JWT and Session callbacks

--- auth.ts
+++ auth.ts
@@ -1,4 +1,3 @@
-import "server-only";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
@@ -11,6 +10,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             clientId: process.env.AUTH_GOOGLE_ID,
             clientSecret: process.env.AUTH_GOOGLE_SECRET,
             async profile(profile) {
+                console.log("[Auth Providers Google] Starting...")
+                console.log("[Auth Providers Google] Fetching /api/signup")
+                const signup = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/signup`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name: profile.name,
+                        username: profile.name,
+                        email: profile.email,
+                        provider: 'Google',
+                        provider_id: profile.sub
+                    })
+                })
+                console.log("[Auth Providers Google] Fetch result:", signup)
+                console.log("[Auth Providers Google] Exiting...")
                 return { ...profile };
             }
         }),
@@ -18,6 +34,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             clientId: process.env.AUTH_FACEBOOK_ID,
             clientSecret: process.env.AUTH_FACEBOOK_SECRET,
             async profile(profile) {
+                console.log("[Auth Providers Facebook] Starting...")
+                console.log("[Auth Providers Facebook] Fetching /api/signup")
+                const signup = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/signup`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name: profile.name,
+                        username: profile.name,
+                        email: profile.email,
+                        provider: 'Facebook',
+                        provider_id: profile.id
+                    })
+                })
+                console.log("[Auth Providers Facebook] Fetch result:", signup)
+                console.log("[Auth Providers Facebook] Exiting...")
                 return { ...profile };
             }
         }),
@@ -27,31 +60,28 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 password: { label: "Password", type: "password" },
             },
             async authorize(credentials) {
-                
-                if (!credentials || !credentials.username || !credentials.password) {
-                    return null;
-                }
-                try {
-                    const response = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/argon/user`, {
-                        method: 'POST',
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            username: credentials.username,
-                            password: credentials.password,
-                        })
+                console.log("[Auth Credentials authorize] Starting...")
+                console.log("[Auth Credentials authorize] Credentials:", credentials)
+                console.log("[Auth Credentials authorize] Fetching /api/argon/user...")
+                const response = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/argon/user`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        username: credentials.username,
+                        password: credentials.password,
                     })
-                    
-                    if (!response.ok || response.status !== 200) {
-                        return null;
-                    }
-                    
-                    const { user } = await response.json();
-                    return user;
-                } catch (error) {
-                    return null;
+                });
+                console.log("[Auth Credentials authorize] Fetch response:", response)
+                
+                if (!response.ok) {
+                    console.log("[Auth Credentials authorize] Parsed response status is not 200")
+                    console.log("[Auth Credentials authorize] Exiting...")
+                    throw new Error ("Invalid credentials.");
                 }
+
+                const data = await response.json();
+                console.log("[Auth Credentials authorize] JSON parse:", data)
+                console.log("[Auth Credentials authorize] Exiting...")
+                return data.user;
             },
         }),
     ],
@@ -59,31 +89,49 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         strategy: "jwt"
     },
     callbacks: {
-        jwt({ token, user, account, trigger }) {
+        async jwt({ token, user, account, trigger }) {
+            console.log("[Auth JWT] Starting...")
             if (user) {
-                token.image = user.image;
-                token.picture = user.picture;
+                console.log("[Auth JWT] User object exists...")
+                token.id = token.sub;
+                if (user.picture && user.picture.data && user.picture.data.url) {
+                    console.log("[Auth JWT] user.picture.data.url exists...")
+                    token.image = user.picture.data.url;
+                } else if (user.picture) {
+                    console.log("[Auth JWT] user.picture exists...")
+                    token.image = user.picture;
+                } else {
+                    console.log("[Auth JWT] No picture property...")
+                    token.image = null;
+                }
                 if (account) {
+                    console.log("[Auth JWT] Account object exists...")
                     token.provider = account.provider;
-                    token.access_token = account.access_token;
                 }
                 if (trigger) {
+                    console.log("[Auth JWT] Trigger object exists...")
                     token.trigger = trigger;
                 }
             }
+            console.log("[Auth JWT] Token:", token)
+            console.log("[Auth JWT] Exiting...")
             return token;
         },
-        session({ session, token }) {
-            session.user.image = token.image;
-            session.user.picture = token.picture;
-            session.user.provider = token.provider;
-            session.user.trigger = token.trigger;
-            session.user.access_token = token.access_token;
+        async session({ session, token }) {
+            console.log("[Auth Session] Starting...")
+            if (session.user) {
+                console.log("[Auth Session] User object exists...")
+                session.user.image = token.image;
+                session.user.provider = token.provider;
+                session.user.id = token.id;
+            }
+            console.log("[Auth Session] Session:", session)
+            console.log("[Auth Session] Exiting...")
             return session;
         },
     },
     pages: {
-        signIn: "/login"
+        signIn: "/login",
     },
     debug: false,
 })
