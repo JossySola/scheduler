e4cefa46ba94a34e2df32ee0b2de226fc172d4e3
Jossy Solá <jossysola@gmail.com>
2025-01-10 01:17:19 -0600

    Creates a client-side component to handle password resets

---
+++ app/ui/molecules/mol-reset-password.tsx
@@ -0,0 +1,90 @@
+'use client'
+import { sendResetPasswordConfirmation } from "../../lib/utils-server";
+import { SubmitButton } from "@/app/ui/atoms/atom-button";
+import { useState } from "react";
+import { handlePasswordReset, handleTokenConfirmation } from "../../(routes)/reset/actions";
+import Link from "next/link";
+
+export default function ResetPassword() {
+    const [ email, setEmail ] = useState<string | undefined>('');
+    const [ emailSent, setEmailSent ] = useState<boolean>(false);
+    const [ tokenConfirmed, setTokenConfirmed ] = useState<boolean>(false);
+    const [ message, setMessage ] = useState<string>('');
+    const [ resetSuccessful, setResetSuccessful ] = useState<boolean>(false);
+
+    if (resetSuccessful) {
+        return (
+            <>
+                <p>{message}</p>
+                <Link href={'/login'}>Login</Link>
+            </>
+            
+        )
+    } else if (tokenConfirmed) {
+        return (
+            <form onSubmit={ async (e) => {
+                e.preventDefault();
+                const form = e.target as HTMLFormElement;
+                const formData = new FormData(form);
+                const password = formData.get('password')?.toString();
+                const confirmationPassword = formData.get('confirm-password')?.toString();
+
+                const reset = await handlePasswordReset(password, confirmationPassword, email);
+                
+                if (reset.status === 200) {
+                    setResetSuccessful(true);
+                    setMessage(reset.statusText);
+                } else {
+                    setMessage(`${reset.statusText}`)
+                }
+
+            }}>
+                <input name="password" id="password" autoComplete="new-password" type="password" required />
+                <input name="confirm-password" id="confirm-password" autoComplete="new-password" type="password" required />
+                <p>{message}</p>
+                <SubmitButton text="Reset password" />
+            </form>
+        )
+    } else if (emailSent) {
+        return (
+            <form onSubmit={ async (e) => {
+                e.preventDefault();
+                const form = e.target as HTMLFormElement;
+                const formData = new FormData(form);
+                const token = formData.get('confirmation-token')?.toString();
+            
+                const confirmation = await handleTokenConfirmation(token, email);
+                
+                if (confirmation.status === 200) {
+                    setTokenConfirmed(true);
+                } else {
+                    setMessage(`${confirmation.statusText}`)
+                }
+            }}>
+                <p>Type the code sent to your email here:</p>
+                <input name="confirmation-token" id="confirmation-token" maxLength={6} required/>
+                <p>{message}</p>
+                <SubmitButton text="Confirm Code" />
+            </form>
+        )
+    } else {
+        return (
+            <>
+                <form onSubmit={ async (e) => {
+                    e.preventDefault();
+                    const form = e.target as HTMLFormElement;
+                    const formData = new FormData(form);
+                    const email = formData.get('email')?.toString();
+    
+                    await sendResetPasswordConfirmation(email);
+                    setEmailSent(true);
+                    setEmail(email)
+                }}>
+                    <p>Please type your email to send a verification code</p>
+                    <input name="email" id="email" placeholder="email@provider.com" required/>
+                    <SubmitButton text="Send Code" />
+                </form>
+            </>
+        )
+    }
+}
