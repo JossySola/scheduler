a2491af8342c486d2ec3c4c66312e4bd93a551f0
Jossy Solá <jossysola@gmail.com>
2025-01-11 01:39:50 -0600

    Converts it to client component. Imports SubmitButton, SaveTableAction and uuidv4

--- app/ui/molecules/mol-XForm.tsx
+++ app/ui/molecules/mol-XForm.tsx
@@ -1,10 +1,21 @@
+"use client"
+import { useActionState } from "react";
 import XTable from "./mol-XTable";
+import { SaveTableAction } from "@/app/(routes)/table/actions";
+import { SubmitButton } from "../atoms/atom-button";
+import { v4 as uuidv4 } from "uuid";
 
-export default async function XForm ({ id }: { id: string }) {
+export default function XForm ({ id }: { id: string }) {
+    const [ saveState, saveAction, savePending ] = useActionState(SaveTableAction, { message: "" });
+    
     // save table
     return (
-        <form>
+        <form id={id ? id : "try"} name={id ? id : "try"}>
+            <input type="text" name="table_title" id="table_title" defaultValue="Untitled table" />
+            
             <XTable id={id} />
+
+            <SubmitButton text={savePending ? "Saving..." : "Save"} formaction={saveAction} disabled={savePending} />
         </form>
     )
 }
