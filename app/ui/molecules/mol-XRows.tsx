767c7dace830b1253c5fb67343dc9bfc66355b40
Jossy Solá <jossysola@gmail.com>
2025-01-11 01:38:51 -0600

    Corrects columns names based on indexes

--- app/ui/molecules/mol-XRows.tsx
+++ app/ui/molecules/mol-XRows.tsx
@@ -65,7 +65,7 @@ const RowInput = ({ rIndex, cIndex, value, setNewValue, currentRows } :
         currentRows: Array<Array<string>>,
     }) => {
     const columnNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    const columnName = `${columnNames[cIndex]}${cIndex}`;
+    const columnName = `${columnNames[cIndex]}${rIndex}`;
     return <input type="text" key={columnName} id={columnName} name={columnName} value={value} autoComplete="off" onChange={(e) => {
         e.preventDefault();
         setNewValue(() => {
