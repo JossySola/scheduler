930c58a3d806951030494d9509a031864131b3e7
Jossy Solá <jossysola@gmail.com>
2025-01-11 19:55:39 -0600

    Receives two additional props as optional

--- app/ui/molecules/mol-XForm.tsx
+++ app/ui/molecules/mol-XForm.tsx
@@ -1,19 +1,23 @@
 "use client"
-import { useActionState } from "react";
+import { useActionState, SetStateAction } from "react";
 import XTable from "./mol-XTable";
 import { SaveTableAction } from "@/app/(routes)/table/actions";
 import { SubmitButton } from "../atoms/atom-button";
 import { v4 as uuidv4 } from "uuid";
 
-export default function XForm ({ id }: { id: string }) {
+export default function XForm ({ id, rows, setRows }: 
+    { 
+        id: string,
+        rows?: Array<Array<string>>,
+        setRows?: React.Dispatch<SetStateAction<string[][]>>
+     }) {
     const [ saveState, saveAction, savePending ] = useActionState(SaveTableAction, { message: "" });
     
-    // save table
     return (
         <form id={id ? id : "try"} name={id ? id : "try"}>
             <input type="text" name="table_title" id="table_title" defaultValue="Untitled table" />
             
-            <XTable id={id} />
+            <XTable id={id} rows={rows} setRows={setRows} />
 
             <SubmitButton text={savePending ? "Saving..." : "Save"} formaction={saveAction} disabled={savePending} />
         </form>
