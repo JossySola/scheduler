e1006c43010cba311abb1a163ddaa3b638f08f54
Jossy Solá <jossysola@gmail.com>
2025-01-15 22:22:21 -0600

    Sets values useState hook to pass prop to XList

--- app/ui/organisms/org-XPanel.tsx
+++ app/ui/organisms/org-XPanel.tsx
@@ -1,18 +1,55 @@
 "use client"
-import { useState } from "react"
-import XForm from "../molecules/mol-XForm"
+import { useEffect, useState } from "react";
+import XForm from "../molecules/mol-XForm";
 import XList from "../molecules/mol-XList";
+import { SessionProvider } from "next-auth/react";
 
-export default function XPanel ({ id }:{ id: string }) {
+export default function XPanel ({ id }:{ id?: string }) {
     const [ colHeaders, setColHeaders ] = useState<Array<string>>([]);
     const [ rowHeaders, setRowHeaders ] = useState<Array<string>>([]);
     const [ rows, setRows ] = useState<Array<Array<string>>>([]);
+    const [ values, setValues ] = useState<Array<string>>([]);
+
+    // Update colHeaders and rowHeaders based on rows modifications
+    useEffect(() => {
+        let update: Array<string> = [];
+        rows.forEach((row, index) => {
+            if (index === 0) {
+                return false;
+            }
+            update.push(row[0]);
+        })
+        setRowHeaders(update);
+    }, [rows]);
+
+    useEffect(() => {
+        if (rows && rows[0]) {
+            setColHeaders(() => {
+                return rows[0].toSpliced(0,1);
+            })
+        }
+    }, [rows]);
 
     return (
-        <>
-        <XForm id={id}>
-            <XList name="Rows" criteria={colHeaders} />
-        </XForm>
-        </>
+        <SessionProvider>
+            <XForm id={id} rows={rows} setRows={setRows} values={values}>
+                <>
+                    <XList 
+                    name="Rows" 
+                    items={rowHeaders}
+                    setItems={setRowHeaders}
+                    criteria={colHeaders} 
+                    values={values}
+                    enableInput={false} 
+                    enableRemoval={false} />
+                    
+                    <XList 
+                    name="Predefined values"
+                    items={values}
+                    setItems={setValues} />
+                </>
+            </XForm>
+        </SessionProvider>
+        
     )
 }
