3cc78dd18483965401f5bf92b36cbd7c34abac79
Jossy Solá <jossysola@gmail.com>
2025-01-15 22:13:18 -0600

    Creates states to handle title and times. Sets two useEffect hooks, one to define 'times' with 'rows.length', the other to use 'setRows' to the Open AI response. Adds an input to let the user choose how many times a column is filled with values

--- app/ui/molecules/mol-XForm.tsx
+++ app/ui/molecules/mol-XForm.tsx
@@ -1,26 +1,73 @@
 "use client"
-import { useActionState, SetStateAction } from "react";
+import { useActionState, SetStateAction, useEffect, useState } from "react";
 import XTable from "./mol-XTable";
-import { SaveTableAction } from "@/app/(routes)/table/actions";
+import { SaveTableAction, UseAiAction } from "@/app/(routes)/table/actions";
 import { SubmitButton } from "../atoms/atom-button";
-import { v4 as uuidv4 } from "uuid";
+import { useSession } from "next-auth/react";
+import { useParams } from "next/navigation";
 
-export default function XForm ({ children, id, rows, setRows }: 
+export default function XForm ({ id, children, rows, setRows, values }: 
     { 
         children: React.JSX.Element,
-        id: string,
+        id?: string,
         rows?: Array<Array<string>>,
-        setRows?: React.Dispatch<SetStateAction<string[][]>>
+        setRows?: React.Dispatch<SetStateAction<string[][]>>,
+        values?: Array<string>,
      }) {
+    const { data: session } = useSession();
+    const params = useParams();
     const [ saveState, saveAction, savePending ] = useActionState(SaveTableAction, { message: "" });
+    const [ aiState, aiAction, aiPending ] = useActionState(UseAiAction, { message: ""} );
+    const [ title, setTitle ] = useState<string>("Untitled table");
+    const [ times, setTimes ] = useState<number>(rows && rows.length ? rows.length : 0);
+
+    useEffect(() => {
+        setTimes(prevCount => {
+            if (rows && rows.length) {
+                if (prevCount === rows.length) {
+                    return rows.length;
+                }
+                return prevCount;
+            }
+            return 0;
+        });
+    }, [rows])
     
+    useEffect(() => {
+        if (aiState.response && aiState.response.result) {
+            if (setRows) {
+                setRows(aiState.response.result);
+            }
+        }
+    }, [aiState])
+
     return (
         <form id={id ? id : "try"} name={id ? id : "try"}>
-            <input type="text" name="table_title" id="table_title" defaultValue="Untitled table" />
+            <input type="text" name="user_id" id="user_id" value={id ? id : ""} hidden readOnly />
+            <input type="text" name="user_email" id="user_email" value={session?.user?.email ? session.user.email : ""} hidden readOnly />
+            <input type="text" name="table_id" id="table_id" value={params && params.id ? params.id : ""} hidden readOnly />
             
-            <XTable id={id} rows={rows} setRows={setRows} />
+            <input type="text" name="table_title" id="table_title" value={title} onChange={e => {
+                e.preventDefault();
+                setTitle(e.target.value);
+            }}/>
+            
+            <XTable rows={rows} setRows={setRows} values={values}/>
             { children }
-            <SubmitButton text={savePending ? "Saving..." : "Save"} formaction={saveAction} disabled={savePending} />
+            {
+                session?.user ? 
+                <SubmitButton text={savePending ? "Saving..." : "Save"} formaction={saveAction} disabled={savePending} /> 
+                : null
+            }
+
+            <label>
+                Fill each column with at least <input type="number" name={`Specification[]-fill-all-columns-with-at-least-${times}-values`} max={rows && rows.length} value={times} onChange={e => {
+                    e.preventDefault();
+                    setTimes(parseInt(e.target.value, 10) || 0);
+                }}/> times.
+            </label>
+
+            <SubmitButton text={aiPending ? "Creating..." : "Create"} formaction={aiAction} disabled={aiPending} />
         </form>
     )
 }
