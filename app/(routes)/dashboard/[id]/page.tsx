f94b92664ca34904952bb30c24e6b1f06d0dfc19
Jossy Solá <jossysola@gmail.com>
2025-01-16 11:29:09 -0600

    Basic setup for /dashboard/[id] route

--- app/(routes)/dashboard/[id]/page.tsx
+++ app/(routes)/dashboard/[id]/page.tsx
@@ -1,6 +1,16 @@
-export default async function Page () {
+import XPanel from "@/app/ui/organisms/org-XPanel";
+import { auth } from "@/auth";
+
+export default async function Page ({ params }: {
+    params: Promise<{ id: string }>
+}) {
+    const id = (await params).id;
+    const session = await auth();
+
+    if (session?.user) {
+        return <XPanel id={id} />
+    }
     return (
-        <>
-        </>
+        <p>Unauthenticated</p>
     )
 }
