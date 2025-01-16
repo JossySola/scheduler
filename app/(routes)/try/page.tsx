35701778929548cb8755b8c3c536a4f43f13c5b6
Jossy Solá <jossysola@gmail.com>
2025-01-15 22:06:14 -0600

    Removes params await

--- app/(routes)/try/page.tsx
+++ app/(routes)/try/page.tsx
@@ -1,13 +1,9 @@
 import XPanel from "@/app/ui/organisms/org-XPanel";
 
-export default async function Page ({
-    params,
-}: { params: Promise<{ id: string }>}) {
-    const { id } = await params;
-    
+export default async function Page () {
     return (
         <>
-        <XPanel id={id} />
+        <XPanel />
         </>
     )
 }
