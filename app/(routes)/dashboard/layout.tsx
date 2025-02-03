194bdb71634bb18db0adccb5e987bebb9e3ff433
Jossy Solá <jossysola@gmail.com>
2025-02-02 18:51:52 -0600

    Creates layout to display parallel routes

---
+++ app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+export default function Layout ({
+    children,
+    list
+}: {
+    children: React.ReactNode,
+    list: React.ReactNode,
+}) {
+    return (
+        <>
+        { children }
+        { list }
+        </>
+    )
+}
