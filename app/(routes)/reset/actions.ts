e6dcadd6ff2e2b46574e6456b37fe6b384ac4a42
Jossy Solá <jossysola@gmail.com>
2025-02-07 17:07:13 -0600

    Refactors Server Action

--- app/(routes)/reset/actions.ts
+++ app/(routes)/reset/actions.ts
@@ -1,78 +1,91 @@
 "use server"
 import "server-only";
 import { isPasswordPwned } from "@/app/lib/utils";
+import { auth, signOut } from "@/auth";
+import { Argon2id } from "oslo/password";
+import pool from "@/app/lib/mocks/db";
 
-export async function handleTokenConfirmation(token: string | undefined, email: string | undefined) {
-    console.error("[handleTokenConfirmation] Starting...")
-    console.error("[handleTokenConfirmation] Checking necessary data...")
-    if (!token || !email) {
-        console.error("[handleTokenConfirmation] Data missing, exiting...")
-        return {
-            status: 400,
-            statusText: 'You must provide the token'
-        }
-    }
-    console.error("[handleTokenConfirmation] Fetching from /api/verify/reset...")
-    const confirm = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/verify/reset`, {
-        method: 'POST',
-        body: JSON.stringify({
-            action: 'verify-token',
-            email,
-            verification_token: token
-        })
-    })
-    console.error("[handleTokenConfirmation] Fetch result:", confirm)
-    console.error("[handleTokenConfirmation] Exiting...")
-    return await confirm.json();
-}
+export async function passwordResetAction(prevState: { message: string }, formData: FormData) {
+    const password = formData.get("password")?.toString();
+    const confirmation = formData.get("confirm-password")?.toString();
+    const token = formData.get("token")?.toString();
+    const session = await auth();
+    const email = session?.user?.email;
 
-export async function handlePasswordReset(password: string | undefined, confirm: string | undefined, email: string | undefined) {
-    console.log("[handlePasswordReset] Starting...")
-    console.log("[handlePasswordReset] Checking necessary data...")
-    if (!password || !confirm || !email) {
-        console.log("[handlePasswordReset] Data missing, exiting...")
+    if (!password || !confirmation) {
         return {
-            status: 400,
-            statusText: 'Please provide a password and the confirmation'
-        }
-    }
-
-    if (password !== confirm) {
-        console.log("[handlePasswordReset] Password confirmation failed between the two provided, exiting...")
-        return {
-            status: 400,
-            statusText: 'Please, confirm the password correctly'
+            message: "Please fill out both input fields."
         }
     }
 
     if (password.length < 8) {
-        console.log("[handlePasswordReset] Password is less than 8 characters, exiting...")
         return {
-            status: 400,
-            statusText: 'The password must have at least 8 characters'
+            message: "Your password must have at least 8 characters."
         }
     }
-    console.log("[handlePasswordReset] Calling isPasswordPwned...")
-    const exposureCheck = await isPasswordPwned(password);
-    console.log("[handlePasswordReset] Function result:", exposureCheck)
 
-    if (exposureCheck !== 0) {
-        console.log("[handlePasswordReset] Function result is a number greater than 0, exiting...")
+    if (password !== confirmation || !token) {
         return {
-            status: 400,
-            statusText: 'Upon a password verification, unfortunately this password has been exposed in a data breach. For security reasons, please choose another password.'
+            message: "Password confirmation failed."
         }
     }
-    console.log("[handlePasswordReset] Fetching from /api/verify/reset...")
-    const reset = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/verify/reset`, {
-        method: 'POST',
-        body: JSON.stringify({
-            action: 'password-reset',
-            password: password,
-            email
-        })
-    })
-    console.log("[handlePasswordReset] Fetch result:", reset)
-    console.log("[handlePasswordReset] Exiting...")
-    return await reset.json();
+
+    const argon = new Argon2id();
+    const oldPassword = await pool.query(`
+       SELECT password FROM scheduler_users
+       WHERE email = $1; 
+    `, [email]);
+    if (oldPassword.rowCount === 0) {
+        return {
+            message: "User not found"
+        }
+    }
+    const comparison = await argon.verify(oldPassword.rows[0].password, password);
+    if (comparison) {
+        return {
+            message: "New password cannot be the same as the old password"
+        }
+    }
+
+    const exposed = await isPasswordPwned(password);
+    if (typeof exposed === 'number' && exposed > 0) {
+        return {
+            message: "After a password checkup, it appears this password has been exposed in a data breach in the past. Please use a stronger password."
+        }
+    }
+    
+    const hash: string = await argon.hash(password);
+
+    const query = await pool.query(`
+       UPDATE scheduler_users
+       SET password = $1
+       WHERE email = $2; 
+    `, [hash, email]);
+    if (query.rowCount === 0) {
+        return {
+            message: "Password reset failed"
+        }
+    }
+
+    const invalidate = await pool.query(`
+        UPDATE scheduler_password_resets
+        SET used_at = NOW()
+        WHERE email = $1 AND token = $2
+        RETURNING used_at;
+    `, [email, token]);
+
+    if (invalidate.rowCount === 0) {
+        return {
+            message: "Failed to invalidate token"
+        }
+    }
+
+    await signOut({
+        redirect: true,
+        redirectTo: "/login"
+    });
+
+    return {
+        message: "Reset successful"
+    }
 }
