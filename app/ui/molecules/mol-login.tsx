d323cd9a100bca97353cc3d70029864fd3cf4acc
Jossy Solá <jossysola@gmail.com>
2025-02-08 17:22:31 -0600

    Implements code to include timer when there are failed attempts to log in

--- app/ui/molecules/mol-login.tsx
+++ app/ui/molecules/mol-login.tsx
@@ -1,13 +1,32 @@
 "use client"
-import { useActionState } from "react";
+import { useActionState, useEffect, useState } from "react";
 import { SubmitButton } from "../atoms/atom-button";
 import Password from "../atoms/atom-password";
 import Link from "next/link";
 import { LogInAction } from "@/app/(routes)/login/actions";
 import PasswordResetButton from "../atoms/atom-button-password-reset";
+import CountdownTimer from "../atoms/atom-timer-attempt";
 
 export default function LogIn () {
-    const [loginState, loginAction, pending] = useActionState(LogInAction, { message: "" });
+    const [ loginState, loginAction, pending ] = useActionState(LogInAction, { message: "" });
+    const [ message, setMessage ] = useState<string>("");
+    const [ timestamp, setTimestamp ] = useState<string>("");
+
+    useEffect(() => {
+        const split = loginState.message.split('/');
+        if (split) {
+            split.forEach((text, index) => {
+                if (index === 0 && typeof text === "string") {
+                    setMessage(text)
+                }
+                if (typeof text === "string" && text.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)) {
+                    setTimestamp(text);
+                }
+            })
+        }
+    }, [loginState]);
+    
+    
     return (
         <>
             <form action={loginAction}>
@@ -20,8 +39,11 @@ export default function LogIn () {
                     <Password />
                 </label>
                 <PasswordResetButton />
-                <SubmitButton text="Log In" disabled={pending} />
-                <p aria-live="polite">{loginState.message}</p>
+
+                <p aria-live="polite">{message}</p>
+                <CountdownTimer nextAttempt={timestamp} />
+                <SubmitButton text="Login" disabled={pending} />
+                
             </form>
 
             <p>If you don't have an account yet, <Link href={"/signup"}>Signup</Link></p>
