4b56d04983e467900ae4d9db51f5e57854df2998
Jossy Solá <jossysola@gmail.com>
2025-02-10 12:58:23 -0600

    Deletes root Page component as it was interferring with internationalization

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export const experimental_ppr = true;
-
-export default function App () {
-  return (
-      <div className="items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1>Root Page.tsx</h1>
-      </div>
-  );
-}
