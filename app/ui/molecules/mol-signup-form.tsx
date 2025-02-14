1a8c70f9c9bd661e60847f99534f1a65142739b3
Jossy Solá <jossysola@gmail.com>
2025-02-13 20:27:20 -0600

    Sets styles and includes <SignInProviders> component

--- app/ui/molecules/mol-signup-form.tsx
+++ app/ui/molecules/mol-signup-form.tsx
@@ -2,8 +2,8 @@
 import { useState } from "react";
 import BasicInformation from "./mol-signup-information";
 import ConfirmEmail from "./mol-confirm-email";
-import Link from "next/link";
 import { useParams } from "next/navigation";
+import SignInProviders from "./mol-signin-providers";
 
 export default function SignupForm () {
     const [ validated , setValidated ] = useState<boolean>(false);
@@ -11,14 +11,14 @@ export default function SignupForm () {
     const { lang } = params;
 
     return (
-        <>
-            <form name="register" id="register">
-                <BasicInformation validated={validated} setValidated={setValidated} />
+            <form name="register" id="register" className="w-screen sm:w-[400px] p-3 flex flex-col items-center justify-center">
+                <h2 className="tracking-tight">{ lang === "es" ? "Regístrate" : "Create an account"}</h2>
+                <BasicInformation validated={ validated} setValidated={ setValidated } />
                 {
-                    validated ? <ConfirmEmail /> : <p>{ lang === "es" ? "o si ya tienes una cuenta, " : "Or if you already have an account, "}<Link href={"/login"}>{ lang === "es" ? "Inicia Sesión" : "Login" }</Link></p>
+                    validated ? 
+                    <ConfirmEmail /> : 
+                    lang && <SignInProviders lang={ lang.toString() } />
                 }
             </form>
-        </>
-        
     )
 }
