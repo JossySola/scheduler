b8c158723d0ce6d9c02154c4ca136aef2b536540
Jossy Solá <jossysola@gmail.com>
2025-02-13 20:24:18 -0600

    Implements <Button> component from @heroui/react library

--- app/ui/atoms/atom-login-button.tsx
+++ app/ui/atoms/atom-login-button.tsx
@@ -1,12 +1,19 @@
 "use client"
-import Link from "next/link"
+import { Button, Link } from "@heroui/react";
 import { useParams } from "next/navigation"
+import { SignIn } from "geist-icons";
 
 export default function LogInButton () {
     const params = useParams();
     const { lang } = params;
 
     return (
-        <Link href={`/${lang}/login`}>{ lang === "es" ? "Iniciar sesión" : "Log in"}</Link>
+        <Button
+        className="bg-transparent border-2"
+        as={Link}
+        endContent={<SignIn />}
+        href={`/${lang}/login`}>
+            { lang === "es" ? "Iniciar sesión" : "Log in"}
+        </Button>
     )
 }
--- app/ui/atoms/atom-signup-button.tsx
+++ app/ui/atoms/atom-signup-button.tsx
@@ -1,12 +1,17 @@
 "use client"
-import Link from "next/link"
-import { useParams } from "next/navigation"
+import { Button, Link } from "@heroui/react";
+import { useParams } from "next/navigation";
 
 export default function SignUpButton () {
     const params = useParams();
     const { lang } = params;
 
     return (
-        <Link href={`${lang}/signup`}>{ lang === "es" ? "Regístrate" : "Sign Up" }</Link>
+        <Button
+        className="bg-gradient-to-tr from-violet-600 to-blue-500 text-white shadow-lg"
+        as={Link}
+        href={`/${lang}/signup`}>
+            { lang === "es" ? "Regístrate" : "Sign Up" }
+        </Button>
     )
 }
