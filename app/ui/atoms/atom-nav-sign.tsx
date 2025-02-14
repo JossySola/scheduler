ccdbc4b4c627df9eae3d70bdf51c8cd97a818200
Jossy Solá <jossysola@gmail.com>
2025-02-13 20:25:09 -0600

    Implements <Navbar> component from @heroui/react library

--- app/ui/atoms/atom-nav-sign.tsx
+++ app/ui/atoms/atom-nav-sign.tsx
@@ -2,19 +2,45 @@ import LogOutButton from "./atom-logout-button";
 import LogInButton from "./atom-login-button";
 import SignUpButton from "./atom-signup-button";
 import { auth } from "@/auth";
+import {
+    Navbar, 
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+} from "@heroui/navbar";
+import { SchedulerBlack, SchedulerWhite } from "./atom-branding";
+
 
 export default async function SignNav () {
     const session = await auth();
 
     return (
-        <nav>
+        <Navbar className="bg-transparent w-screen pt-2" shouldHideOnScroll>
+            <NavbarContent justify="start">
+                <NavbarBrand className="hidden dark:inline-block">
+                    <SchedulerWhite />
+                </NavbarBrand>
+                <NavbarBrand className="dark:hidden">
+                    <SchedulerBlack />
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent justify="end">
             {
-                session?.user ? <LogOutButton /> : (
+                session?.user ? 
+                <NavbarItem>
+                    <LogOutButton />
+                </NavbarItem> : (
                 <>
+                <NavbarItem>
                     <LogInButton  />
+                </NavbarItem>
+                <NavbarItem>
                     <SignUpButton />
+                </NavbarItem>
                 </>)
             }
-        </nav>
+            </NavbarContent>
+        </Navbar>
     )
 }
