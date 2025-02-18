89059b7a2ed6cf431a6d9d00fb7c868bca25e6af
Jossy Solá <jossysola@gmail.com>
2025-02-18 00:20:22 -0600

    Deletes routes folder

--- app/(routes)/delete-my-data/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page () {
-    return (
-        <section>
-            <h1>How do I delete my data?</h1>
-        </section>
-    )
-}
--- app/(routes)/terms/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page () {
-    return (
-        <section>
-            <h1>Terms and Conditions</h1>
-        </section>
-    )
-}
