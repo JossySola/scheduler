e9f1c539a114adfbcc441060a45087c5633ec811
Jossy Solá <jossysola@gmail.com>
2025-02-18 00:23:42 -0600

    Moves SendResetEmailAction to /recover/actions.ts

--- app/[lang]/login/actions.ts
+++ app/[lang]/login/actions.ts
@@ -1,9 +1,6 @@
 "use server"
 import "server-only";
-import { auth, signIn } from "@/auth";
-import pool from "@/app/lib/mocks/db";
-import { randomUUID } from "crypto";
-import sgMail from "@sendgrid/mail";
+import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
 import { headers } from "next/headers";
 
@@ -19,7 +16,7 @@ export async function LogInAction (prevState: { message: string }, formData: For
     if (!formData || !username || !password) {
         console.log("[LogInAction] There is data missing...")
         return {
-            message: "Data is missing/"
+            message: locale === "es" ? "Por favor, llena todos los campos/" : "Please, fill out both fields/"
         }
     }
     
@@ -40,7 +37,7 @@ export async function LogInAction (prevState: { message: string }, formData: For
         console.log("[LogInAction] Entering catch block...")
         console.log("[LogInAction] Error", error)
 
-        let errorMessage = "Unknown error/";
+        let errorMessage = locale === "es" ? "Error interno/" : "Unknown error/";
         if (error instanceof Error && error.message !== "NEXT_REDIRECT") {
             if (error instanceof AuthError) {
                 errorMessage = `${error.message}/`;
@@ -53,81 +50,5 @@ export async function LogInAction (prevState: { message: string }, formData: For
             return { message: `${errorMessage}/` };
         }
         throw error;
-    }
-}
-
-export async function SendResetEmailAction () {
-    const session = await auth();
-    const email = session?.user?.email;
-
-    if (!email) {
-        return {
-            ok: false,
-            message: "Session missing"
-        }
-    };
-
-    const query = await pool.query(`
-       SELECT EXISTS (
-        SELECT 1 FROM scheduler_users WHERE email = $1
-       );
-    `, [email]);
-    const userExists = query.rows[0].exists;
-
-    if (!userExists) {
-        return {
-            ok: false,
-            message: "User not found"
-        }
-    };
-
-    const token = randomUUID();
-    const url = new URL(`${process.env.NEXT_PUBLIC_ORIGIN}/reset/q`);
-    url.searchParams.append('email', email);
-    url.searchParams.append('token', token);
-
-    const message = {
-        to: `${email}`,
-        from: 'no-reply@jossysola.com',
-        subject: 'Scheduler: Reset Password Request',
-        html: `
-        <html>
-            <body>
-                <a href="${url.toString()}">Reset Password</a>
-            </body>
-        </html>
-        `,
-    }
-
-    try {
-       process.env.SENDGRID_API_KEY && sgMail.setApiKey(process.env.SENDGRID_API_KEY); 
-        const query = await pool.query(`
-           INSERT INTO scheduler_password_resets
-           (email, token, expires_at) 
-           VALUES ($1, $2, CURRENT_TIMESTAMP + INTERVAL '3 minutes');
-        `, [email, token]);
-        if (query.rowCount === 0) {
-            throw new Error("Failed insertion");
-        }
-
-        const sending = await sgMail.send(message);
-        if (sending[0].statusCode !== 202) {
-            throw new Error("Failed sending email");
-        }
-        return {
-            ok: true,
-            message: "Email sent!"
-        }
-    } catch (e) {
-        if (e instanceof Error) {
-            return {
-                ok: false,
-                message: e.message,
-            }
-        }
-        return {
-            ok: false,
-            message: "Unknown error"
-        }
     }
 }
