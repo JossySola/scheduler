a555e20f6b1fe454d16ff5d00e3c223ab0634acd
Jossy Solá <jossysola@gmail.com>
2025-02-18 00:38:35 -0600

    Imports and uses <PrimaryButtonAsLink>

--- app/ui/atoms/atom-signup-button.tsx
+++ app/ui/atoms/atom-signup-button.tsx
@@ -1,17 +1,14 @@
 "use client"
-import { Button, Link } from "@heroui/react";
 import { useParams } from "next/navigation";
+import { PrimaryButtonAsLink } from "./atom-button";
 
 export default function SignUpButton () {
     const params = useParams();
     const { lang } = params;
 
     return (
-        <Button
-        className="bg-gradient-to-tr from-violet-600 to-blue-500 text-white shadow-lg"
-        as={Link}
-        href={`/${lang}/signup`}>
+        <PrimaryButtonAsLink link={`/${lang}/signup`}>
             { lang === "es" ? "Regístrate" : "Sign Up" }
-        </Button>
+        </PrimaryButtonAsLink>
     )
 }
