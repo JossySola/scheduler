aa63771796647c4a1bcecce7cb3ce8f59dcc59ff
Jossy Solá <jossysola@gmail.com>
2025-02-18 23:33:02 -0600

    Updates ActionButton version

--- app/ui/atoms/atom-form-button-validation.tsx
+++ app/ui/atoms/atom-form-button-validation.tsx
@@ -3,11 +3,11 @@ import { validateAction } from "@/app/[lang]/signup/actions";
 import { SetStateAction, useActionState, useEffect } from "react";
 import { ActionButton } from "./atom-button";
 
-export default function FormButtonValidation ({
-    text,
-    formName,
-    setValidated
-} : { text: string, formName: string, setValidated: React.Dispatch<SetStateAction<boolean>> }) {
+export default function FormButtonValidation ({ text, setValidated, endContent }: { 
+    text: string,
+    setValidated: React.Dispatch<SetStateAction<boolean>>, 
+    endContent?: React.JSX.Element 
+    }) {
     const [ nextState, nextAction, pending ] = useActionState(validateAction, { message: '', passes: false, descriptive: []})
 
     useEffect(() => {
@@ -22,13 +22,21 @@ export default function FormButtonValidation ({
                 nextState.descriptive?.map((description, index) => {
                     if (description) {
                         if (description.error) {
-                            return <p key={index}>{description.error.issues[0].message}</p>
+                            return <p key={index} className="text-danger">{description.error.issues[0].message}</p>
                         }
                     }
-                    return <p key={index}>{nextState.message}</p>;
+                    return <p key={index} className="text-red-500">{nextState.message}</p>;
                 })
             }
-            <ActionButton text={text} type="submit" action="signup_registration" form={formName} formaction={nextAction} disabled={pending}/>
+            <ActionButton
+            type="submit"
+            form="register"
+            formAction={nextAction} 
+            disabled={pending}
+            loading={pending}
+            endContent={endContent}>
+                { text }
+            </ActionButton>
         </>
     )
 }
