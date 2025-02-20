f4f081da70b331299058ca44299acb8ea04fc2f7
Jossy Solá <jossysola@gmail.com>
2025-02-20 00:24:55 -0600

    Creates component

---
+++ app/ui/molecules/mol-delete-table-modal.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useParams } from "next/navigation"
+import { ActionButton, SecondaryButton } from "../atoms/atom-button"
+import { Form, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@heroui/react";
+import { Trash } from "geist-icons";
+import { DeleteTableAction } from "@/app/[lang]/dashboard/@list/actions";
+
+export default function DeleteTableModal ({ table_id }: {
+    table_id: string
+}) {
+    const params = useParams();
+    const lang = params.lang;
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    return (
+        <>
+        <ActionButton onPress={onOpen} color="danger" endContent={<Trash width="16px" />}>{ lang === "es" ? "Borrar" : "Delete"}</ActionButton>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+            <ModalContent>
+                {onClose => (
+                    <>
+                    <ModalHeader className="flex flex-col gap-1 text-center">
+                        <h3>{ lang === "es" ? "Eliminar tabla" : "Delete schedule" }</h3>
+                    </ModalHeader>
+                    <ModalBody className="flex flex-col justify-center items-center">
+                        <Form action={DeleteTableAction} className="m-5 flex flex-col items-center">
+                            <p>{ lang === "es" ? "Confirma esta acción, ya que será irreversible" : "Confirm this action as this will be irreversible" }</p>
+                            <input value={table_id} name="item_id" readOnly hidden />
+                            <ActionButton type="submit" color="danger">{ lang === "es" ? "Eliminar" : "Delete" }</ActionButton>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <SecondaryButton onPress={onClose}>{ lang === "es" ? "Cancelar" : "Cancel" }</SecondaryButton>
+                    </ModalFooter>
+                    </>
+                )}
+            </ModalContent>
+        </Modal>
+        </>
+    )
+}
