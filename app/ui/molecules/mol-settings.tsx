624bad504ad16768704396532c056adf1a072f60
Jossy Solá <jossysola@gmail.com>
2025-02-20 00:25:25 -0600

    Refactors and implements <Modal> from @heroui/react

--- app/ui/molecules/mol-settings.tsx
+++ app/ui/molecules/mol-settings.tsx
@@ -1,16 +1,51 @@
-import { auth } from "@/auth";
-import DisconnectProviders from "./mol-disconnect-providers";
+"use client"
 import DangerButton from "../atoms/atom-danger-button";
+import DisconnectGoogle from "../atoms/atom-disconnect-google";
+import DisconnectFacebook from "../atoms/atom-disconnect-facebook";
+import { ActionButton, SecondaryButton } from "../atoms/atom-button";
+import { redirect } from "next/navigation";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@heroui/react";
+import { SettingsGearFill } from "geist-icons";
 
-export default async function Settings () {
-    const session = await auth();
-// reset password
-    if (session?.user) {
-        return (
-            <section>
-                <DisconnectProviders />
-                <DangerButton />
-            </section>
-        )
-    }
+export default function Settings ({ lang, data }: {
+    lang: string,
+    data: Array<{ provider: string }>
+}) {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    return (
+        <>
+            <Button isIconOnly aria-label={ lang === "es" ? "Ajustes" : "Settings"} onPress={onOpen}>
+                <SettingsGearFill />
+            </Button>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {onClose => (
+                        <>
+                        <ModalHeader className="flex flex-row items-center justify-center">
+                            <h3 className="mr-3 text-center">{ lang === "es" ? "Ajustes de Cuenta" : "Account Settings"}</h3>
+                        </ModalHeader>
+                        <ModalBody className="flex flex-col items-center">
+                            <ActionButton onPress={() => redirect(`/${lang}/recover`)}>{ lang === "es" ? "Restaurar contraseña" : "Reset password" }</ActionButton>
+                            {
+                                data && data.map((row: { provider: string }) => {
+                                    if (row.provider === "Google") {
+                                        return <DisconnectGoogle key={row.provider} />
+                                    }
+                                    if (row.provider === "Facebook") {
+                                        return <DisconnectFacebook key={row.provider} />
+                                    }
+                                })
+                            }
+                            <DangerButton />
+                        </ModalBody>
+                        <ModalFooter>
+                            <SecondaryButton onPress={onClose}>{ lang === "es" ? "Cerrar" : "Close" }</SecondaryButton>
+                        </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    )
 }
