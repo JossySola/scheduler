097b0f282fa8e4826f56788ca4d7484e8bfbc67b
Jossy Solá <jossysola@gmail.com>
2025-02-23 00:16:43 -0600

    Accepts table_name as prop

--- app/ui/molecules/mol-delete-table-modal.tsx
+++ app/ui/molecules/mol-delete-table-modal.tsx
@@ -5,8 +5,9 @@ import { Form, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisc
 import { Trash } from "geist-icons";
 import { DeleteTableAction } from "@/app/[lang]/dashboard/@list/actions";
 
-export default function DeleteTableModal ({ table_id }: {
-    table_id: string
+export default function DeleteTableModal ({ table_id, table_name }: {
+    table_id: string,
+    table_name: string,
 }) {
     const params = useParams();
     const lang = params.lang;
@@ -23,6 +24,7 @@ export default function DeleteTableModal ({ table_id }: {
                         <h3>{ lang === "es" ? "Eliminar tabla" : "Delete schedule" }</h3>
                     </ModalHeader>
                     <ModalBody className="flex flex-col justify-center items-center">
+                        <h3 className="m-0">{ table_name }</h3>
                         <Form action={DeleteTableAction} className="m-5 flex flex-col items-center">
                             <p>{ lang === "es" ? "Confirma esta acción, ya que será irreversible" : "Confirm this action as this will be irreversible" }</p>
                             <input value={table_id} name="item_id" readOnly hidden />
