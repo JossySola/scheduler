c3d0d6c51477c72bb74cec59ee3f16bb93498730
Jossy Solá <jossysola@gmail.com>
2025-02-23 00:19:08 -0600

    Replaces native HTML Form elements with HeroUI components

--- app/ui/molecules/mol-XItem.tsx
+++ app/ui/molecules/mol-XItem.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Checkbox, CheckboxGroup, Divider, NumberInput, Switch } from "@heroui/react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -34,7 +35,9 @@ export default function XItem ({ name, preferences, criteria = [], values = []} 
         }
         return false;
     });
-    const [ enabledColumns, setEnabledColumns ] = useState<Array<boolean>>(criteria.map(value => {
+    const [ enabledColumns, setEnabledColumns ] = useState<Array<string>>([]);
+    const [ enabledValues, setEnabledValues ] = useState<Array<string>>([]);
+    /*const [ enabledColumns, setEnabledColumns ] = useState<Array<boolean>>(criteria.map(value => {
         if (preferences) {
             const result = preferences.find(subArray => {
                 return (subArray[0].trim() === `Specification:Row-${name}-should-be-used-on` && subArray[1].trim() === value.trim())
@@ -44,8 +47,8 @@ export default function XItem ({ name, preferences, criteria = [], values = []} 
             }
         }
         return false;
-    }));
-    const [ enabledValues, setEnabledValues ] = useState<Array<boolean>>(values.map(value => {
+    }));*/
+    /*const [ enabledValues, setEnabledValues ] = useState<Array<boolean>>(values.map(value => {
         if (preferences) {
             const result = preferences.find(subArray => {
                 return (subArray[0].trim() === `Specification:Row-${name}-use-this-value-specifically` && subArray[1].trim() === value.trim())
@@ -55,79 +58,53 @@ export default function XItem ({ name, preferences, criteria = [], values = []} 
             }
         }
         return false;
-    }));
+    }));*/
     
     useEffect(() => {
         setCount(prevCount => (prevCount === criteria.length ? criteria.length : prevCount));
     }, [criteria]);
     
     return (
-        <details>
-            <summary>{name}</summary>
-            <label>
-                { lang === "es" ? "Deshabilitar en todas las columnas:" : "Disable on all columns:" }
-                <input 
-                type="radio" 
-                name={`Specification:disable-Row-${name}-on-table`} 
-                value="Yes" 
-                checked={disable} 
-                onChange={() => setDisable(prev => !prev)}/>
-            </label>
+        <section>
+            <h3 className="text-center">{ lang === "es" ? `Especificaciones de la fila "${name}"` : `"${name}" row criteria` }</h3>
+            <Switch className="m-4" color="danger" isSelected={disable} onValueChange={setDisable} name={`Specification:disable-Row-${name}-on-table`}>
+                { lang === "es" ? "Deshabilitar en todas las columnas" : "Disable on all columns" }
+            </Switch>
 
-            <fieldset>
-                <legend>{ lang === "es" ? "Habilitar solo en ciertas columnas:" : "Enable/disable in certain columns:" }</legend>
+            <CheckboxGroup
+            className="m-4"
+            label={ lang === "es" ? "Habilitar solo en ciertas columnas:" : "Enable/disable on certain columns:" }
+            value={enabledColumns}
+            onValueChange={setEnabledColumns}>
                 {
                     criteria && criteria.map((variable, index) => {
-                        return <label key={`${variable}-${index}`}>
-                            <input 
-                            type="checkbox" 
-                            name={`Specification:Row-${name}-should-be-used-on`} 
-                            value={variable} 
-                            checked={!!enabledColumns[index]}
-                            onChange={(() => {
-                                setEnabledColumns(prev => 
-                                    prev.map((col, colIndex) => colIndex === index ? !col : col )
-                                )
-                            })}/>
-                            {variable}
-                        </label>
+                        return <Checkbox key={`${variable}${index}`} name={`Specification:Row-${name}-should-be-used-on`} value={variable}>{ variable }</Checkbox>
                     })
                 }
-            </fieldset>
+            </CheckboxGroup>
 
-            <label>
-                { lang === "es" ? "¿Cuántas veces debería ser rellenado?" : "How many times it should appear?" }
-                <input 
-                type="number" 
-                name={`Specification:Row-${name}-should-appear-only-this-amount-of-times`} 
-                min={0}
-                max={criteria.length} 
-                value={count} 
-                onChange={e => {
-                    setCount(parseInt(e.target.value, 10) || 0);
-                }}/>
-            </label>
+            <NumberInput 
+            label={ lang === "es" ? "¿Cuántas veces debería ser en la tabla?" : "How many times it should appear on the schedule?" }
+            labelPlacement="outside-left"
+            value={count}
+            minValue={0}
+            maxValue={criteria.length}
+            onValueChange={setCount}/>
             
-            <fieldset>
-                <legend>{ lang === "es" ? "Preferir estos valores:" : "Prefer the following values:"}</legend>
+            <CheckboxGroup
+            className="m-4"
+            label={ lang === "es" ? "Preferir usar estos valores en la fila:" : "Prefer the following values to use in this row:"}
+            value={enabledValues}
+            onValueChange={setEnabledValues}>
                 {
-                    values && values.map((value, index) => {
-                        return <label key={`${value}-${index}`}>
-                            <input 
-                            type="checkbox" 
-                            name={`Specification:Row-${name}-use-this-value-specifically`} 
-                            value={value} 
-                            checked={!!enabledValues[index]}
-                            onChange={(() => {
-                                setEnabledValues(prev =>
-                                    prev.map((col, colIndex) => colIndex === index ? !col : col )
-                                )
-                            })}/>
-                            {value}
-                        </label>
+                    values && values.map((variable, index) => {
+                        return <Checkbox 
+                        key={`${variable}${index}`} 
+                        name={`Specification:Row-${name}-use-this-value-specifically`} value={ variable }>{ variable }</Checkbox>
                     })
                 }
-            </fieldset>
-        </details>
+            </CheckboxGroup>
+            <Divider />
+        </section>
     )
 }
