8f4d35736c425543d8195088fecadee8036da44c
Jossy Solá <jossysola@gmail.com>
2025-02-28 22:12:37 -0600

    Passes lang prop

--- app/ui/molecules/mol-signin-providers.tsx
+++ app/ui/molecules/mol-signin-providers.tsx
@@ -2,13 +2,13 @@ import FacebookSignIn from "../atoms/atom-facebook-signin";
 import GoogleSignIn from "../atoms/atom-google-signin";
 
 export default function SignInProviders ({ lang }: {
-    lang: string
+    lang: "en" | "es"
 }) {
     return (
         <section className="w-full p-3 sm:w-[400px] flex flex-col justify-center items-center text-center">
             <p className="text-sm text-[#a1a1aa] m-2">{ lang === "es" ? "O inicia sesión con cualquiera de los siguientes proveedores:" : "Or signin with your provider" }</p>
-            <GoogleSignIn />
-            <FacebookSignIn />
+            <GoogleSignIn lang={ lang } />
+            <FacebookSignIn lang={ lang } />
         </section>
     )
 }
