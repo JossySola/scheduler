e408d8e048dec850d539e16ac7724fa4897ddc64
Jossy Solá <jossysola@gmail.com>
2025-02-28 22:05:37 -0600

    Adds logic to Page to refactor client-side code

--- app/[lang]/signup/page.tsx
+++ app/[lang]/signup/page.tsx
@@ -1,9 +1,14 @@
-import SignUp from "@/app/ui/organisms/org-signup";
+import SignupForm from "@/app/ui/molecules/mol-signup-form";
 
-export default async function Page () {
-    return (
-        <main>
-            <SignUp />
-        </main>
-    )
+export default async function Page ({ params }: {
+    params: Promise<{ lang: string }>
+}) {
+    const lang = (await params).lang;
+
+    return <main className="flex flex-col justify-center items-center pt-5">
+        <h2 className="tracking-tight">{ lang === "es" ? "Regístrate" : "Create an account" }</h2>
+        {
+            <SignupForm lang={ lang as "en" | "es" } />
+        }
+    </main>
 }
