cf047158e35ca2904e2f472b2362b89f4fdf599d
Jossy Solá <jossysola@gmail.com>
2025-02-28 22:15:45 -0600

    Deletes component

--- app/ui/organisms/org-signup.tsx
+++
@@ -1,9 +0,0 @@
-import SignupForm from "../molecules/mol-signup-form";
-
-export default function SignUp () {
-    return (
-        <section className="flex flex-col justify-center items-center pt-5">
-            <SignupForm />
-        </section>
-    )
-}
