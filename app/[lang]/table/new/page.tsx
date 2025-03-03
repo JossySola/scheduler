a3966b68a916da2b78d885c9aff075c0df67855a
Jossy Solá <jossysola@gmail.com>
2025-03-03 17:09:51 -0600

    Moves TableButtonSave here

--- app/[lang]/table/new/page.tsx
+++ app/[lang]/table/new/page.tsx
@@ -1,10 +1,11 @@
+import TableButtonSave from "@/app/ui/atoms/atom-table-button-save";
 import TableNameInput from "@/app/ui/atoms/atom-table-name-input";
 import YTable from "@/app/ui/molecules/mol-YTable";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function Page ({ params }: {
-    params: Promise<{ lang: string }>
+    params: Promise<{ lang: "en" | "es" }>
 }) {
     const lang = (await params).lang;
     const session = await auth();
@@ -13,7 +14,10 @@ export default async function Page ({ params }: {
         return (
             <main className="w-full mt-10">
                 <form className="flex flex-col justify-center items-center">
+                    <div className="w-full flex flex-row justify-center items-center gap-5">
                     <TableNameInput name={ lang === "es" ? "Sin título" : "No title yet" } />
+                    <TableButtonSave lang={ lang } />
+                    </div>
                     <YTable lang={ lang } />
                 </form>
             </main>
