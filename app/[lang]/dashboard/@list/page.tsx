3eeaaf2c91d7d8a2496d9f0aa2f5ea2775e05e63
Jossy Solá <jossysola@gmail.com>
2025-03-06 18:27:37 -0600

    Accepts params.lang and makes DB query

--- app/[lang]/dashboard/@list/page.tsx
+++ app/[lang]/dashboard/@list/page.tsx
@@ -1,42 +1,22 @@
+import pool from "@/app/lib/mocks/db";
 import DashboardTable from "@/app/ui/molecules/mol-dashboard-table";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function Page ({ params }: {
-    params: Promise<{ lang: string }>,
+    params: Promise<{ lang: "en" | "es" }>,
 }) {
     const session = await auth();
-    const user_email = session?.user?.email ? session.user.email : null;
     const lang = (await params).lang;
-    
-    if (user_email) {
-        const request = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/user/id`, {
-            method: "GET",
-            headers: {
-                user_email
-            }
-        })
-        const response = await request.json();
-        
-        if (response && response.data.user_id) {
-            const request = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/user/tables`, {
-                method: "GET",
-                headers: {
-                    "user_id": response.data.user_id
-                }
-            });
-            const tables = await request.json();
 
-            if (tables.data) {
-                return tables.data && <DashboardTable rows={tables.data} lang={lang as "en" | "es"}/>
-            }
-            return (
-                <p>{ lang === "es" ? "Aún no hay contenido para mostrar" : "No tables yet" }</p>
-            )            
-        }
-        return (
-            <p>User not found</p>
-        )
+    if (session?.user) {
+        const table_data = await pool.query(`
+            SELECT id AS table_id, table_name, updated_at, created_at
+            FROM scheduler_users_tables
+            WHERE user_id = $1;
+        `, [session.user.id]);
+
+        return <DashboardTable rows={ table_data.rows } lang={ lang } />
     }
-    return redirect(`/${lang}/login`);
+    redirect(`/${lang}/login`);
 }
