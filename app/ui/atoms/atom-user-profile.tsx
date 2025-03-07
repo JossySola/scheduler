c8837b751c0d97a82e1c606aa80f0cd8f26fce29
Jossy Solá <jossysola@gmail.com>
2025-03-06 18:36:57 -0600

    Refactors and gives condition on image prop

--- app/ui/atoms/atom-user-profile.tsx
+++ app/ui/atoms/atom-user-profile.tsx
@@ -3,20 +3,7 @@ import UserBadge from "./atom-user-badge";
 
 export default async function UserProfile() {
     const session = await auth();
-    if (!session?.user || !session?.user.email || !session.user.name) return null;
-    
-    const imageRequest = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/user/image`, {
-        method: "GET",
-        headers: {
-            "user_email": session.user.email
-        }
-    });
-
-    const image = (await imageRequest.json()).user_image;
-
-    return (
-        <section className="mb-5">
-            <UserBadge name={session.user.name} image={image} email={session.user.email}/>
-        </section>
-    )
+    if (session && session.user && session.user.name && session.user.email) {
+        return <UserBadge name={ session.user.name } image={ session.user.image ? session.user.image : "" } email={ session.user.email } />
+    }
 }
