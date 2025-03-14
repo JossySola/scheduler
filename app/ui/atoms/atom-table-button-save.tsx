ae06d87d0c49d166f097b94c4493059aab12226c
Jossy Solá <jossysola@gmail.com>
2025-03-14 15:05:47 -0600

    Implements .bind method to include React states into the Server Action along side the FormData

--- app/ui/atoms/atom-table-button-save.tsx
+++ app/ui/atoms/atom-table-button-save.tsx
@@ -1,26 +1,42 @@
 "use client"
-
 import { SaveTableAction } from "@/app/[lang]/table/actions"
-import { useActionState } from "react"
+import { useActionState, useContext, useEffect } from "react"
 import { ActionButton } from "./atom-button";
 import { FloppyDisk } from "geist-icons";
+import { addToast } from "@heroui/react";
+import { TableSpecsContext, TableSpecsType } from "@/app/[lang]/table/context";
 
 export default function TableButtonSave ({ lang }: {
-    lang: "en" | "es"
+    lang: "en" | "es",
 }) {
-    const [ saveState, saveAction, savePending ] = useActionState(SaveTableAction, { message: "" });
+    const { values, colSpecs, specs }: TableSpecsType = useContext(TableSpecsContext);
+    const initialState = {
+        message: "",
+        ok: false,
+    }
+    const boundAction = (state: { message: string, ok: boolean }, formData: FormData) => {
+        return SaveTableAction(state, formData, { values, colSpecs, specs });
+    };
+    const [ saveState, saveAction, savePending ] = useActionState(boundAction, initialState);
+    
+    useEffect(() => {
+        if (saveState.message) {
+            addToast({
+                title: saveState.message,
+                color: saveState.ok ? "success" : "danger",
+            })
+        }
+    }, [saveState]);
 
     return (
-        <>
-        <p aria-live="polite" className="text-danger">{ saveState.message }</p>
         <ActionButton 
         type="submit"
         formAction={ saveAction }
         loading={ savePending }
+        className="absolute top-[-8] right-8 sm:right-[5vw]"
         disabled={ savePending }
         endContent={<FloppyDisk width="16px" />}>
             { lang === "es" ? "Guardar" : "Save" }
         </ActionButton>
-        </>
     )
 }
