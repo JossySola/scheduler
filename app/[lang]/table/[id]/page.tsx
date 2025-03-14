bff4179c243f56e91765c0e7722864caf1f9a793
Jossy Solá <jossysola@gmail.com>
2025-03-14 14:57:21 -0600

    Implements decryptToString function. Uses <TableWithProvider> and passes stored data fetched from database

--- app/[lang]/table/[id]/page.tsx
+++ app/[lang]/table/[id]/page.tsx
@@ -1,15 +1,19 @@
-import TableButtonSave from "@/app/ui/atoms/atom-table-button-save";
 import TableNameInput from "@/app/ui/atoms/atom-table-name-input";
-import YTable from "@/app/ui/molecules/mol-YTable";
+import TableWithProvider from "@/app/ui/molecules/mol-provider-table";
 import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 
 export default async function Page ({ params }: { 
     params: Promise<{ id: string, lang: "en" | "es" }>
 }) {
     const { id, lang } = await params;
     const session = await auth();
-
+    const decryptToString = (decrypt: { type: 'Buffer', data: Array<number> }) => {
+        if (!decrypt) return;
+        const buffer = Buffer.from(decrypt.data);
+        const string = buffer.toString("utf-8");
+        const data = JSON.parse(string);
+        return data;
+    }
     if (session?.user?.id) {
         const storedTable = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/table/${id}`, {
             method: 'GET',
@@ -18,24 +22,19 @@ export default async function Page ({ params }: {
             }
         })
         const response = await storedTable.json();
-        
         return (
-            <main className="w-full mt-10">
-                <form className="flex flex-col justify-center items-center">
-                    <div className="w-full flex flex-row justify-center items-center gap-5">
+            <main className="w-screen mt-10">
+                <form className="flex flex-col justify-center items-center relative">
+                    <input type="text" value={`${id}`} aria-label="table-id" id="table_id" name="table_id" hidden readOnly/>
                     <TableNameInput name={ response.title } />
-                    <TableButtonSave lang={ lang } />
-                    </div>
-                    <YTable 
+                    <TableWithProvider 
                     lang={ lang }
-                    storedRows={ response.table }
-                    storedSpecs={ response.specs }
-                    storedValues={ response.values }
-                    storedColSpecs={ response.cols } 
-                    storedTimestamps={ response.timestamps }/>
+                    storedRows={ decryptToString(response.rows) }
+                    storedSpecs={ decryptToString(response.specs) }
+                    storedValues={ decryptToString(response.values) }
+                    storedColSpecs={ decryptToString(response.colSpecs) } />
                 </form>
             </main>
         )
     }
-    redirect(`/${lang}/login`);
 }
