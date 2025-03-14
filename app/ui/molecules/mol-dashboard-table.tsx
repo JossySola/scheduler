c1264d681ed1bc35251c043e01561b7251553810
Jossy Solá <jossysola@gmail.com>
2025-03-14 15:10:15 -0600

    Creates months Array in both English and Spanish. Uses <Chip> HeroUI component to display different color according to the table limit. Adds the date of creation into the dashboard table

--- app/ui/molecules/mol-dashboard-table.tsx
+++ app/ui/molecules/mol-dashboard-table.tsx
@@ -10,11 +10,13 @@ export default function DashboardTable ({ rows, lang }: {
     rows: Array<{ table_id: string, table_name: string, updated_at: string, created_at: string }>,
     lang: "en" | "es",
 }) {
+    const months = lang === "es" ? ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] : 
+    ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octubre", "November", "December"];
     return (
         <section className="p-8">
             <Button as={HeroLink} href={`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/table/new`} color="success" className="text-white mt-3 mb-3" endContent={<PlusCircle />} disabled={ rows && rows.length === 3 }>{ lang === "es" ? "Crear nuevo" : "Create new"}</Button>
             {
-                rows && rows.length === 0 && <Chip className="m-3" variant="dot" color="success">{ lang === "es" ? `Tabla ${rows.length} de 3` : `Schedule ${rows.length} out of 3` }</Chip>
+                rows && rows.length === 0 || rows.length === 1 && <Chip className="m-3" variant="dot" color="success">{ lang === "es" ? `Tabla ${rows.length} de 3` : `Schedule ${rows.length} out of 3` }</Chip>
             }
             {
                 rows && rows.length === 2 && <Chip className="m-3" variant="dot" color="warning">{ lang === "es" ? `Tabla ${rows.length} de 3` : `Schedule ${rows.length} out of 3` }</Chip>
@@ -25,17 +27,19 @@ export default function DashboardTable ({ rows, lang }: {
             <Table aria-label="Dashboard table" className="shadow-lg">
                 <TableHeader>
                     <TableColumn>{ lang === "es" ? "Nombre" : "Name" }</TableColumn>
-                    <TableColumn>{ lang === "es" ? "Fecha de creación" : "Created at" }</TableColumn>
+                    <TableColumn>{ lang === "es" ? "Fecha de creación" : "Created on" }</TableColumn>
                     <TableColumn>{ lang === "es" ? "Última modificación" : "Last Update" }</TableColumn>
                     <TableColumn>{ lang === "es" ? "Acción" : "Action" }</TableColumn>
                 </TableHeader>
                 <TableBody emptyContent={ lang === "es" ? "No hay nada por mostrar" : "No tables to display" }>
                 {
                     rows && rows.map((row, index) => { 
+                        const created = new Date(row.created_at);
+                        const createdDate = `${months[created.getMonth()]} ${created.getDate()}, ${created.getFullYear()}`
                         return (
                         <TableRow key={`${row.table_name}${index}`}>
                             <TableCell><Link href={`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/table/${row.table_id}`}>{row.table_name}</Link></TableCell>
-                            <TableCell><Timestamp updated_at={row.created_at}/></TableCell>
+                            <TableCell>{createdDate}</TableCell>
                             <TableCell><Timestamp updated_at={row.updated_at}/></TableCell>
                             <TableCell>
                                 <DeleteTableModal table_id={row.table_id} table_name={row.table_name} />
