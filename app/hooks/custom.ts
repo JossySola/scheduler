e9270de5f2b1bc40a5cb7067a4be9880a143d5d6
Jossy Solá <jossysola@gmail.com>
2025-03-14 15:02:05 -0600

    Implements two new custom hooks: useTableSpecs and useTableHandlers

--- app/hooks/custom.ts
+++ app/hooks/custom.ts
@@ -1,5 +1,5 @@
 "use client"
-import { SetStateAction, useEffect, useState } from "react";
+import { SetStateAction, useState } from "react";
 
 export type Specs = {
     disable:boolean,
@@ -7,118 +7,61 @@ export type Specs = {
     enabledValues: Array<string>,
     enabledColumns: Array<string>,
 }
-export function useRows (rows?: string[][], storedValues?: string[], storedSpecs?: Specs[], storedColSpecs?: number[]): [
-    // map
-    string[][],
-    // headers
-    string[],
-    React.Dispatch<SetStateAction<string[]>>,
-    string[],
-    React.Dispatch<SetStateAction<string[]>>,
-    // specs
-    Specs[],
-    number[],
-    // values
-    string[],
-    React.Dispatch<SetStateAction<string[]>>,
-    // handlers
-    () => void,
-    () => void,
-    () => void,
-    () => void,
-] {
+
+export function useTableSpecs () {
     const [ values, setValues ] = useState<Array<string>>([]);
-    const [ specs, setSpecs ] = useState<Array<Specs>>([]);
     const [ colSpecs, setColSpecs ] = useState<Array<number>>([]);
-    const [ colHeaders, setColHeaders ] = useState<Array<string>>([]);
-    const [ rowHeaders, setRowsHeaders ] = useState<Array<string>>([]);
-
-    const [ map, setMap ] = useState<Array<Array<string>>>([]);
-
-    useEffect(() => {
-        if (rows) {
-            setMap(() => rows.map(row => {
-                return row.map(() => {
-                    return "";
-                })
-            }))
-        }
-    }, []);
-
-    useEffect(() => {
-        if (rows) {
-            setColHeaders(rows[0]);
-            setRowsHeaders(rows.map(header => header[0]));
-        };
-    }, []);
-    useEffect(() => {
-        setColHeaders(map[0]);
-        setRowsHeaders(map && map.map(header => header[0]));
-    }, [map])
-
-    useEffect(() => {
-        if (storedValues) setValues(storedValues);
-    }, []);
-
-    useEffect(() => {
-        if (storedSpecs) setSpecs(specs);
-    })
-
-    useEffect(() => {
-        if (storedColSpecs) setColSpecs(storedColSpecs);
-    }, []);
-    
-    const handleAddColumn = () => {
-        if (!map.length) {
-            return setMap([[""]]);
-        }
-        setMap(() => 
-            map && map.map(row => [...row, ""])
-        );
-    };
-    const handleAddRow = () => {
-        if (!map.length) {
-            return setMap([[""]]);
-        }
-        setMap(prev => 
-            [...prev, 
-            map[0] && map[0].length ? 
-            map[0].map(() => {
-                return '';
-            }) : ['']]
-        );
-    };
-    const handleDeleteColumn = () => {
-        setMap(prev => {
-            if (!prev.length) prev;
-            if (prev[0] && prev[0].length === 1) {
-                return [];
-            };
-            return prev.map(row => row.slice(0, -1));
-        });
-    };
-    const handleDeleteRow = () => {
-        setMap(prev => {
-            if(!prev.length) prev;
-            if (prev.length && prev.length === 1) {
-                return [];
-            };
-            return prev.slice(0, -1);
-        })
-    };
-    return [ 
-        map,
-        colHeaders,
-        setColHeaders,
-        rowHeaders,
-        setRowsHeaders,
-        specs,
-        colSpecs,
+    const [ specs, setSpecs ] = useState<Array<Specs>>([]);
+    return {
         values,
         setValues,
-        handleAddColumn, 
-        handleDeleteColumn, 
-        handleAddRow, 
+        colSpecs,
+        setColSpecs,
+        specs,
+        setSpecs,
+    }
+}
+export function useTableHandlers (setSpecs: React.Dispatch<SetStateAction<Array<Specs>>>) {
+    const [ columnHeaders, setColumnHeaders ] = useState<Array<string>>([]);
+    const [ rowHeaders, setRowHeaders ] = useState<Array<string>>([]);
+
+    const handleAddColumn = (): void => {
+        setColumnHeaders(prev => prev ? [...prev, ""] : [""]);
+    }
+    const handleAddRow = (): void => {
+        if (!rowHeaders.length) setColumnHeaders([""]);
+        setRowHeaders(prev => [...prev, ""]);
+        setSpecs(prev => [...prev, {
+            disable: false,
+            count: columnHeaders && columnHeaders.length,
+            enabledValues: [],
+            enabledColumns: [],
+        }])
+    }
+    const handleDeleteColumn = (): void => {
+        if (!columnHeaders.length) return;
+        if (columnHeaders.length === 1) {
+            setColumnHeaders([])
+            setRowHeaders([])
+        };
+        return setColumnHeaders(prev => prev.slice(0, -1));
+    }
+    const handleDeleteRow = (): void => {
+        if (!rowHeaders.length) return;
+        if (rowHeaders.length === 1) {
+            setRowHeaders([])
+            setColumnHeaders([])
+        };
+        return setRowHeaders(prev => prev.slice(0, -1));
+    }
+    return {
+        setColumnHeaders,
+        setRowHeaders,
+        columnHeaders,
+        rowHeaders,
+        handleAddColumn,
+        handleAddRow,
+        handleDeleteColumn,
         handleDeleteRow,
-    ]
+    }
 }
