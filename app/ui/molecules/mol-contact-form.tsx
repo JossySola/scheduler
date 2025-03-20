456ce2696ec27f965eb2696e69eedf2b2c83fb12
Jossy Solá <jossysola@gmail.com>
2025-03-19 22:59:40 -0600

    Creates component

---
+++ app/ui/molecules/mol-contact-form.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { addToast, Button, Form, Input, Select, SelectItem, Textarea } from "@heroui/react"
+import { useActionState, useEffect } from "react"
+import contactAction from "@/app/[lang]/contact/actions"
+
+export default function ContactForm ({ lang }: {
+    lang: "es" | "en"
+}) {
+    const [ contactState, formAction, contactPending ] = useActionState(contactAction, { ok: false, message: "" })
+    useEffect(() => {
+        if (contactState && contactState.message) {
+            addToast({
+                title: contactState.message,
+                color: contactState.ok ? "success" : "danger",
+            })
+        }
+    }, [contactState]);
+    return <Form className="w-full p-5 sm:w-[400px] flex flex-col items-center">
+        <Select label={ lang === "es" ? "¿Cuál es el asunto? " : "What's the subject? " } name="subject" isRequired>
+            <SelectItem key="bug issue">{ lang === "es" ? "Problemas técnicos" : "Bug / Error Issue" }</SelectItem>
+            <SelectItem key="account inquiry">{ lang === "es" ? "Consulta sobre la cuenta" : "Account Inquiry" }</SelectItem>
+            <SelectItem key="general inquiry">{ lang === "es" ? "Consulta General" : "General Inquiry" }</SelectItem>
+        </Select>
+        <Input isRequired isClearable type="name" name="name" label={ lang === "es" ? "Nombre " : "Name " } />
+        <Input isRequired isClearable type="email" name="email" label={ lang === "es" ? "Correo electrónico " : "E-mail " } />
+        <Textarea isRequired name="message" label={ lang === "es" ? "Mensaje " : "Message " }  placeholder={ lang === "es" ? "Por favor escribe un mensaje descriptivo sobre tu consulta." : "Provide a descriptive message about your inquiry." } />
+        <Button type="submit" isLoading={ contactPending } isDisabled={ contactPending } className="bg-black text-white" formAction={ formAction }>
+            { lang === "es" ? "Enviar" : "Send" }
+        </Button>
+    </Form>
+}
