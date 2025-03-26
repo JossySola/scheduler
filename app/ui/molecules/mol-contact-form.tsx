2725956ba5c66e31ef6febc2a6aef132d7af6fba
Jossy Solá <jossysola@gmail.com>
2025-03-26 09:57:17 -0600

    Adds input fields

--- app/ui/molecules/mol-contact-form.tsx
+++ app/ui/molecules/mol-contact-form.tsx
@@ -16,15 +16,15 @@ export default function ContactForm ({ lang }: {
         }
     }, [contactState]);
     return <Form className="w-full p-5 sm:w-[400px] flex flex-col items-center">
-        <Select label={ lang === "es" ? "¿Cuál es el asunto? " : "What's the subject? " } name="subject" isRequired>
-            <SelectItem key="bug issue">{ lang === "es" ? "Problemas técnicos" : "Bug / Error Issue" }</SelectItem>
+        <Select size="lg" label={ lang === "es" ? "¿Cuál es el asunto? " : "What's the subject? " } name="subject" isRequired>
+            <SelectItem key="feedback">Feedback</SelectItem>
+            <SelectItem key="bug issue">{ lang === "es" ? "Reportar problema técnico" : "Report bug / error" }</SelectItem>
             <SelectItem key="account inquiry">{ lang === "es" ? "Consulta sobre la cuenta" : "Account Inquiry" }</SelectItem>
-            <SelectItem key="general inquiry">{ lang === "es" ? "Consulta General" : "General Inquiry" }</SelectItem>
         </Select>
-        <Input isRequired isClearable type="name" name="name" label={ lang === "es" ? "Nombre " : "Name " } />
-        <Input isRequired isClearable type="email" name="email" label={ lang === "es" ? "Correo electrónico " : "E-mail " } />
+        <Input isRequired isClearable size="lg" type="name" name="name" label={ lang === "es" ? "Nombre " : "Name " } />
+        <Input isRequired isClearable size="lg" type="email" name="email" label={ lang === "es" ? "Correo electrónico " : "E-mail " } />
         <Textarea isRequired name="message" label={ lang === "es" ? "Mensaje " : "Message " }  placeholder={ lang === "es" ? "Por favor escribe un mensaje descriptivo sobre tu consulta." : "Provide a descriptive message about your inquiry." } />
-        <Button type="submit" isLoading={ contactPending } isDisabled={ contactPending } className="bg-black text-white" formAction={ formAction }>
+        <Button size="lg" type="submit" isLoading={ contactPending } isDisabled={ contactPending } className="bg-black text-white" formAction={ formAction }>
             { lang === "es" ? "Enviar" : "Send" }
         </Button>
     </Form>
