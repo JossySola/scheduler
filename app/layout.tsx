855f72df6a5cfe9dd2d79432180b782cb7a2d0c0
Jossy Solá <jossysola@gmail.com>
2025-04-02 14:50:42 -0600

    Adds openGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,29 @@ const solitreo = localFont({
 
 export const metadata: Metadata = {
   title: "Scheduler App",
-  description: "Plan schedules using Artificial Intelligence by setting up criteria and hitting a button!",
+  description: "Let the AI do the thinking to make strategic schedules!",
+  openGraph: {
+    url: "https://scheduler.jossysola.com",
+    type: "website",
+    title: "Scheduler",
+    description: "Let the AI do the thinking to make strategic schedules!",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/25f031f4-8b86-4763-acb6-14e5afdd57a2.png?token=gJDkX97UMNp3Xk5siysdNuubSuuKyLKZ_rYL8AYrmoc&height=630&width=1200&expires=33279455004",
+        width: 1200,
+        height: 630,
+        alt: "Scheduler preview image",
+      },
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Scheduler",
+    description: "Let the AI do the thinking to make strategic schedules!",
+    images: ["https://opengraph.b-cdn.net/production/images/25f031f4-8b86-4763-acb6-14e5afdd57a2.png?token=gJDkX97UMNp3Xk5siysdNuubSuuKyLKZ_rYL8AYrmoc&height=630&width=1200&expires=33279455004"],
+  },
 };
-
+// Meta Tags Generated via https://www.opengraph.xyz
 export default function RootLayout({
   children,
 }: Readonly<{
