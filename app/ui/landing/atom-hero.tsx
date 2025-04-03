35a6a3ad828773ee95b7635daceeadd6264a1964
Jossy Solá <jossysola@gmail.com>
2025-04-02 18:12:37 -0600

    Changes opacity transform

--- app/ui/landing/atom-hero.tsx
+++ app/ui/landing/atom-hero.tsx
@@ -1,19 +1,19 @@
 "use client"
 import { motion, useScroll, useTransform } from "framer-motion"
+import { ArrowCircleDown } from "geist-icons";
 
 export default function Hero ({ lang }: {
     lang: "es" | "en"
 }) {
     const { scrollY } = useScroll();
-    const opacity = useTransform(scrollY, [0, 100, 130, 160, 190, 200, 230, 260, 290, 300, 310], [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]);
+    const opacity = useTransform(scrollY, [200, 230, 260, 290, 300, 310], [1, 0.4, 0.3, 0.2, 0.1, 0]);
 
     return (
-        <div className="flex flex-col justify-center items-center h-[95vh] w-full bg-transparent relative top-[-64] left-0 z-0">
-            <motion.h1 
-            style={{ opacity }}
-            className="text-center text-[8vw] sm:text-[3rem]">
+        <motion.div style={{ opacity }} className="flex flex-col justify-center items-center h-[95vh] w-full bg-transparent relative top-[-64] left-0 z-0">
+            <motion.h1 className="text-center text-[8vw] sm:text-[3rem]">
                 { lang === "es" ? "Planeación estratégica" : "Strategic planning" }
             </motion.h1>
-        </div>
+            <ArrowCircleDown width="64px" height="64px" className="m-6" />
+        </motion.div>
     )
 }
