2f43512af11ae18c904365bd7eb92729352a19a0
Jossy Solá <jossysola@gmail.com>
2025-04-08 00:33:11 -0600

    Sets background color in dark mode

--- app/ui/atoms/atom-footer-contact.tsx
+++ app/ui/atoms/atom-footer-contact.tsx
@@ -6,7 +6,7 @@ import { redirect, useParams } from "next/navigation"
 export default function FooterContact () {
     const params = useParams();
     const lang = params.lang ?? "en";
-    return <Button radius="full" className="bg-[#f0eee670] backdrop-blur-sm" onPress={() => redirect(`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/contact`)}>
+    return <Button radius="full" className="bg-[#f0eee670] dark:bg-[#3F3F4670] backdrop-blur-sm" onPress={() => redirect(`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/contact`)}>
         <Envelope />
     </Button>
 }
