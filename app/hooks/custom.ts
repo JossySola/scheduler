fe67cde522f79a5f7bce31bf74554517827287c0
Jossy Solá <jossysola@gmail.com>
2025-04-08 00:30:25 -0600

    Implements separation of rows and columns criteria

--- app/hooks/custom.ts
+++ app/hooks/custom.ts
@@ -2,37 +2,51 @@
 import { SetStateAction, useActionState, useState } from "react";
 import { generateTableAction } from "../[lang]/table/actions";
 
-export type Specs = {
+export type RowSpecs = {
     disable:boolean,
     count: number,
     enabledValues: Array<string>,
     enabledColumns: Array<string>,
 }
-
+export type ColSpecs = {
+    numberOfRows: number,
+    amountOfValues: Array<string>,
+}
 export function useTableSpecs () {
     const [ values, setValues ] = useState<Array<string>>([]);
-    const [ colSpecs, setColSpecs ] = useState<Array<number>>([]);
-    const [ specs, setSpecs ] = useState<Array<Specs>>([]);
+    const [ colSpecs, setColSpecs ] = useState<Array<ColSpecs>>([]);
+    const [ rowSpecs, setRowSpecs ] = useState<Array<RowSpecs>>([]);
     return {
         values,
         setValues,
         colSpecs,
         setColSpecs,
-        specs,
-        setSpecs,
+        rowSpecs,
+        setRowSpecs,
     }
 }
-export function useTableHandlers (setSpecs: React.Dispatch<SetStateAction<Array<Specs>>>) {
+export function useTableHandlers (setRowSpecs: React.Dispatch<SetStateAction<Array<RowSpecs>>>, setColSpecs: React.Dispatch<SetStateAction<Array<ColSpecs>>>) {
     const [ columnHeaders, setColumnHeaders ] = useState<Array<string>>([]);
     const [ rowHeaders, setRowHeaders ] = useState<Array<string>>([]);
 
     const handleAddColumn = (): void => {
+        if (rowHeaders && rowHeaders.length === 0) {
+            setColSpecs(prev => [...prev, {
+                numberOfRows: rowHeaders.length ?? 0,
+                amountOfValues: [],
+            }])
+            return setRowHeaders([""]);
+        }
+        setColSpecs(prev => [...prev, {
+            numberOfRows: rowHeaders.length ?? 0,
+            amountOfValues: [],
+        }])
         return setColumnHeaders(prev => !prev ? [""] : [...prev, ""] );
     }
     const handleAddRow = (): void => {
         if (!columnHeaders.length) return setColumnHeaders([""]);
         setRowHeaders(prev => !prev ? [""] : [...prev, ""]);
-        setSpecs(prev => [...prev, {
+        setRowSpecs(prev => [...prev, {
             disable: false,
             count: columnHeaders && columnHeaders.length ? columnHeaders.length : 0,
             enabledValues: [],
@@ -42,8 +56,8 @@ export function useTableHandlers (setSpecs: React.Dispatch<SetStateAction<Array<
     const handleDeleteColumn = (): void => {
         if (!columnHeaders.length) return;
         if (columnHeaders.length === 1) {
-            setColumnHeaders([])
-            setRowHeaders([])
+            setColumnHeaders([]);
+            setRowHeaders([]);
         };
         return setColumnHeaders(prev => prev.slice(0, -1));
     }
