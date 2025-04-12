cd17cd2e47ada6dcc9492aa8851b629220e71b19
Jossy Solá <jossysola@gmail.com>
2025-04-12 01:51:24 -0600

    Implements useDebouncedCallback

--- app/ui/molecules/mol-table-columns.tsx
+++ app/ui/molecules/mol-table-columns.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { TableHandlersContext, TableHandlersType } from "@/app/[lang]/table/context";
 import { Input } from "@heroui/react";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function TableColumn ({ headerIndex, value }: {
     headerIndex: number,
@@ -10,6 +11,17 @@ export default function TableColumn ({ headerIndex, value }: {
     const colLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     const { setColumnHeaders }: TableHandlersType = useContext(TableHandlersContext);
     const [ text, setText ] = useState<string>(value ?? " ");
+    const update = () => {
+        setColumnHeaders && setColumnHeaders(prev => {
+            let updated = [...prev];
+            updated[headerIndex] = text;
+            return updated;
+        })
+    }
+    const debouncedUpdate = useDebouncedCallback(update, 1000);
+    useEffect(() => {
+        debouncedUpdate();
+    }, [text]);
     return (
         <th scope="col">
             <span className="text-tiny">{`${colLetters[headerIndex]}`}</span>
@@ -29,14 +41,7 @@ export default function TableColumn ({ headerIndex, value }: {
                 value={ text }
                 variant="flat"
                 autoComplete="off"
-                onChange={ e => {
-                    setText(e.target.value);
-                    setColumnHeaders && setColumnHeaders(prev => {
-                        let duplicate = [...prev];
-                        duplicate[headerIndex] = e.target.value;
-                        return duplicate;
-                    });
-                }} />
+                onValueChange={ setText } />
             </div>
         </th>
     )
