f3829ac45d7120850404929fcc44479f457a3fab
Jossy Solá <jossysola@gmail.com>
2025-04-12 01:42:57 -0600

    Uses useParams to get the language

--- app/ui/atoms/atom-logout-icon.tsx
+++ app/ui/atoms/atom-logout-icon.tsx
@@ -2,11 +2,13 @@
 import { SignoutAction } from "@/app/[lang]/actions"
 import { Button } from "@heroui/react"
 import { Logout } from "geist-icons"
+import { useParams } from "next/navigation"
 import { useActionState } from "react"
 
-export default function SignOutButton ({ lang }: {
-    lang: "en" | "es"
-}) {
+export default function SignOutButton () {
+    const params = useParams();
+    const lang = params.lang;
+
     const [ state, action, pending ] = useActionState(SignoutAction, { message: "" })
     return <form action={ action }>
         <Button
