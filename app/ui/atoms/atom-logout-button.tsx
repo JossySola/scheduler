366f3e6edfc7d946ec9650b9310fd127654a43b1
Jossy Solá <jossysola@gmail.com>
2025-04-12 01:42:32 -0600

    Removes logic and leaves SignOutButton component alone

--- app/ui/atoms/atom-logout-button.tsx
+++ app/ui/atoms/atom-logout-button.tsx
@@ -1,8 +1,5 @@
-import { headers } from "next/headers";
 import SignOutButton from "./atom-logout-icon";
 
 export default async function LogOutButton () {
-    const requestHeaders = headers();
-    const lang = (await requestHeaders).get("x-user-locale") || "en";
-    return <SignOutButton lang={ lang as "en" | "es" } />
+    return <SignOutButton />
 }
