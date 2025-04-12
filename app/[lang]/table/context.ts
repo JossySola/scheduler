b11b70814331aaf35f28731d6744da4d20bdca77
Jossy Solá <jossysola@gmail.com>
2025-04-12 01:34:36 -0600

    Removes Specs Context. Edits TableHandlers and creates TableDatabaseType

--- app/[lang]/table/context.ts
+++ app/[lang]/table/context.ts
@@ -1,27 +1,32 @@
 import { ColSpecs, RowSpecs } from "@/app/hooks/custom";
 import React, { createContext, SetStateAction } from "react";
 
-export const TableSpecsContext = createContext({});
+export const TableDatabaseContext = createContext({});
 export const TableHandlersContext = createContext({});
 export const AnthropicGenerationContext = createContext({});
 
-export interface TableSpecsType {
+export interface TableHandlersType {
+    setColumnHeaders?: React.Dispatch<SetStateAction<Array<string>>>,
+    setRowHeaders?: React.Dispatch<SetStateAction<Array<string>>>,
+    columnHeaders?: Array<string>,
+    rowHeaders?: Array<string>,
     values?: Array<string>,
     setValues?: React.Dispatch<SetStateAction<Array<string>>>,
     colSpecs?: Array<ColSpecs>,
     setColSpecs?: React.Dispatch<SetStateAction<Array<ColSpecs>>>,
     rowSpecs?: Array<RowSpecs>,
     setRowSpecs?: React.Dispatch<SetStateAction<Array<RowSpecs>>>,
-}
-export interface TableHandlersType {
-    setColumnHeaders?: React.Dispatch<SetStateAction<Array<string>>>,
-    setRowHeaders?: React.Dispatch<SetStateAction<Array<string>>>,
-    columnHeaders?: Array<string>,
-    rowHeaders?: Array<string>,
     handleAddColumn?: () => void,
     handleAddRow?: () => void,
     handleDeleteColumn?: () => void,
     handleDeleteRow?: () => void,
+}
+export interface TableDatabaseType {
+    storedTitle?: string,
+    storedRows?: Array<Array<string>>,
+    storedValues?: Array<string>,
+    storedRowSpecs?: Array<RowSpecs>,
+    storedColSpecs?: Array<ColSpecs>,
 }
 export interface AnthropicGenerationType {
     anthropicAction?: (payload: FormData) => void,
