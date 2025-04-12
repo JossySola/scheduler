7f6646376e89c2314afb87c753b13f5ccb5f8414
Jossy Solá <jossysola@gmail.com>
2025-04-12 01:32:25 -0600

    Edits privacy content

--- app/[lang]/privacy/page.tsx
+++ app/[lang]/privacy/page.tsx
@@ -6,11 +6,11 @@ export default async function Page ({ params }: {
 }) {
     const lang = (await params).lang;
     return (
-        <section className="pb-10 p-5 text-base/7">
+        <section className="p-10 pb-[6rem] text-base/7 h-fit">
             
             <h1>Privacy Policy</h1>
 
-            <i>Effective Date: February 19th, 2025.</i>
+            <i>Effective Date: April 9th, 2025.</i>
             <Divider className="my-4"/>
             <h2>1. Introduction</h2>
 
@@ -36,15 +36,15 @@ export default async function Page ({ params }: {
                     <ul className="pl-16 list-disc">
                         <li>Must be at least 8 characters long.</li>
                         <li>Recommended use of a Password Manager’s suggested password.</li>
-                        <li>Checked against the <Code size="sm">Have I Been Pwned API <a href="#footer"><sup>1</sup></a></Code> to verify it hasn’t been exposed in data breaches.</li>
+                        <li>Checked against a public <Code size="sm">database of exposed passwords <a href="#footer"><sup>1</sup></a></Code> to verify it hasn’t been exposed in data breaches.</li>
                         <li>Hashed using <Code size="sm">Argon2id <a href="#footer"><sup>2</sup></a></Code> before storage (we never store raw passwords).</li>
-                        <li>As a second security layer, the hashed password is encrypted before being stored in the database.</li>
+                        <li>As a second security layer, the hashed password is encrypted before being stored in the database with a <b>UNIQUE</b> encryption key <a href="#footer"><sup>3</sup></a>.</li>
                     </ul>
                 </li>
                 <li>
                     <h3>Schedule Encryption:</h3>
                     <ul className="pl-16 list-disc">
-                        <li>All schedules and tables are encrypted using <Code size="sm">BYTEA encryption <a href="#footer"><sup>3</sup></a></Code> before storage.</li>
+                        <li>All schedules and tables are encrypted <a href="#footer"><sup>4</sup></a> using strong encryption algorithms before storage.</li>
                     </ul>
                 </li>
             </ul>
@@ -69,18 +69,22 @@ export default async function Page ({ params }: {
                 <li><b>Artificial Intelligence Service (Anthropic):</b> An external AI system assists in generating strategic schedules.</li>
             </ul>
 
-            <i>These third-party services may collect data as per their own privacy policies. We encourage you to review their terms before connecting your account. <a href="#footer"><sup>4</sup></a></i>
+            <i>These third-party services may collect data as per their own privacy policies. We encourage you to review their terms before connecting your account <a href="#footer"><sup>5</sup></a>.</i>
 
             <h2>6. User Control & Account Deletion</h2>
-            
-            <p><b>Account Deletion:</b> You may delete your account at any time. This will:</p>
 
             <ul className="pl-8 list-disc">
-                <li>Remove all personal data (name, username, birthday, email, password).</li>
-                <li>Erase all stored schedules and tables.</li>
-                <li>Disconnect any linked providers (Google and/or Facebook).</li>
+                <li>
+                    <b>Account Deletion:</b> You may delete your account at any time. This will:
+                    <ul className="pl-8 list-disc">
+                        <li>Remove all personal data (name, username, birthday, email, password).</li>
+                        <li>Erase all stored schedules and tables.</li>
+                        <li>Disconnect any linked providers (Google and/or Facebook).</li>
+                    </ul>
+                </li>
                 <li><b>Disconnecting Providers:</b> You can manually disconnect your account from Google or Facebook without deleting your account.</li>
                 <li><b>Password Change:</b> You can change your password at any time.</li>
+                <li><b>Table creation:</b> You can create, modify, save and delete the tables you own in the platform at any time.</li>
             </ul>
 
             <h2>7. Data Retention</h2>
@@ -91,19 +95,29 @@ export default async function Page ({ params }: {
             
             <p>Our web app is not intended for individuals under the age of 13. If we become aware that a child under 13 has provided personal data, we will take immediate steps to delete the information.</p>
 
-            <h2>9. Updates to This Policy</h2>
+            <h2>9. Cookies</h2>
+
+            <p>We only use <Code size="sm">Cookies <a href="#footer"><sup>6</sup></a></Code> for User's Authentication management.</p>
+
+            <h2>10. User roles in the platform</h2>
+
+            <p>All users in the database have the same privileges and options. This means there are no super-users or roles with special privileges within the platform that may be able to view and modify any of your data.</p>
+            
+            <h2>11. Updates to This Policy</h2>
 
             <p>We may update this Privacy Policy from time to time. If we make significant changes, we will notify you via email or within the web app.</p>
 
-            <h2>10. Contact Us</h2>
+            <h2>12. Contact Us</h2>
 
             <p>If you have any questions about this Privacy Policy, please contact us {<Link href={`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/contact`}>here</Link>}.</p>
             <Divider className="my-4"/>
             <footer className="text-tiny dark:bg-[#1A1A1A]" id="footer">
-                <p><sup>1</sup> <Link href="https://haveibeenpwned.com/Passwords">Have I Been Pwned</Link> is a service that provides a list of passwords previously exposed in data breaches. This is used to avoid the use of unsuitable passwords.</p>
-                <p><sup>2</sup> <Link href="https://en.wikipedia.org/wiki/Argon2">Argon2id</Link> is a hybrid version of <b>Argon2</b>, which is a key derivation function used to hash passwords.</p>
-                <p><sup>3</sup> <Link href="https://www.postgresql.org/docs/current/pgcrypto.html#PGCRYPTO-PGP-ENC-FUNCS-PGP-SYM-ENCRYPT">BYTEA encryption</Link> is the way we encrypt data before storing it into our database. The encryption and decryption of such data is done with a unique symmetric key/password.</p>
-                <p><sup>4</sup> <Link href="https://www.anthropic.com/legal/privacy">Anthropic Privacy Policy.</Link> For the Artificial Intelligence Service, we use <Code className="text-tiny"><b>Anthropic: </b>claude-3-opus-20240229</Code></p>
+                <p><sup>1</sup> <Link href="https://haveibeenpwned.com/Passwords">Have I Been Pwned</Link> is a service that provides a list of passwords previously exposed in data breaches. This is used to avoid the use of unsuitable passwords. From our side, we check your password against a public database of exposed passwords (Have I Been Pwned) using a privacy-preserving method that ensures your password is never fully revealed.</p>
+                <p><sup>2</sup> <Link href="https://en.wikipedia.org/wiki/Argon2">Argon2id</Link> is a hybrid version of <b>Argon2</b>. We hash all passwords using Argon2id, a secure password hashing algorithm recommended by security experts.</p>
+                <p><sup>3</sup> <Link href="https://docs.aws.amazon.com/kms/latest/developerguide/overview.html">AWS KMS</Link> is the <b>Key Management Service</b> provided by the <b>Amazon Web Services</b>. We use AWS’s secure key management infrastructure to encrypt sensitive data.</p>
+                <p><sup>4</sup> Sensitive data is encrypted before being stored in our database using a strong encryption algorithm and securely managed keys.</p>
+                <p><sup>5</sup> <Link href="https://www.anthropic.com/legal/privacy">Anthropic Privacy Policy.</Link> For AI-related features, we use a privacy-compliant language model provided by Anthropic.</p>
+                <p><sup>6</sup> <Link href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Cookies">Cookies</Link> are small pieces of data a Server sends to the User's browser in order to preserve and manage information between the Client and the Server. In this case, we use Cookies to preserve and manage the User's authentication and session while using the application if they sign in.</p>
             </footer>
         </section>
     )
