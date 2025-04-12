d3fffd9378ea372eec003ce04a13af9e7b377cff
Jossy Solá <jossysola@gmail.com>
2025-04-12 01:37:04 -0600

    Removes Cookies store

--- app/[lang]/actions.ts
+++ app/[lang]/actions.ts
@@ -1,12 +1,11 @@
 "use server"
 import { signOut } from "@/auth";
-import { cookies, headers } from "next/headers";
+import { headers } from "next/headers";
 import "server-only";
 
 export async function SignoutAction (previousState: { message: string }, formData: FormData) {
     const requestHeaders = headers();
     const lang = (await requestHeaders).get("x-user-locale") || "en";
-    const cookieStore = await cookies();
     await signOut({
         redirect: true,
         redirectTo: `/${lang}/login`
