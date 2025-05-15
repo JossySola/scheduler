947e4e11146659ab36d51f91f2faf42aa0a30a53
Jossy Solá <jossysola@gmail.com>
2025-05-14 19:48:25 -0600

    Refactors catch logic

--- app/[lang]/login/actions.ts
+++ app/[lang]/login/actions.ts
@@ -1,7 +1,6 @@
 "use server"
 import "server-only";
 import { signIn } from "@/auth";
-import { AuthError } from "@auth/core/errors";
 import { headers } from "next/headers";
 
 export async function LogInAction (prevState: { message: string, nextAttempt: number | null }, formData: FormData) {
@@ -14,7 +13,7 @@ export async function LogInAction (prevState: { message: string, nextAttempt: nu
     if (!formData || !username || !password) {
         return {
             message: locale === "es" ? "Por favor, llena todos los campos" : "Please, fill out both fields",
-            nextAttempt: null
+            nextAttempt: null,
         }
     }
     
@@ -23,52 +22,62 @@ export async function LogInAction (prevState: { message: string, nextAttempt: nu
             username,
             password,
             redirect: true,
-            redirectTo: `/${locale}/dashboard`
+            redirectTo: `/${locale}/dashboard`,
         })
         return {
             message: "Logged in",
-            nextAttempt: null
+            nextAttempt: null,
         }
     } catch (error) {
-        if (error instanceof Error && error.message !== "NEXT_REDIRECT") {
-            if (error instanceof AuthError) {
-                if (error.cause?.next_attempt) {
-                    return {
-                        message: locale === "es" ? "Información inválida" : "Invalid credentials",
-                        nextAttempt: error.cause.next_attempt as number
-                    };
-                }
-                if (typeof error.cause === "number") {
-                    if (error.cause === 404) {
-                        return {
-                            message: locale === "es" ? "Usuario no existe" : "User not found",
-                            nextAttempt: null
-                        }
-                    } else if (error.cause === 409) {
-                        return {
-                            message: locale === "es" ? "Registro incorrecto. Favor de reportar error" : "Bad registry. Please report this specific issue",
-                            nextAttempt: null
-                        }
-                    } else if (error.cause === 500) {
-                        return {
-                            message: locale === "es" ? "Error interno" : "Internal Error",
-                            nextAttempt: null
-                        }
-                    }
-
-                }
-                console.log(error.cause)
+        if (error.type && error.type === "CallbackRouteError") {
+            const cause = error.cause.err.cause;
+            if (error.cause.next_attempt) {
                 return {
-                    message: locale === "es" ? "Este correo electrónico está registrado con un proveedor externo, usa ese método para iniciar sesión." : "This e-mail is registered with an external provider, use that instead.",
-                    nextAttempt: null
+                    message: locale === "es" ? "Información inválida" : "Invalid credentials",
+                    nextAttempt: error.cause.next_attempt as number,
                 }
-            } else if (error instanceof Error) {
+            }
+            if (cause === 404) {
                 return {
-                    message: locale === "es" ? "Error inesperado. Inténtalo nuevamente" : "Something unexpected happened. Please try again later",
-                    nextAttempt: null
+                    message: locale === "es" ? "Usuario no existe" : "User not found",
+                    nextAttempt: null,
+                }
+            } else if (cause === 409) {
+                return {
+                    message: locale === "es" ? "Registro incorrecto. Favor de reportar error" : "Bad registry. Please report this specific issue",
+                    nextAttempt: null,
+                }
+            } else if (cause === 500) {
+                return {
+                    message: locale === "es" ? "Error interno" : "Internal Error",
+                    nextAttempt: null,
+                }
+            } else if (cause == 400) {
+                return {
+                    message: locale === "es" ? "Has iniciado sesión con un proveedor externo (Google o Facebook) anteriormente. Por favor, inicia sesión con ese proveedor" : "You have signed in with an external provider in the past (Google or Facebook), please use that method instead",
+                    nextAttempt: null,
                 }
             }
         }
-        throw error;
+        return {
+            message: locale === "es" ? "Error inesperado. Inténtalo nuevamente" : "Something unexpected happened. Please try again later",
+            nextAttempt: null,
+        }
+    }
+}
+export async function GoogleSignInAction (prevState: { message: string }, formData: FormData) {
+    const requestHeaders = headers();
+    const locale = (await requestHeaders).get("x-user-locale") || "en";
+    await signIn("google", { redirect: true, redirectTo: `/${locale}/dashboard`});
+    return {
+        message: "Google signup"
+    }
+}
+export async function FacebookSignInAction (prevState: { message: string }, formData: FormData) {
+    const requestHeaders = headers();
+    const locale = (await requestHeaders).get("x-user-locale") || "en";
+    await signIn("facebook", { redirect: true, redirectTo: `/${locale}/dashboard`});
+    return {
+        message: "Facebook signup"
     }
 }
