fb9d4be8187026ca8a09b0a6d7504a301eb20657
Jossy Solá <jossysola@gmail.com>
2025-05-26 20:56:47 -0600

    Moves parallel @list route page logic into the file. Removes <Suspense> components

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -1,17 +1,24 @@
 import pool from "@/app/lib/mocks/db";
 import UserProfile from "@/app/ui/atoms/atom-user-profile";
-import { UserSkeleton } from "@/app/ui/atoms/skeletons";
+import { DashboardSkeleton, UserSkeleton } from "@/app/ui/atoms/skeletons";
 import Settings from "@/app/ui/molecules/mol-settings";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { sql } from "@vercel/postgres";
+import DashboardTable from "@/app/ui/molecules/mol-dashboard-table";
 
 interface ProviderData {
     provider: string,
 }
+interface TableData {
+    table_id: string;
+    table_name: string;
+    updated_at: string;
+    created_at: string;
+}
 export default async function Page ({ params }: {
-    params: Promise<{ lang: string }>
+    params: Promise<{ lang: "en" | "es" }>
 }) {
     const session = await auth();
     const lang = (await params).lang;
@@ -22,13 +29,19 @@ export default async function Page ({ params }: {
             WHERE email = ${session.user.email};
         `;
         const providersResponse = providers.rows as ProviderData[] ?? [];
-
+        const table_data = await sql`
+            SELECT id AS table_id, table_name, updated_at, created_at
+            FROM scheduler_users_tables
+            WHERE user_id = ${session.user.id};
+        `;
+        const rows = table_data.rows as TableData[];
         return (
-            <section className="w-full flex flex-row justify-center gap-6 p-5 sm:justify-start sm:p-10">
-                <Suspense fallback={ <UserSkeleton /> }>
-                    <UserProfile />
-                    <Settings lang={lang} data={providersResponse}/>
-                </Suspense>
+            <section>
+                <section className="w-full flex flex-row justify-center gap-6 p-5 sm:justify-start sm:p-10">
+                        <UserProfile />
+                        <Settings lang={lang} data={providersResponse}/>
+                </section>
+                    <DashboardTable rows={ rows } lang={ lang } />
             </section>
         )
     }
