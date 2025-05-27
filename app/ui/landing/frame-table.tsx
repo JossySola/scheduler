a6b4a5f6933e8ba6c4f0eefdc3ca6b2943c0bc0a
Jossy Solá <jossysola@gmail.com>
2025-05-26 20:58:35 -0600

    Minor styling changes

--- app/ui/landing/frame-table.tsx
+++ app/ui/landing/frame-table.tsx
@@ -14,8 +14,8 @@ export default function FrameTable ({ lang, ref }: {
     const opacity = useTransform(scrollYProgress, [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], [0, 0.2, 0.4, 0.7, 0.8, 0.9, 1, 1, 1, 1, 1]);
     const x = useTransform(scrollYProgress, [0, 0.1, 0.2, 0.3, 0.4, 0.5], [100, 80, 60, 40, 20, 0]);
     return (
-        <motion.section className="w-full z-0">
-            <motion.div className="flex flex-col justify-center items-center">
+        <motion.section className="w-full">
+            <motion.div className="flex flex-row justify-center items-center">
                     <motion.div
                     ref={ ref }
                     style={{ opacity, x }}
