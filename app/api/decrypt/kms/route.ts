c5643e48660e98277dd15918a710382b79dd2da8
Jossy Solá <jossysola@gmail.com>
2025-05-29 15:34:40 -0600

    Creates endpoint to decrypt KMS key

---
+++ app/api/decrypt/kms/route.ts
@@ -0,0 +1,18 @@
+"use server"
+import { decryptKmsDataKey } from "@/app/lib/utils";
+import { auth } from "@/auth";
+import { AuthenticatedRequest } from "@/middleware";
+import { NextResponse } from "next/server";
+import "server-only";
+
+
+export const GET = auth(async function GET (req: AuthenticatedRequest) {
+    const payload = await req.json();
+    const key: string = payload.key;
+
+    if (key) {
+        const decryption = await decryptKmsDataKey(key);
+        return NextResponse.json({ decryption }, { status: 200 });
+    }
+    return NextResponse.json({ error: "Missing key" }, { status: 400 })
+})
