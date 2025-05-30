bff7dc3b0f7c884f8d7999412c5b2eff87a7dd65
Jossy Solá <jossysola@gmail.com>
2025-05-29 20:17:08 -0600

    Adds console logs to debug

--- app/api/decrypt/kms/route.ts
+++ app/api/decrypt/kms/route.ts
@@ -5,10 +5,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET (req: NextRequest): Promise<Response> {
     const payload = await req.json();
+    console.log("📄 Payload: ", payload)
     const key: string = payload.key;
-
+    console.log("🔑 Key: ", key)
     if (key) {
         const decryption = await decryptKmsDataKey(key);
+        console.log("🔐 Decryption: ", decryption)
         return NextResponse.json({ decryption }, { status: 200 });
     }
     return NextResponse.json({ error: "Missing key" }, { status: 400 })
