8f155ee5ed52c4f3041f1c4f504d83a64e65ab1e
Jossy Solá <jossysola@gmail.com>
2025-05-29 20:49:56 -0600

    Corrects headers consumption as it was wrongly expecting a JSON body like a POST method but it is a GET method instead.

--- app/api/decrypt/kms/route.ts
+++ app/api/decrypt/kms/route.ts
@@ -4,13 +4,10 @@ import { decryptKmsDataKey } from "@/app/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET (req: NextRequest): Promise<Response> {
-    const payload = await req.json();
-    console.log("📄 Payload: ", payload)
-    const key: string = payload.key;
-    console.log("🔑 Key: ", key)
+    const headersList: Headers = req.headers
+    const key = headersList.get("key");
     if (key) {
         const decryption = await decryptKmsDataKey(key);
-        console.log("🔐 Decryption: ", decryption)
         return NextResponse.json({ decryption }, { status: 200 });
     }
     return NextResponse.json({ error: "Missing key" }, { status: 400 })
--- app/api/verify/password/route.ts
+++ app/api/verify/password/route.ts
@@ -6,16 +6,16 @@ import { auth } from "@/auth";
 import { AuthenticatedRequest } from "@/middleware";
 
 export const GET = auth(async function GET (req: AuthenticatedRequest): Promise<NextResponse> {
-    const payload = await req.json();
-    const password: string = payload.password;
-    const decrypted: string = payload.decrypted;
-    
+    const headersList: Headers = req.headers;
+    const password: string | null = headersList.get("password");
+    const decrypted: string | null = headersList.get("decrypted");  
+    if (!password || !decrypted) {
+        return NextResponse.json({ error: 'Missing data to verify' }, { status: 400 });
+    }
     const argon = new Argon2id();
     const verify = await argon.verify(decrypted, password);
-
     if (!verify) {
         return NextResponse.json({ error: 'Invalid credentials' }, { status: 403 })
     }
-
     return NextResponse.json({ statusText: 'Granted' }, { status: 200 });
 })
