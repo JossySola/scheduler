bdad43da8707b1d5f598d24fad41375932bc212e
Jossy Solá <jossysola@gmail.com>
2025-06-05 01:34:58 -0600

    Creates argon verification function

---
+++ app/lib/argon-server.ts
@@ -0,0 +1,6 @@
+import { Argon2id } from "oslo/password";
+
+export async function verifyPassword(hashed: string, plain: string): Promise<boolean> {
+  const argon = new Argon2id();
+  return await argon.verify(hashed, plain);
+}
