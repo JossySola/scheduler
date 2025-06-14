43ea327b8ff6218aa56012bf420dd9d030c16b57
Jossy Solá <jossysola@gmail.com>
2025-06-14 01:36:26 -0600

    Changes on CSS styles

--- app/ui/refactor/col-headers.tsx
+++ app/ui/refactor/col-headers.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { useMemo } from "react"
 import Cell from "./cell"
 
@@ -9,11 +8,13 @@ export default function ColHeaders({ row, rowIndex, size }: {
     size: number,
 }) {
     return useMemo(() => {
-        return Array.from(row.values()).map((column: string, colIndex: number) => {
-            return <th scope="col" key={`col${colIndex}row${rowIndex}`}>
-                {
-                <Cell value={column} rowIndex={rowIndex} colIndex={colIndex} />
-                }
+        return Array.from(row.entries()).map(([key, value]: [any, any], colIndex: number) => {
+            return <th scope="col" key={`col${colIndex}row${rowIndex}`} className="flex flex-col justify-center items-center gap-2" >
+                <span className="text-tiny">{String(key)}</span>
+                <div className="flex flex-row items-center gap-3">
+                    { colIndex === 0 && <span className="text-tiny w-[1rem]">{String(rowIndex)}</span> }
+                    { <Cell value={value} rowIndex={rowIndex} colIndex={colIndex} /> }
+                </div>
             </th>
         })
     }, [size])
