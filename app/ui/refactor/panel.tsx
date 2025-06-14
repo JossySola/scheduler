24245111580ffda7bb25f6156579d3e06bb68466
Jossy Solá <jossysola@gmail.com>
2025-06-14 01:37:21 -0600

    Adds more props and restructures return JSX

--- app/ui/refactor/panel.tsx
+++ app/ui/refactor/panel.tsx
@@ -1,49 +1,39 @@
 "use client"
-import Table from "./table"
-import Specs from "./specs"
-import { useParams } from "next/navigation"
+import Table from "./table";
+import { useParams } from "next/navigation";
 import { TableContext } from "@/app/[lang]/table/context";
-import { Table as TableClass } from "@/app/lib/utils-client";
-import { useCallback, useState } from "react";
-import { Button } from "@heroui/react";
+import { useState } from "react";
+import ColumnsActions from "./columns-actions";
+import RowsActions from "./rows-actions";
+import TableSettings from "./table-settings";
+import { DynamicTable } from "@/app/lib/utils-client";
 
-export default function Panel ({ storedTable }: { storedTable?: TableClass }) {
+export default function Panel ({ 
+    title, stored_rows, stored_values, stored_row_specs, stored_col_specs }: { 
+        title?: string,
+        stored_rows?: Array<Map<any, any>>,
+        stored_values?: Array<string>,
+        stored_row_specs?: Array<Map<any, any>>,
+        stored_col_specs?: Array<Map<any, any>>, 
+    }) {
     const params = useParams<{ lang: "en" | "es" }>();
     const [_v, setVersion] = useState<number>(0);
-    const [tableInstance, _t] = useState<TableClass>(new TableClass(storedTable && storedTable.title, storedTable && storedTable.rows));
-
-    const updateTable = useCallback((mutatorFn: () => void) => {
-        mutatorFn();
-        setVersion(v => v + 0.5);
-    }, [tableInstance]);
+    const [tableInstance, _t] = useState<DynamicTable>(new DynamicTable(
+        title ? title : (params.lang === "es" ? "Sin título" : "Untitled table"), 
+        stored_rows && stored_rows,
+        stored_values && stored_values,
+        stored_row_specs && stored_row_specs,
+        stored_col_specs && stored_col_specs,
+    ));
 
     return (
-        <section>
-            <h1>Panel</h1>
-            <Button onPress={() => updateTable(() => tableInstance.addColumn())}>
-                {
-                    params.lang === "es" ? "" : "Add column"
-                }
-            </Button>
-            <Button onPress={() => updateTable(() => tableInstance.removeColumn(tableInstance.size - 1))}>
-                {
-                    params.lang === "es" ? "" : "Delete column"
-                }
-            </Button>
-            <Button onPress={() => updateTable(() => tableInstance.addRow())}>
-                {
-                    params.lang === "es" ? "" : "Add row"
-                }
-            </Button>
-            <Button onPress={() => updateTable(() => tableInstance.removeRow())}>
-                {
-                    params.lang === "es" ? "" : "Delete row"
-                }
-            </Button>
-            <TableContext value={{ table: tableInstance , setVersion }}>
+        <TableContext.Provider value={{ table: tableInstance, setVersion }}>
+            <section className="grid grid-rows-[auto_auto] grid-cols-[auto_auto]">
+                <TableSettings />
+                <ColumnsActions setVersion={setVersion} tableInstance={tableInstance} />
+                <RowsActions setVersion={setVersion} tableInstance={tableInstance} />
                 <Table />
-                <Specs />
-            </TableContext>
-        </section>
+            </section>
+        </TableContext.Provider>
     )
 }
