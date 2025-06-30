3d07551848854a2f412d83b5c99c86d4dba7b51c
Jossy Solá <jossysola@gmail.com>
2025-06-29 18:01:37 -0600

    Removes the dashboard table temporarily

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -29,19 +29,13 @@ export default async function Page ({ params }: {
             WHERE email = ${session.user.email};
         `;
         const providersResponse = providers.rows as ProviderData[] ?? [];
-        const table_data = await sql`
-            SELECT id AS table_id, table_name, updated_at, created_at
-            FROM scheduler_users_tables
-            WHERE user_id = ${session.user.id};
-        `;
-        const rows = table_data.rows as TableData[];
+        
         return (
             <section>
                 <section className="w-full flex flex-row justify-center gap-6 p-5 sm:justify-start sm:p-10">
                         <UserProfile />
                         <Settings lang={lang} data={providersResponse}/>
                 </section>
-                    <DashboardTable rows={ rows } lang={ lang } />
             </section>
         )
     }
