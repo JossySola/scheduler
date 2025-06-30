0b927877e6eaf1bb5309e1861adf4f1dfa80cd8c
Jossy Solá <jossysola@gmail.com>
2025-06-29 18:07:45 -0600

    Uses new custom hook 'useCallbackAction'. Uses 'useEffect' hook to call 'addToast'. Uses the new hook's properties 'isPending' and 'run'

--- app/ui/v3/save-button.tsx
+++ app/ui/v3/save-button.tsx
@@ -1,18 +1,36 @@
 "use client"
-
-import { Button } from "@heroui/react"
+import { addToast, Button } from "@heroui/react"
 import { FloppyDisk } from "../icons"
 import { useParams } from "next/navigation"
+import { SaveNewTableAction } from "@/app/[lang]/table/actions";
+import { useContext, useEffect } from "react";
+import { TableContext } from "@/app/[lang]/table/context";
+import { useCallbackAction } from "@/app/hooks/custom";
 
 export default function SaveButton () {
     const params = useParams<{ lang: "es" | "en" }>();
     const { lang } = params;
+    const { table } = useContext(TableContext);
+    const save = useCallbackAction(
+        () => SaveNewTableAction(table.name, table.rows, table.values, table.columnType, table.interval), 
+        { ok: false, message: "" }
+    );
+    useEffect(() => {
+        if (save.state.message) {
+            addToast({
+                title: save.state.message,
+                color: save.state.ok ? "success" : "danger",
+            })
+        }
+    }, [save.state]);
     return (
         <Button
         type="button"
         size="lg"
         className="dark:bg-white bg-black dark:text-black text-white text-lg"
-        endContent={<FloppyDisk />}>
+        endContent={<FloppyDisk />}
+        isLoading={ save.isPending }
+        onPress={ save.run }>
             { lang === "es" ? "Guardar" : "Save" }
         </Button>
     )
