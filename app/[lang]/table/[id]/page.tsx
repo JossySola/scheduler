fd10e23851755fd060f9ca89781a867692710db6
Jossy Solá <jossysola@gmail.com>
2025-07-03 15:37:31 -0600

    Completes logic to fetch data and pass it as prop to Panel

--- app/[lang]/table/[id]/page.tsx
+++ app/[lang]/table/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { decryptKmsDataKey } from "@/app/lib/utils";
+import { RowType } from "@/app/lib/utils-client";
 import { BackButton } from "@/app/ui/atoms/atom-button-back";
+import Panel from "@/app/ui/v3/client-panel";
 import { auth } from "@/auth";
 import { sql } from "@vercel/postgres";
 
@@ -8,70 +10,64 @@ export default async function Page ({ params }: {
 }) {
     const { id, lang } = await params;
     const session = await auth();
-    const decryptToString = (decrypt: Buffer) => {
-        if (!decrypt) return;
-        const buffer = Buffer.from(decrypt);
-        const string = buffer.toString("utf-8");
-        const data = JSON.parse(string);
-        return data;
-    }
     if (session?.user?.id) {
-        const user_id = session.user.id;
         // Replaced API endpoint with direct query logic
-        const doesTableExist = await sql`
-        SELECT id FROM scheduler_users_tables
-        WHERE id = ${(await params).id};
-        `;
-        if (!doesTableExist.rows.length || doesTableExist.rowCount === 0) {
+        const keys = await sql`
+        SELECT table_name_key, table_rows_key, table_values_key
+        FROM scheduler_users_tables
+        WHERE id = ${id};`.then(result => result.rowCount !== 0 ? result.rows[0] : null).catch(() => null);
+
+        if (keys && keys.table_name_key && keys.table_rows_key && keys.table_values_key) {
+            const name_key = await decryptKmsDataKey(keys.table_name_key);
+            const rows_key = await decryptKmsDataKey(keys.table_rows_key);
+            const values_key = await decryptKmsDataKey(keys.table_values_key);
+            const serialized_table = await sql`
+            SELECT
+                user_id,
+                pgp_sym_decrypt_bytea(table_name, ${name_key}) AS decrypted_name,
+                pgp_sym_decrypt_bytea(table_rows, ${rows_key}) AS decrypted_rows,
+                pgp_sym_decrypt_bytea(table_values, ${values_key}) AS decrypted_values,
+                table_type,
+                table_interval,
+                created_at,
+                updated_at
+            FROM scheduler_users_tables
+            WHERE id = ${id};`
+            .then(result => result.rowCount !== 0 ? result.rows[0] : null)
+            .catch(() => null);
+            
+            if (serialized_table) {
+                const rows: Array<Map<string, RowType>> = JSON.parse(serialized_table.decrypted_rows).map((row: Array<[string, RowType]>) => {
+                    return new Map(row);
+                })
+                // Remember to deserialize this object if you pass any property as prop to a component
+                const table = {
+                    user_id: serialized_table.user_id,
+                    name: serialized_table.decrypted_name.toString(),
+                    rows,
+                    values: JSON.parse(serialized_table.decrypted_values),
+                    type: serialized_table.table_type,
+                    interval: serialized_table.table_interval,
+                    created_at: serialized_table.created_at,
+                    updated_at: serialized_table.updated_at,
+                }
+                
+                return (
+                    <section className="w-full h-fit mt-15 mb-20">
+                        <Panel 
+                        name={table.name}
+                        stored_rows={table.rows}
+                        stored_values={table.values}
+                        stored_type={table.type}
+                        stored_interval={table.interval} />
+                    </section>
+                )
+            }
+        } else {
             return <section className="h-full flex flex-col justify-start items-center">
                 <BackButton />
                 <h2 className="text-center">{ lang === "es" ? "Tabla no encontrada" : "Schedule not found" }</h2>
             </section>
         }
-        const tableKey = await sql`
-        SELECT table_data_key FROM scheduler_users_tables
-        WHERE user_id = ${user_id} AND id = ${(await params).id}; 
-        `;
-        if (tableKey.rowCount === 0) {
-            return <section className="h-full flex flex-col justify-start items-center">
-                <BackButton />
-                <h2 className="text-center">{ lang === "es" ? "Ha ocurrido un error, inténtalo más tarde." : "An unexpected error has happened, please try again." }</h2>
-            </section>
-        }
-        const key = tableKey.rows[0].table_data_key;
-        const decryptedKey = await decryptKmsDataKey(key);
-        const request = await sql`
-            SELECT table_name,
-                    pgp_sym_decrypt_bytea(table_data, ${decryptedKey}) AS decrypted_table,
-                    pgp_sym_decrypt_bytea(table_rowspecs, ${decryptedKey}) AS decrypted_rowspecs,
-                    pgp_sym_decrypt_bytea(table_colspecs, ${decryptedKey}) AS decrypted_colspecs,
-                    pgp_sym_decrypt_bytea(table_values, ${decryptedKey}) AS decrypted_values,
-                    created_at,
-                    updated_at
-            FROM scheduler_users_tables
-            WHERE user_id = ${user_id} AND id = ${(await params).id};
-        `;
-        const data = request.rows[0];
-        const response = {
-            title: data.table_name as string,
-            rows: data.decrypted_table as Buffer,
-            rowSpecs: data.decrypted_rowspecs as Buffer,
-            values: data.decrypted_values as Buffer,
-            colSpecs: data.decrypted_colspecs as Buffer,
-            created_at: data.created_at as string,
-            updated_at: data.updated_at as string,
-        }
-        const title = response.title;
-        const rows = decryptToString(response.rows);
-        const rowSpecs = decryptToString(response.rowSpecs);
-        const values = decryptToString(response.values);
-        const colSpecs = decryptToString(response.colSpecs);
-        return (
-            <main className="h-full mt-10 pb-10">
-                <form className="flex flex-col justify-center items-center relative">
-                    <input type="text" name="table_id" value={ id } readOnly hidden />
-                </form>
-            </main>
-        )
     }
 }
