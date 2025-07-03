561c173bf3470f3f8bff111445723c3bbe56cc31
Jossy Solá <jossysola@gmail.com>
2025-07-03 15:38:02 -0600

    Replaces Panel directory path with client-panel

--- app/[lang]/table/new/page.tsx
+++ app/[lang]/table/new/page.tsx
@@ -1,13 +1,8 @@
 import pool from "@/app/lib/mocks/db";
-import Panel from "@/app/ui/v3/panel";
+import Panel from "@/app/ui/v3/client-panel";
 import { auth } from "@/auth";
-import { sql } from "@vercel/postgres";
-import { redirect } from "next/navigation";
 
-export default async function Page ({ params }: {
-    params: Promise<{ lang: "en" | "es" }>
-}) {
-    const lang = (await params).lang;
+export default async function Page () {
     const session = await auth();
 
     if (session && session.user) {
@@ -18,37 +13,3 @@ export default async function Page ({ params }: {
         )
     }
 }
-
-/*
-if (!session?.user) redirect(`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/login`);
-
-    if (session && session.user) {
-        const numTables = await sql`
-            SELECT num_tables 
-            FROM scheduler_users
-            WHERE id = ${session.user.id};
-        `;
-
-        if (numTables.rows && numTables.rows.length > 0) {
-            if (numTables.rows[0].num_tables < 3) {
-                return (
-                    <main className="h-full mt-10 pb-10">
-                        <form className="flex flex-col justify-center items-center relative">
-                            <TableWithProvider lang={ lang } />
-                        </form>
-                    </main>
-                )
-            } else {
-                redirect(`${process.env.NEXT_PUBLIC_ORIGIN}/${lang}/dashboard`);
-            }
-        } else {
-            return (
-                <main className="w-full h-full mt-10 pb-10">
-                    <form className="w-full flex flex-col justify-center items-center relative">
-                        <TableWithProvider lang={ lang } />
-                    </form>
-                </main>
-            )
-        }
-    }
-*/
