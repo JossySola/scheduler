08d50dc56043e47b8bd49d79ac8a75d2dac09ec1
Jossy Solá <jossysola@gmail.com>
2025-07-06 21:30:47 -0600

    Adds setConflicts, isGenerating, and setIsGenerating in Context

--- app/[lang]/table/context.ts
+++ app/[lang]/table/context.ts
@@ -1,13 +1,19 @@
 import { TableExtended } from "@/app/lib/utils-client";
-import { createContext } from "react";
+import { createContext, SetStateAction } from "react";
 
 interface TableContextType {
     table: TableExtended;
     panelUpdate: () => void;
+    setConflicts: React.Dispatch<SetStateAction<Array<string>>> | undefined;
+    isGenerating: boolean,
+    setIsGenerating: React.Dispatch<SetStateAction<boolean>> | undefined,
 }
 export const TableContext = createContext<TableContextType>({
     table: new TableExtended(""),
     panelUpdate: () => {},
+    setConflicts: undefined,
+    isGenerating: false,
+    setIsGenerating: undefined,
 });
 export const AnthropicGenerationContext = createContext({});
 export interface AnthropicGenerationType {
