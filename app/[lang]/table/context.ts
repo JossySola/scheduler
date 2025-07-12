4016729a8828044d43269b4876bc3ddec5056e22
Jossy Solá <jossysola@gmail.com>
2025-07-12 13:42:18 -0600

    Adds 'generatedRows' to context type

--- app/[lang]/table/context.ts
+++ app/[lang]/table/context.ts
@@ -5,11 +5,19 @@ interface TableContextType {
     table: TableExtended;
     panelUpdate: () => void;
     setConflicts: React.Dispatch<SetStateAction<Array<string | undefined> | undefined>> | undefined;
+    generatedRows: Array<{ 
+        colIndex: number, 
+        rowIndex: number, 
+        name: string, 
+        value: string, 
+        hasConflict: boolean 
+    }> | undefined;
 }
 export const TableContext = createContext<TableContextType>({
     table: new TableExtended(""),
     panelUpdate: () => {},
     setConflicts: undefined,
+    generatedRows: undefined,
 });
 export const AnthropicGenerationContext = createContext({});
 export interface AnthropicGenerationType {
