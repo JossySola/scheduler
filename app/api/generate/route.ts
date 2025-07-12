931a32a3dc0109fe791567b7fbdbaf71bae1ecb0
Jossy Solá <jossysola@gmail.com>
2025-07-12 13:10:49 -0600

    Separates promt into 'system' and 'prompt' object properties

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -26,7 +26,7 @@ export async function POST (request: NextRequest) {
         temperature: 1,
         output: "object",
         schema: tableGenerationSchema,
-        prompt: `
+        system: `
             You are a sophisticated scheduling algorithm designed to create strategic schedules with the given data. Your task is to generate a schedule based on the provided information to efficiently meet the specifications from the user.
 
             The rows that you are going to check are structured this way:
@@ -51,13 +51,9 @@ export async function POST (request: NextRequest) {
                     1. "colTimes": Type: number. It specifies how many rows must be filled in with a value in the current column.
                     2. "valueTimes": Type: Map<string, number>. The string specifies the "value" and the number specifies the amount of times that value must be assigned in the current column.
             
-            Generate a highly strategic schedule with the following data. The Maps and Sets are converted into Arrays:
-            **Rows**: ${JSON.stringify(rows)}
-            **Values**: ${JSON.stringify(values)}
-
             The output generated must be an Object containing:
-                1. rows: A property holding an Array of Arrays. The child arrays will be tuples. The first element of a child array will be the index of the cell (for example: "A0","B0",..., "C2", etc.), the second element will be the value assigned by you, and the third element will be a boolean to know whether or not that cell has any conflicts. Therefore, the output will be an array of arrays with tuples containing the information only about the working cells, not column or row headers.
-                2. conflicts: An array of strings containing any descriptive conflicts found while generating the schedule, as instance, any conflict with contradictory criteria. The conflict text must be in this language: ${lang}
+                1. rows: A property holding an Array of Objects. The child objects will be these properties: colIndex(number), rowIndex(number), name (i.e. "A3")(string), value(string), and hasConflict(boolean).
+                2. conflicts: An array of strings containing any descriptive conflicts found while generating the schedule, as instance, any conflict with contradictory criteria. The conflict texts must be in this language: ${lang}
 
             Rules to follow:
             1. **Randomness**
@@ -76,24 +72,64 @@ export async function POST (request: NextRequest) {
                 b. If "disabledCols" has content, leave each array's second element that have the corresponding cell name that is part of the row, with the empty string it started in Output. Keep these disabled columns in mind.
                 c. If "preferValues" has content, save them to work with the next and last specification. If it does not have content, use all values from "Values" at least once in random order. If there are less values than columns, you can also repeat them randomly. If there are more values than columns, choose the values randomly to fill the cells.
                 d. If "rowTimes" is greater than 0, randomly (order and location) fill the cells with the "preferValues"'s values if applicable, avoiding the cells with empty strings you set in step "b". If there are no columns left to apply this specification, add this issue in "conflicts". If there are some columns available but not enough for this specification to succeed, fill those available columns and annotate this in "conflicts". If there are more columns than "rowTimes", fill the amount of cells specified and the remaining fill them with empty strings. If "rowTimes" is 0, fill all available columns with any assigned or random values, and suggest on "conflicts" to use the "disabled" option, to disable the row. Any cell conflicting with a column's specification fill it as requested but add this in "conflicts".
-            4. It is important to note that the input cells are displayed as select types. So based on the "Values" array and the elements' indexes, set the prior values with the value's index as a string, so instead of words the value must be index numbers as strings.
+            4. If the "Values" array is populated, set the prior values with the value's index as a string, so instead of words the value must be index numbers as strings as the inputs are going to be a Select type. Otherwise, use words.
             
             
             This is an example of how the output should look like:
             {
                 rows: [
-                    ["B1", "1", true],
-                    ["C1", "0", true],
-                    ["B2", "0", false],
-                    ["C2", "2", false],
-                    ["B3", "0", false],
-                    ["C3", "0", false]
+                    {
+                        colIndex: 2,
+                        rowIndex: 1,
+                        name: "B1",
+                        value: "1",
+                        hasConflict: true
+                    },
+                    {
+                        colIndex: 3,
+                        rowIndex: 1,
+                        name: "C1",
+                        value: "0",
+                        hasConflict: true
+                    },
+                    {
+                        colIndex: 2,
+                        rowIndex: 2,
+                        name: "B2",
+                        value: "0",
+                        hasConflict: false
+                    },
+                    {
+                        colIndex: 3,
+                        rowIndex: 2,
+                        name: "C2",
+                        value: "2",
+                        hasConflict: false
+                    },
+                    {
+                        colIndex: 2,
+                        rowIndex: 3,
+                        name: "B3",
+                        value: "0",
+                        hasConflict: false
+                    },
+                    {
+                        colIndex: 3,
+                        rowIndex: 3,
+                        name: "C3",
+                        value: "0",
+                        hasConflict: false
+                    },
                 ],
                 conflicts: ["In the row 'Chema' it was expected to fill 5 columns but the table only has 2, resolve a contradicting specification under 'Row Specifications'.", ...],
             }
 
             This is previous output: ${previous ? JSON.stringify(previous) : null}, do not repeat it/them. 
         `,
+        prompt: `
+        Generate a highly strategic schedule with the following data and criteria:
+        **Rows**: ${JSON.stringify(rows)}
+        **Values**: ${JSON.stringify(values)}`,
     });
     return result.toTextStreamResponse();
 } 
