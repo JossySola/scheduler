3d91dd5948095e86397ced3a37baeccdcd44508c
Jossy Solá <jossysola@gmail.com>
2025-07-12 22:32:38 -0600

    Moves LottieLoader code outside <table>

--- app/ui/v3/table.tsx
+++ app/ui/v3/table.tsx
@@ -12,44 +12,41 @@ export default function Table () {
     const { table, isGenerating } = useContext(TableContext);
 
     return (
-        <section className="col-start-2 row-start-2 relative">
-            <table className="relative h-fit">
+        <section className="col-start-2 row-start-2 relative h-fit">
+            {
+                isGenerating 
+                ?  (
+                    <div className="w-[75vw] h-full backdrop-blur-xs absolute top-0 z-3 flex flex-col items-center">
+                        <LottieLoader />
+                    </div> 
+                )
+                : null
+            }
+            <table className="flex flex-col gap-2 overflow-x-scroll w-[75vw] h-fit pt-5 pb-5">
                 {
-                    isGenerating 
-                    ?  (
-                        <div className="w-[75vw] h-full backdrop-blur-xs absolute top-0 z-3 flex flex-col justify-center items-center">
-                            <LottieLoader />
-                        </div> 
-                    )
-                    : null
-                }
-                
-                <div className="flex flex-col gap-2 overflow-x-scroll w-[75vw] h-fit pt-5 pb-5">
-                    {
-                        table && table.rows.map((row: Map<string, RowType>, rowIndex: number) => {
-                            if (rowIndex === 0) {
-                                return (
-                                    <thead key={`row${rowIndex}`}>
-                                        <tr className="flex flex-row gap-2">
-                                            {
-                                                <ColHeaders row={row} rowIndex={rowIndex} />
-                                            }
-                                        </tr>
-                                    </thead>
-                                )
-                            }
+                    table && table.rows.map((row: Map<string, RowType>, rowIndex: number) => {
+                        if (rowIndex === 0) {
                             return (
-                                <tbody key={`row${rowIndex}`}>
+                                <thead key={`row${rowIndex}`}>
                                     <tr className="flex flex-row gap-2">
-                                    {
-                                        <RowHeaders row={row} rowIndex={rowIndex} />
-                                    }
+                                        {
+                                            <ColHeaders row={row} rowIndex={rowIndex} />
+                                        }
                                     </tr>
-                                </tbody>
+                                </thead>
                             )
-                        })
-                    }
-                </div>
+                        }
+                        return (
+                            <tbody key={`row${rowIndex}`}>
+                                <tr className="flex flex-row gap-2">
+                                {
+                                    <RowHeaders row={row} rowIndex={rowIndex} />
+                                }
+                                </tr>
+                            </tbody>
+                        )
+                    })
+                }
             </table>
         </section>
     )
@@ -57,9 +54,8 @@ export default function Table () {
 
 const LottieLoader = () => {
     return (
-        <div className="w-1/6 h-full">
+        <div className="w-1/6 h-full flex flex-col justify-center">
             <Lottie animationData={ animationData } loop autoplay />
         </div>
-        
     )
 }
