59bed0b9fe7eb690e1166411348d1467c40e4c0b
Jossy Solá <jossysola@gmail.com>
2025-07-28 17:22:17 -0600

    Update

--- app/ui/landing/benefits.tsx
+++ app/ui/landing/benefits.tsx
@@ -6,7 +6,7 @@ export default function Benefits () {
         <section className='w-full lg:w-[900px] p-5 flex flex-col justify-center items-center gap-20'>
             <section className="w-full flex flex-col md:flex-row justify-center items-center md:justify-between gap-10 text-center sm:text-left">
                 <h3>Adapts to your constraints</h3>
-                <div className='w-[432px] max-w-[500px]'>
+                <div className='w-[332px] max-w-[416px]'>
                     <DotLottieReact src="/assets/settings.lottie" autoplay />
                 </div>
             </section>
