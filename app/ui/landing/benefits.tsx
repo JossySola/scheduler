0245d336e101f495d262bfd9244d2059855d9c4a
Jossy Solá <jossysola@gmail.com>
2025-08-25 23:34:24 -0600

    Implements Motion

--- app/ui/landing/benefits.tsx
+++ app/ui/landing/benefits.tsx
@@ -1,48 +1,75 @@
 "use client"
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { useParams } from 'next/navigation';
+import { motion } from 'motion/react';
 
 export default function Benefits () {
-    const params = useParams<{ lang: "en" | "es"}>();
+    const params = useParams<{ lang: "en" | "es" }>();
     const { lang } = params;
+    
+    const container = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { delayChildren: 0.5 } },
+    }
+    const item = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    }
     return (
-        <section className='w-full lg:w-[900px] p-5 flex flex-col justify-center items-center gap-20'>
-            <section className="w-full flex flex-col md:flex-row justify-center items-center md:justify-between gap-10 text-center sm:text-left">
-                <h3>
+        <section className='w-full lg:w-[900px] p-5 flex flex-col justify-center items-center gap-y-[50vh]'>
+            <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="text-center w-full">
+            {
+                lang === "es" 
+                ? "Planificación de trabajo, coordinación de horarios y bloques de tiempo ajustables"
+                : "Workforce planning, shift coordination, and custom time blocks." 
+            }
+            </motion.h2>
+
+            <motion.section 
+            variants={container}
+            initial="hidden"
+            whileInView="visible"
+            className="w-full flex flex-col md:flex-row justify-center items-center md:justify-between gap-10 text-center sm:text-left">
+                <motion.h3
+                variants={item}>
                     {
                         lang === "es"
                         ? "Se adapta a tus especificaciones"
                         : "Adapts to your constraints"
                     }
-                </h3>
-                <div className='max-w-[416px]'>
-                    <DotLottieReact src="/assets/settings.lottie" autoplay />
-                </div>
-            </section>
-            <section className="w-full flex flex-col sm:flex-row justify-center items-center md:justify-between gap-10 text-center sm:text-left">
-                <h3>
+                </motion.h3>
+                <DotLottieReact src="/assets/settings.lottie" autoplay loop mode='bounce' />
+            </motion.section>
+
+            <motion.section
+            variants={container}
+            initial="hidden"
+            whileInView="visible" 
+            className="w-full flex flex-col sm:flex-row justify-center items-center md:justify-between gap-10 text-center sm:text-left">
+                <motion.h3 variants={item}>
                     {
                         lang === "es"
                         ? "Te da todo el control"
                         : "Gives you full control"
                     }
-                </h3>
-                <div className='max-w-[500px]'>
-                    <DotLottieReact src='/assets/input-types.lottie' speed={0.5} autoplay loop />
-                </div>
-            </section>
-            <section className="w-full flex flex-col sm:flex-row justify-center items-center md:justify-between gap-10 text-center sm:text-left">
-                <h3>
+                </motion.h3>
+                <DotLottieReact src='/assets/input-types.lottie' speed={0.5} autoplay loop />
+            </motion.section>
+
+            <motion.section
+            variants={container}
+            initial="hidden"
+            whileInView="visible" 
+            className="w-full flex flex-col sm:flex-row justify-center items-center gap-10 text-center">
+                <motion.h3 variants={item}>
                     {
                         lang === "es"
                         ? "Crece junto tu equipo"
                         : "Scales with your team"
                     }
-                </h3>
-                <div className='max-w-[500px]'>
-
-                </div>
-            </section>
+                </motion.h3>
+                <DotLottieReact src='/assets/team.lottie' autoplay loop />
+            </motion.section>
         </section>
     )
 }
