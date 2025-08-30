308347b04dab45b3b1caf5ec9e64e29904b6b46b
Jossy Solá <jossysola@gmail.com>
2025-08-30 02:09:12 -0600

    Creates sorting icons

--- app/ui/icons.tsx
+++ app/ui/icons.tsx
@@ -503,3 +503,56 @@ export function WarningFill ({ width = 16, height = 16, color = "currentcolor"})
         </svg>
     )
 }
+export function SortAscending ({ width = 16, height = 16, color = "currentcolor" }) {
+    return (
+        <svg 
+        data-testid="geist-icon" 
+        height={ height }
+        strokeLinejoin="round" 
+        style={{ color }}
+        viewBox="0 0 16 16" 
+        width={ width }>
+            <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            d="M12.5303 1.21967C12.2374 0.926777 11.7626 0.926777 11.4697 1.21967L9.21967 3.46967L8.68934 4L9.75 5.06066L10.2803 4.53033L11.25 3.56066V13.25V14H12.75V13.25V3.56066L13.7197 4.53033L14.25 5.06066L15.3107 4L14.7803 3.46967L12.5303 1.21967ZM1.75 4H1V5.5H1.75H5.25H6V4H5.25H1.75ZM1.75 8.25H1V9.75H1.75H4.25H5V8.25H4.25H1.75ZM1.75 12.5H1V14H1.75H7.25H8V12.5H7.25H1.75Z" 
+            fill="currentColor">
+            </path>
+        </svg>
+    )
+}
+export function SortDescending ({ width = 16, height = 16, color = "currentcolor" }) {
+    return (
+        <svg 
+        data-testid="geist-icon" 
+        height={ height } 
+        strokeLinejoin="round" 
+        viewBox="0 0 16 16" 
+        width={ width } 
+        style={{ color }}>
+            <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            d="M1.75 12H1V10.5H1.75H5.25H6V12H5.25H1.75ZM1.75 7.75H1V6.25H1.75H4.25H5V7.75H4.25H1.75ZM1.75 3.5H1V2H1.75H7.25H8V3.5H7.25H1.75ZM12.5303 14.7803C12.2374 15.0732 11.7626 15.0732 11.4697 14.7803L9.21967 12.5303L8.68934 12L9.75 10.9393L10.2803 11.4697L11.25 12.4393V2.75V2H12.75V2.75V12.4393L13.7197 11.4697L14.25 10.9393L15.3107 12L14.7803 12.5303L12.5303 14.7803Z" 
+            fill="currentColor">
+            </path>
+        </svg>
+    )
+}
+export function Sort ({ width = 16, height = 16, color = "currentcolor" }) {
+    return (
+        <svg 
+        data-testid="geist-icon" 
+        height={ height } 
+        strokeLinejoin="round" 
+        viewBox="0 0 16 16"
+        width={ width } 
+        style={{ color }}>
+            <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            d="M1 0H1.75H14.25H15V0.75V3V3.31066L14.7803 3.53033L10.5 7.81066V15.25V16H9.75H9H8.7816L8.59734 15.8827L5.84734 14.1327L5.5 13.9117V13.5V7.81066L1.21967 3.53033L1 3.31066V3V0.75V0ZM2.5 1.5V2.68934L6.78033 6.96967L7 7.18934V7.5V13.0883L9 14.361V7.5V7.18934L9.21967 6.96967L13.5 2.68934V1.5H2.5Z" fill="currentColor">
+            </path>
+        </svg>
+    )
+}
