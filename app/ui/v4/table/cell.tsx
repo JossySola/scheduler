c893e54799031e29560870cb6380626a9b00c0a2
Jossy Solá <jossysola@gmail.com>
2025-09-04 23:32:19 -0600

    Modifies conditional rendering

--- app/ui/v4/table/cell.tsx
+++ app/ui/v4/table/cell.tsx
@@ -3,10 +3,10 @@ import { motion } from "motion/react";
 import { VTData } from "@/app/hooks/custom"
 import { Input, Select, SelectItem } from "@heroui/react";
 import { Getter, Table } from "@tanstack/react-table"
-import { memo, useState } from "react";
+import { useState } from "react";
 import { Header } from "../../icons";
 
-const CellRenderer = memo(function ({getValue, row, column, table, values}: {
+export default function CellRenderer ({getValue, row, column, table, values}: {
     getValue: Getter<unknown>,
     row: { index: number },
     column: { id: string },
@@ -24,59 +24,40 @@ const CellRenderer = memo(function ({getValue, row, column, table, values}: {
         setSelection(newSelection);
         table.options.meta?.updateData(row.index, column.id, newSelection);
     }
-
-    if (values && values.size > 0) {
-        if (row.index !== 0 && column.id !== "A") {
-            const isVirtual = values.size > 10;
-            return (
-            <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
-                <Select
-                isVirtualized={isVirtual}
-                selectionMode="single"
-                variant="bordered"
-                size="lg"
-                classNames={{ mainWrapper: "w-[55vw] sm:w-64" }}
-                selectedKeys={selection}
-                onChange={(event) => handleSelectionChange(event.target.value)}
-                >
-                {Array.from(values.values()).map((val, idx) => (
-                    <SelectItem key={idx}>{val}</SelectItem>
-                ))}
-                </Select>
-            </motion.div>
-            );
-        }
+    if (values.size > 0 && row.index !== 0 && column.id !== "A") {
+        const isVirtual = values.size > 10;
         return (
-            <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
-            <Input
-                variant="bordered"
-                classNames={{
-                mainWrapper: "w-[55vw] sm:w-64",
-                inputWrapper: "h-[48px]",
-                }}
-                value={(value as string) ?? ""}
-                onValueChange={setValue}
-                startContent={row.index === 0 || column.id === "A" ? <Header color="#3f3f46" /> : null}
-                onBlur={onBlur}
-            />
-            </motion.div>
-        );        
+        <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
+            <Select
+            isVirtualized={isVirtual}
+            selectionMode="single"
+            variant="bordered"
+            size="lg"
+            classNames={{ mainWrapper: "w-[55vw] sm:w-64" }}
+            selectedKeys={selection}
+            onChange={(event) => handleSelectionChange(event.target.value)}
+            >
+            {Array.from(values.values()).map((val, idx) => (
+                <SelectItem key={idx}>{val}</SelectItem>
+            ))}
+            </Select>
+        </motion.div>
+        );
     } else {
         return (
             <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
-            <Input
-                variant="bordered"
-                classNames={{
-                mainWrapper: "w-[55vw] sm:w-64",
-                inputWrapper: "h-[48px]",
-                }}
-                value={(value as string) ?? ""}
-                onValueChange={setValue}
-                startContent={row.index === 0 || column.id === "A" ? <Header color="#3f3f46" /> : null}
-                onBlur={onBlur}
-            />
+                <Input
+                    variant="bordered"
+                    classNames={{
+                    mainWrapper: "w-[55vw] sm:w-64",
+                    inputWrapper: "h-[48px]",
+                    }}
+                    value={(value as string) ?? ""}
+                    onValueChange={setValue}
+                    startContent={row.index === 0 || column.id === "A" ? <Header color="#3f3f46" /> : null}
+                    onBlur={onBlur}
+                />
             </motion.div>
         );
     }
-});
-export default CellRenderer;
+}
