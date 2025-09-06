005019c7c70867dacb40314548254b36110a270d
Jossy Solá <jossysola@gmail.com>
2025-09-05 20:39:35 -0600

    Removes cookies chip

--- app/ui/atoms/atom-footer.tsx
+++ app/ui/atoms/atom-footer.tsx
@@ -5,7 +5,6 @@ import FooterCookies from "./atom-footer-cookie";
 
 export default async function Footer () {
     return <footer className="w-full flex flex-row justify-center items-center gap-1 sm:gap-3 p-5 sm:p-3 fixed bottom-0 bg-transparent z-10">
-        <FooterCookies />
         <FooterContact />
         <PowerWithAnthropic />
         <FooterLegal />
