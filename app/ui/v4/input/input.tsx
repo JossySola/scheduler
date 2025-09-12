fd548243b76b3ed455ebcc9df43dfea14b0bafe6
Jossy Solá <jossysola@gmail.com>
2025-09-11 21:20:46 -0600

    Creates component

---
+++ app/ui/v4/input/input.tsx
@@ -0,0 +1,58 @@
+"use client"
+import { motion } from "motion/react";
+import { Input as InputHeroUI, SharedSelection } from "@heroui/react";
+import HeaderModal from "../modal/header-modal";
+import { Header } from "../../icons";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useParams } from "next/navigation";
+import { ColumnDef, Table } from "@tanstack/react-table";
+import { VTData } from "@/app/hooks/custom";
+
+export default function Input({ initialValue, handleDuplicates, isDuplicate, table, row, column, interval, headerType, setInterval, setHeaderType }: {
+    initialValue: unknown,
+    handleDuplicates: (val: string) => void,
+    isDuplicate: boolean,
+    table: Table<VTData>,
+    row: { index: number },
+    column: { id: string },
+    interval: number,
+    headerType: SharedSelection,
+    setInterval: Dispatch<SetStateAction<number>>,
+    setHeaderType: Dispatch<SetStateAction<SharedSelection>>,   
+}) {
+    const { lang } = useParams<{ lang: "es" | "en" }>();
+    const [value, setValue] = useState<string>(initialValue as string ?? ""); 
+    const onBlur = () => {
+        table.options.meta?.updateData(row.index, column.id, value);
+    };    
+    return (
+        <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
+            <InputHeroUI
+                variant="bordered"
+                classNames={{
+                mainWrapper: "w-[63vw] sm:w-64",
+                inputWrapper: "h-[48px]",
+                }}
+                value={(value as string) ?? ""}
+                onChange={e => handleDuplicates(e.target.value)}
+                onValueChange={setValue}
+                startContent={
+                    row.index === 0 || column.id === "A" 
+                    ? row.index === 0 && column.id === "B" 
+                        ? <HeaderModal interval={interval} setInterval={setInterval} headerType={headerType} setHeaderType={setHeaderType} />
+                        : <Header color={ isDuplicate ? "oklch(57.7% 0.245 27.325)" : "#3f3f46"} /> 
+                    : null
+                }
+                onBlur={onBlur}
+                errorMessage={
+                    isDuplicate 
+                    ? lang === "en" 
+                        ? "Duplicate value" 
+                        : "Valor duplicado"
+                    : undefined
+                }
+                isInvalid={isDuplicate}
+            />
+        </motion.div>
+    )
+}
---
+++ app/ui/v4/input/select.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { motion } from "motion/react";
+import { Select as SelectHeroUi, SelectItem } from "@heroui/react";
+import { useState } from "react";
+import { Table } from "@tanstack/react-table";
+import { VTData } from "@/app/hooks/custom";
+
+export default function Select({ value, table, row, column, values }: {
+    value: string,
+    table: Table<VTData>,
+    row: { index: number },
+    column: { id: string },
+    values: Set<string>,
+}) {
+    const [selection, setSelection] = useState<string>("");
+    const handleSelectionChange = (newSelection: string) => {
+        setSelection(newSelection);
+        table.options.meta?.updateData(row.index, column.id, newSelection);
+    }    
+    const isVirtual = values.size > 10;
+    return (
+        <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
+            <SelectHeroUi
+            isVirtualized={isVirtual}
+            selectionMode="single"
+            variant="bordered"
+            size="lg"
+            classNames={{ mainWrapper: "w-[63vw] sm:w-64" }}
+            selectedKeys={selection}
+            onChange={(event) => handleSelectionChange(event.target.value)}
+            >
+            {Array.from(values.values()).map((val, idx) => (
+                <SelectItem key={idx}>{`${idx} - ${val} `}</SelectItem>
+            ))}
+            </SelectHeroUi>
+        </motion.div>        
+    )
+}
