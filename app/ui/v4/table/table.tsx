5feef9028aa399d936b119d46c93d39e63865109
Jossy Solá <jossysola@gmail.com>
2025-09-21 20:33:35 -0600

    Implements stored data into custom hook and modifies styling

--- app/ui/v4/table/table.tsx
+++ app/ui/v4/table/table.tsx
@@ -1,23 +1,63 @@
 "use client"
 import { useVirtualizedTable } from "@/app/hooks/custom";
-import { Button } from "@heroui/react";
+import { Button, Input } from "@heroui/react";
 import { flexRender } from "@tanstack/react-table";
 import Filter from "../input/filter";
-import { ChevronDoubleDown, ChevronDoubleLeft, ChevronDoubleRight, ChevronDoubleUp, Sort, SortAscending, SortDescending } from "../../icons";
-import { useParams } from "next/navigation";
+import { ArrowCircleLeft, ChevronDoubleDown, ChevronDoubleLeft, ChevronDoubleRight, ChevronDoubleUp, Sort, SortAscending, SortDescending } from "../../icons";
+import { useParams, useRouter } from "next/navigation";
 import Settings from "../drawer/settings";
+import SaveButton from "../button/save";
 
-export default function Table() {
-    const params = useParams<{ lang: "en" | "es" }>();
+export default function Table({storedData}: {
+    storedData?: { 
+    user_id: string,
+    name: string,
+    data: Array<{ [key: string]: string }>,
+    values: Array<string>,
+    type: Array<string>,
+    interval: number,
+    cols_specs: { numberOfRows: { [key: string]: number }, amountOfValues: { [key: string]: Array<number> } },
+    rows_specs: { disable: { [key: number]: boolean }, count: { [key: string]: number }, enabledValues: { [key: string]: Array<string> }, enabledColumns: { [key: string]: Array<string> } },
+    created_at: number,
+    updated_at: number,
+    cols_num: Array<string>,
+    }
+}) {
+    const router = useRouter();
+    const {lang} = useParams<{ lang: "en" | "es" }>();
     const { 
         table,
-        setData,
         controls,
+        getTableStates,
         state,
         setter,
-    } = useVirtualizedTable();
+    } = useVirtualizedTable(storedData);
     
     return (
+        <>
+        <div className="w-full md:w-5/6 justify-self-center grid grid-cols-[1fr_3fr_1fr] mb-10 px-5">
+            <header className="col-start-1 col-end-1 flex flex-row justify-start items-center gap-5">
+                <Button
+                isIconOnly
+                size="lg"
+                aria-label="go back button"
+                variant="light"
+                onPress={() => router.back() }><ArrowCircleLeft width={32} height={32} /></Button>
+            </header>
+            <Input
+            isClearable
+            className="w-full"
+            variant="underlined"
+            size="lg"
+            label={lang === "es" ? "Título" : "Title"}
+            value={state.title}
+            onValueChange={setter.setTitle}
+            classNames={{
+                input: "text-2xl",
+                clearButton: "p-0 top-[65%]",
+            }} />
+        </div>
+
         <section className="w-full px-5 md:w-5/6 grid grid-rows-[auto_auto] grid-cols-[auto_1fr] justify-self-center my-15">
             <Settings 
             values={state.values} 
@@ -28,23 +68,27 @@ export default function Table() {
             rowSpecs={state.rowSpecs}
             setColSpecs={setter.setColSpecs}
             setRowSpecs={setter.setRowSpecs} />
-            <div className="col-start-2 col-span-1 flex flex-row gap-2">
-                <Button 
-                isIconOnly
-                className="p-2"
-                size="lg"
-                variant="bordered" 
-                onPress={() => controls.handleDeleteColumn()}>
-                    <ChevronDoubleLeft width={32} height={32} />
-                </Button>
-                <Button 
-                isIconOnly
-                className="p-2"
-                size="lg"
-                aria-label={params.lang === "es" ? "Añadir columna" : "Add column"} 
-                onPress={() => controls.handleAddColumn()}>
-                    <ChevronDoubleRight width={32} height={32}/>
-                </Button>
+            <div className="col-start-2 col-span-1 flex flex-row justify-between gap-2">
+                <div className="flex flex-row gap-2">
+                    <Button 
+                    isIconOnly
+                    className="p-2"
+                    size="lg"
+                    variant="bordered" 
+                    onPress={() => controls.handleDeleteColumn()}>
+                        <ChevronDoubleLeft width={32} height={32} />
+                    </Button>
+                    <Button 
+                    isIconOnly
+                    className="p-2"
+                    size="lg"
+                    aria-label={lang === "es" ? "Añadir columna" : "Add column"} 
+                    onPress={() => controls.handleAddColumn()}>
+                        <ChevronDoubleRight width={32} height={32}/>
+                    </Button>
+                </div>
+
+                <SaveButton states={getTableStates()} />
             </div>
             <div className="row-start-2 row-span-1 flex flex-col gap-2 items-end w-full pr-5 mt-5">
                 <Button 
@@ -52,7 +96,7 @@ export default function Table() {
                 className="p-2"
                 size="lg"
                 variant="bordered"
-                aria-label={params.lang === "es" ? "Eliminar fila" : "Delete row"} 
+                aria-label={lang === "es" ? "Eliminar fila" : "Delete row"} 
                 onPress={() => controls.handleDeleteRow()}>
                     <ChevronDoubleUp width={32} height={32} />
                 </Button>
@@ -60,7 +104,7 @@ export default function Table() {
                 isIconOnly
                 className="p-2"
                 size="lg"
-                aria-label={params.lang === "es" ? "Añadir fila" : "Add row"}                 
+                aria-label={lang === "es" ? "Añadir fila" : "Add row"}                 
                 onPress={() => controls.handleAddRow()}>
                     <ChevronDoubleDown width={32} height={32} />
                 </Button>
@@ -123,6 +167,7 @@ export default function Table() {
                 }
                 </tbody>
             </table>
-        </section>
+        </section>        
+        </>
     )
 }
