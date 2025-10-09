034d49fb82587282c449419f34fc8ca6c665de7c
Jossy Solá <jossysola@gmail.com>
2025-10-08 20:29:55 -0600

    Removes h2 and curly wrap

--- app/[lang]/signup/page.tsx
+++ app/[lang]/signup/page.tsx
@@ -6,9 +6,6 @@ export default async function Page ({ params }: {
     const lang = (await params).lang;
 
     return <section className="h-fit flex flex-col justify-start items-center p-5 mb-10">
-        <h2 className="tracking-tight">{ lang === "es" ? "Regístrate" : "Create an account" }</h2>
-        {
-            <SignupForm lang={ lang as "en" | "es" } />
-        }
+        <SignupForm lang={ lang as "en" | "es" } />
     </section>
 }
