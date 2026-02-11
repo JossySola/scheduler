8db30a061020e3c29b939e9655ddb1c66d4a251f
Jossy Solá <jossysola@gmail.com>
2026-02-11 14:43:58 -0600

    Adds projects' configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,24 @@ export default defineConfig({
             enabled: true,
             reporter: ['html'],
         },
+        projects: [
+            'app/lib/tests/*',
+            {
+                extends: true,
+                test: {
+                    include: ['unit-testing/*.server.test.{ts,js}'],
+                    name: 'Server-side tests',
+                    environment: 'node',
+                }
+            },
+            {
+                extends: true,
+                test: {
+                    include: ['unit-testing/*.client.test.{ts,js}'],
+                    name: 'Client-side tests',
+                    environment: 'happy-dom',
+                }
+            }
+        ]
     }
 })
