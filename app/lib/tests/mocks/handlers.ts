04ffc98998f9eb43f5347c763ed3cfd8839b06c0
Jossy Solá <jossysola@gmail.com>
2026-02-28 22:50:30 -0600

    Creates new http endpoint

--- app/lib/tests/mocks/handlers.ts
+++ app/lib/tests/mocks/handlers.ts
@@ -1,7 +1,11 @@
 import { http, HttpResponse } from "msw";
+import mockExposedPasswords from "./mock-exposed-passwords";
 
 export const handlers = [
     http.post("http://localhost:3000/api/argon2/verify", async () => {
         return HttpResponse.json({ isValid: true });
     }),
-]
+    http.get("https://api.pwnedpasswords.com/range/:range", async () => {
+        return HttpResponse.text(mockExposedPasswords);
+    }),
+];
