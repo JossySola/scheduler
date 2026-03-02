3c740e24cfa3c3fd0eef8ad1278f5539464e2736
Jossy Solá <jossysola@gmail.com>
2026-03-02 00:52:49 -0600

    Modifies Zod verification

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,101 +3,43 @@ import crypto, { randomBytes } from "crypto";
 import pool from "./tests/mocks/db";
 import sgMail from "@sendgrid/mail";
 import * as z from "zod/v4";
-import { KMSDataKey, UserResponse, UtilResponse } from "./definitions";
+import { KMSDataKey, UtilResponse } from "./definitions";
 import { SignatureV4 } from "@aws-sdk/signature-v4";
 import { Sha256 } from "@aws-crypto/sha256-js";
 import { DecryptCommand, KMSClient } from "@aws-sdk/client-kms";
 import { sql } from "@vercel/postgres";
 
-export async function getUserFromDb (username: string, password: string): Promise<UtilResponse & UserResponse> {
-  if (!username || !password) {
-    return {
-      message: "Data missing",
-      provider: "",
-      ok: false,
-    }
-  }
-  
-  try {
-    const user = await sql`
-        SELECT * FROM scheduler_users 
-            WHERE username = ${username} OR email = ${username};
-    `;
-    
-    if (user.rowCount === 0) {
-      return {
-        message: "User not found",
-        provider: "",
-        ok: false,
-      }
-    }
-    
-    const userRecord = user.rows[0];
-    
-    if (userRecord.password === null) {
-      const provider = await sql`
-        SELECT provider FROM scheduler_users_providers
-        WHERE email = ${userRecord.email};
-      `;
-      const name = provider.rowCount === 2 ? `${provider.rows[0].provider} and ${provider.rows[1].provider}` : `${provider.rows[0]}`;
-
-      return {
-        message: `This account uses ${name} authentication.`,
-        provider: name,
-        ok: false
-      }
-    }
-    const { verify } = await import('argon2');
-    const hashVerified = await verify(userRecord.password, password);
-
-    if (hashVerified) {
-      return {
-          id: userRecord.id,
-          name: userRecord.name,
-          username: userRecord.username,
-          email: userRecord.email,
-          role: userRecord.role,
-          message: "",
-          ok: true
-      };
-    }
-    throw new Error ("Invalid credentials");
-  } catch (error) {
-    return {
-      message: "Invalid credentials",
-      ok: false,
-    }
-  }
-}
 export async function isPasswordPwned (password: string): Promise<number | UtilResponse> {
-  if (!password) throw new Error("No password received")
   try {
-      // API requires password to be hashed with SHA1
-      const hashed = crypto.createHash('sha1').update(password).digest('hex');
-      // "range" endpoint requires only the first 5 characters of the hashed password
-      const range = hashed.slice(0,5);
-      const suffixToCheck = hashed.slice(5).toUpperCase();
-      const response = await fetch(`https://api.pwnedpasswords.com/range/${range}`);
-      // The API returns a plain text response, not a JSON
-      const text = await response.text();
-      // For each \n break, convert the plain text into an Array
-      const lines = text.split('\n');
-      for (const line of lines) {
-          // The format of each line is divided by a ":", the left part is the suffix and the right part is the count
-          const [hashSuffix, count] = line.split(':');
-          // If the rest of the hashed password is found on the exposed list
-          if (hashSuffix.includes(suffixToCheck)) {
-              // return the count part as a number
-              return parseInt(count, 10);
-          }
+    const verifyInput = z.string().nonempty({ message: "Password must be provided" }).safeParse(password);
+    if (!verifyInput.success) throw new Error(verifyInput.error?.issues?.[0]?.message || "Unknown validation error");
+    // API requires password to be hashed with SHA1
+    const hashed = crypto.createHash('sha1').update(password).digest('hex');
+    // "range" endpoint requires only the first 5 characters of the hashed password
+    const range = hashed.slice(0,5);
+    console.log(range)
+    const suffixToCheck = hashed.slice(5).toUpperCase();
+    const response = await fetch(`https://api.pwnedpasswords.com/range/${range}`);
+    // The API returns a plain text response, not a JSON
+    const text = await response.text();
+    // For each \n break, convert the plain text into an Array
+    const lines = text.split('\n');
+    for (const line of lines) {
+      // The format of each line is divided by a ":", the left part is the suffix and the right part is the count
+      const [hashSuffix, count] = line.split(':');
+      // If the rest of the hashed password is found on the exposed list
+      if (hashSuffix.includes(suffixToCheck)) {
+          // return the count part as a number
+          return parseInt(count, 10);
       }
-      // If the remaining part of the hashed password is not found on the list, return 0
-      return 0;
+    }
+    // If the remaining part of the hashed password is not found on the list, return 0
+    return 0;
   } catch (error) {
-      return {
-          message: "Unknown error from exposed password checking.",
-          ok: false
-      };
+    return {
+      message: "Unknown error from exposed password checking.",
+      ok: false
+    };
   }
 }
 export async function sendResetPasswordConfirmation (email: string): Promise<UtilResponse> {
