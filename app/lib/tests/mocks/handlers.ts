2a3430873bc4185803bcefff78c5cb17f2c03361
Jossy Solá <jossysola@gmail.com>
2026-03-02 00:44:47 -0600

    Creates two interceptions

--- app/lib/tests/mocks/handlers.ts
+++ app/lib/tests/mocks/handlers.ts
@@ -5,7 +5,10 @@ export const handlers = [
     http.post("http://localhost:3000/api/argon2/verify", async () => {
         return HttpResponse.json({ isValid: true });
     }),
-    http.get("https://api.pwnedpasswords.com/range/:range", async () => {
+    http.get("https://api.pwnedpasswords.com/range/21BD1", async () => {
         return HttpResponse.text(mockExposedPasswords);
     }),
+    http.get("https://api.pwnedpasswords.com/range/8be3c", async () => {
+        return HttpResponse.text("");
+    })
 ];
