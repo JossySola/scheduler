67b974ef13e34411a6e84a852353aaa5038ddb41
Jossy Solá <jossysola@gmail.com>
2026-03-13 15:12:20 -0600

    Removes HeroUI content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
