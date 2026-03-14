3c07c6409f948bb6cfca62f1dc80046ba566e39c
Jossy Solá <jossysola@gmail.com>
2026-03-14 16:55:24 -0600

    Adds sendEmailSchema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -6,4 +6,11 @@ export const signUpSchema = z.object({
     email: z.email().nonempty(),
     password: z.string().min(8),
     confirmPassword: z.string().min(8)
+});
+export const sendEmailSchema = z.object({
+    to: z.email(),
+    subject: z.string().nonempty(),
+    text: z.string().nonempty(),
+    url: z.optional(z.string()),
+    linkText: z.optional(z.string()),
 })
