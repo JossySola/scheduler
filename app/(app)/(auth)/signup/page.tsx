23b88ae497726517e702219c78b162500251beb8
Jossy Solá <jossysola@gmail.com>
2026-03-15 02:50:40 -0600

    Adds signin providers section

--- app/(app)/(auth)/signup/page.tsx
+++ app/(app)/(auth)/signup/page.tsx
@@ -1,7 +1,12 @@
+import SignInProviders from "@/features/auth/components/signin-providers";
 import SignUpForm from "@/features/auth/components/signup-form";
 
 export default async function SignUpPage() {
     return (
-        <SignUpForm />
+        <>
+        <SignInProviders />
+        <SignUpForm />        
+        </>
+
     )
 }
