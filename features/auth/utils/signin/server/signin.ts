22d1a5a2073dbcfb07f7301386d5a6d006461c3b
Jossy Solá <jossysola@gmail.com>
2026-03-18 01:51:50 -0600

    Adds typing and console errors

--- features/auth/utils/signin/server/signin.ts
+++ features/auth/utils/signin/server/signin.ts
@@ -1,24 +1,31 @@
 import z from "zod";
 import getMultipleZodErrors from "../../../../../lib/utils/getMultipleZodErrors";
 import { auth } from "@/auth";
+import { SignInUsernameData } from "@/lib/definitions";
 
 export default async function handleSignIn(username: string, password: string) {
     try {
-        const emailVerification = z.email({ error: "Invalid input: expected email"}).nonempty({ error: "Input empty" }).safeParse(username);
+        const emailVerification = z.string({ error: "Invalid input: expected string"}).nonempty({ error: "Input empty" }).safeParse(username);
         const passwordVerification = z.string({ error: "Invalid input: expected string" }).min(8, { error: "Password must be minimum 8 characters long" }).safeParse(password);
         if (!emailVerification.success || !passwordVerification.success) {
             const messages = getMultipleZodErrors([emailVerification, passwordVerification]);
             throw new Error(`Zod Errors: ${messages}`);
         }
-        const data = await auth.api.signInUsername({
+        const data: SignInUsernameData = await auth.api.signInUsername({
             body: {
                 username,
                 password,
             }
         })
         return data;
-    } catch (e) {
-        console.error(e)
-        throw new Error("Failed signing in");
+    } catch (e: any) {
+        console.error(e);
+        console.error(e.message);
+        if (e.message.includes("Invalid username")) {
+            throw new Error("Invalid credentials");
+        } else if (e.message.includes("Email not verified")) {
+            throw new Error("Email not verified");
+        }
+        throw new Error(`${e.message}`);
     }
 }
