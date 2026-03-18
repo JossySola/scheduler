0322d014cea070af6be0ac1fcf31ee4c47a3de31
Jossy Solá <jossysola@gmail.com>
2026-03-18 01:49:03 -0600

    Creates SignInUsernameData type

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -23,5 +23,20 @@ export type SignUpEmailData = {
         image?: string | null | undefined;
         username?: string | null | undefined;
         displayUsername?: string | null | undefined; 
-    }
+    };
+}
+export type SignInUsernameData = {
+    token: string;
+    user: {
+        id: string;
+        createdAt: Date;
+        updatedAt: Date;
+        email: string;
+        emailVerified: boolean;
+        name: string;
+        image?: string | null | undefined;
+    } & {
+        username: string;
+        displayUsername: string;
+    };
 }
