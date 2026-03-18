0714dc8a7f0b0cede1027fa658eee9d53afbc6c2
Jossy Solá <jossysola@gmail.com>
2026-03-18 01:49:32 -0600

    Adds error message to all schemas

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,16 +1,20 @@
 import z from "zod";
 
 export const signUpSchema = z.object({
-    name: z.string().nonempty(),
-    username: z.string().nonempty(),
-    email: z.email().nonempty(),
-    password: z.string().min(8),
-    confirmPassword: z.string().min(8)
+    name: z.string({ error: "Invalid input format" }).nonempty({ error: "This field should not be empty" }),
+    username: z.string({ error: "Invalid input format" }).nonempty({ error: "This field should not be empty" }),
+    email: z.email({ error: "Invalid email address" }).nonempty({ error: "This field should not be empty" }),
+    password: z.string({ error: "Invalid input format" }).min(8, { error: "Password must be minimum 8 characters long" }),
+    confirmPassword: z.string({ error: "Invalid input format" }).min(8, { error: "Password must be minimum 8 characters long" }),
 });
-export const sendEmailSchema = z.object({
-    to: z.email(),
-    subject: z.string().nonempty(),
-    text: z.string().nonempty(),
-    url: z.optional(z.string()),
-    linkText: z.optional(z.string()),
+export const signInSchema = z.object({
+    username: z.string({ error: "Invalid input format" }).nonempty({ error: "This field should not be empty" }),
+    password: z.string({ error: "Invalid input format" }).min(8, { error: "Password must be minimum 8 characters long" }),
 })
+export const sendEmailSchema = z.object({
+    to: z.email({ error: "Invalid email address" }),
+    subject: z.string({ error: "Invalid input format" }).nonempty({ error: "This field must not be empty" }),
+    text: z.string({ error: "Invalid input format" }).nonempty({ error: "This field must not be empty" }),
+    url: z.optional(z.string({ error: "Invalid input format" })),
+    linkText: z.optional(z.string({ error: "Invalid input format" })),
+});
