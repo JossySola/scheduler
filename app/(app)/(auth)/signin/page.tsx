6b4f56d3fbe8b6f72b5c8d48db66a410daeb2d6b
Jossy Solá <jossysola@gmail.com>
2026-03-18 01:56:54 -0600

    Adds <SignInProviders>

--- app/(app)/(auth)/signin/page.tsx
+++ app/(app)/(auth)/signin/page.tsx
@@ -1,5 +1,11 @@
+import SignInForm from "@/features/auth/components/signin-form";
+import SignInProviders from "@/features/auth/components/signin-providers";
+
 export default async function Page() {
     return (
-        <></>
+        <section>
+            <SignInProviders />
+            <SignInForm />        
+        </section>
     )
 }
