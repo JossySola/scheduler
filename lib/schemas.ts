57c12bccc3d265d58f0a3341b794c3408d5ea996
Jossy Solá <jossysola@gmail.com>
2026-03-21 01:31:38 -0600

    Adds resetPasswordSchema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -18,3 +18,7 @@ export const sendEmailSchema = z.object({
     url: z.optional(z.string({ error: "Invalid input format" })),
     linkText: z.optional(z.string({ error: "Invalid input format" })),
 });
+export const resetPasswordSchema = z.object({
+    password: z.string({ error: "Invalid input format" }).min(8, { message: "Password must be minimum 8 characters long" }),
+    token: z.string({ error: "Invalid input format" }).nonempty({ error: "Token is missing" }),
+});
