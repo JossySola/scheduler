e9ff391e6caa1eca62189c359d3bb0a08b357756
Jossy Solá <jossysola@gmail.com>
2026-03-21 01:33:58 -0600

    Adds rememberMe and callbackURL properties to auth.api

--- features/auth/utils/signin/server/signin.ts
+++ features/auth/utils/signin/server/signin.ts
@@ -1,11 +1,11 @@
 import z from "zod";
-import getMultipleZodErrors from "../../../../../lib/utils/getMultipleZodErrors";
+import getMultipleZodErrors from "@/lib/utils/getMultipleZodErrors";
 import { auth } from "@/auth";
 import { SignInUsernameData } from "@/lib/definitions";
 
 export default async function handleSignIn(username: string, password: string) {
     try {
-        const emailVerification = z.string({ error: "Invalid input: expected string"}).nonempty({ error: "Input empty" }).safeParse(username);
+        const emailVerification = z.string({ error: "Invalid input: expected string" }).nonempty({ error: "Input empty" }).safeParse(username);
         const passwordVerification = z.string({ error: "Invalid input: expected string" }).min(8, { error: "Password must be minimum 8 characters long" }).safeParse(password);
         if (!emailVerification.success || !passwordVerification.success) {
             const messages = getMultipleZodErrors([emailVerification, passwordVerification]);
@@ -15,8 +15,10 @@ export default async function handleSignIn(username: string, password: string) {
             body: {
                 username,
                 password,
-            }
-        })
+                rememberMe: true,
+                callbackURL: "/dashboard",
+            },
+        });
         return data;
     } catch (e: any) {
         console.error(e);
