2c5bbe5d79ceaee26d7d9c95c074684f276ceb5e
Jossy Solá <jossysola@gmail.com>
2026-03-21 20:33:11 -0600

    Creates route

---
+++ app/(app)/update-password/actions.ts
@@ -0,0 +1,33 @@
+"use server"
+import { auth } from "@/auth";
+import { updatePasswordSchema } from "@/lib/schemas";
+import z from "zod";
+
+export async function updatePasswordAction(initialState: { message?: string, errors?: Array<string> }, formData: FormData) {
+    try {
+        const currentPassword = formData.get("current-password")?.toString() as string;
+        const newPassword = formData.get("new-password")?.toString() as string;
+        const verification = updatePasswordSchema.safeParse({
+            currentPassword,
+            newPassword,
+        });
+        if (!verification.success) return ({
+            errors: Object.values(z.flattenError(verification.error).fieldErrors)[0],
+        })
+        await auth.api.changePassword({
+            body: {
+                newPassword,
+                currentPassword,
+                revokeOtherSessions: true,
+            },
+        });
+        return {
+            message: "Password update successful"
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            message: `${error}`,
+        }
+    }
+}
---
+++ app/(app)/update-password/page.tsx
@@ -0,0 +1,10 @@
+import UpdatePasswordForm from "@/features/update-password/components/update-password-form";
+
+export default async function UpdatePasswordPage() {
+    return (
+        <section>
+            <h2>Update password</h2>
+            <UpdatePasswordForm />
+        </section>
+    )
+}
