1b38199b22252af95619b907881f880ad7300e43
Jossy Solá <jossysola@gmail.com>
2026-03-21 21:37:14 -0600

    Restructures tests into three separate files: actions, features, and utils

--- lib/tests/unit-testing/actions.server.test.ts
+++ lib/tests/unit-testing/actions.server.test.ts
@@ -1,10 +1,7 @@
 import { signInAction } from "@/app/(app)/(auth)/signin/actions";
 import { signUpAction } from "@/app/(app)/(auth)/signup/actions";
 import { auth } from "@/auth";
-import sendEmail from "@/features/auth/utils/sendEmail/sendEmail";
-import { JSX } from "react";
-import { Resend } from "resend";
-import { beforeAll, beforeEach, describe, expect, expectTypeOf, test, vi } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 
 vi.mock("@/auth", () => ({
     auth: {
@@ -36,28 +33,9 @@ vi.mock("@/auth", () => ({
         },     
     },
 }));
-vi.mock("resend", () => ({
-    Resend: vi.fn(class {
-        key: string;
-        constructor(key: string) {
-            this.key = key;
-        }
-        emails = {
-            send: vi.fn(({ from, to, subject, react }: {
-                from: string,
-                to: Array<string>,
-                subject: string,
-                react: JSX.Element
-            }) => ({
-                data: "Success",
-                error: "",
-            }))
-        }
-    })
-}))
-describe("(auth)",() => {
-    describe("signin/", () => {
-        describe("@app/**/signin -> signInAction", () => {
+describe("(app)",() => {
+    describe("(auth)", () => {
+        describe("signin", () => {
             test("returns successful message", async () => {
                 const initialState = { message: "" };                
                 const payload = {
@@ -83,9 +61,7 @@ describe("(auth)",() => {
                 expect(result).toEqual({ errors: ["Password must be minimum 8 characters long"] })
             });
         });
-    });
-    describe("signup/", () => {
-        describe("@app/**/signup -> signUpAction", () => {
+        describe("signup", () => {
             test("returns object with successful message", async () => {
                 const payload = {
                     email: "name@domain.com",
@@ -171,68 +147,6 @@ describe("(auth)",() => {
                 const result = await signUpAction({ message: "" }, formData);
                 expect(result).toEqual({ message: "Error: Error bubbling from auth.api.signUpEmail" });                
             });
-        });
-    });
-    describe("sendEmail", () => {
-        beforeAll(() => vi.stubEnv("RESEND_API_KEY", "123456789"));
-        beforeEach(() => vi.clearAllMocks());
-
-        test("is void after the function successfully completed", async () => {
-            const payload = {
-                to: "contact@jossysola.com",
-                subject: "Scheduler: Confirm your email!",
-                text: "Please confirm your email to complete the sign up process.",
-                url: "",
-                linkText: "",
-            }
-            const result = await sendEmail(payload);
-            expectTypeOf(result).toBeVoid();
-        });
-        test("the properties: from, to, subject and react are passed to Resend API", async () => {
-            const payload = {
-                to: "contact@jossysola.com",
-                subject: "Scheduler: Confirm your email!",
-                text: "Please confirm your email to complete the sign up process.",
-                url: "",
-                linkText: "",
-            }
-            await sendEmail(payload);
-            const resendInstance = vi.mocked(Resend).mock.results[0].value;
-            expect(resendInstance.emails.send).toHaveBeenCalledWith({
-                from: expect.any(String),
-                to: ["contact@jossysola.com"],
-                subject: "Scheduler: Confirm your email!",
-                react: expect.any(Object),
-            });
-        });
-        test("throws when any input uses an invalid format", async () => {
-            const payload = {
-                to: "contact",
-                subject: "Scheduler: Confirm your email!",
-                text: "Please confirm your email to complete the sign up process.",
-                url: "",
-                linkText: "",
-            }
-            await expect(sendEmail(payload)).rejects.toThrow("Error: ✖ Invalid email address");
-        });
-        test("throws when the Resend API fails", async () => {
-            vi.mocked(Resend).mockImplementationOnce(class {
-                key: string;
-                constructor(key: string) {
-                    this.key = key;
-                }
-                emails = {
-                    send: vi.fn().mockRejectedValue(new Error("Resend API error")),
-                }
-            } as any);
-            const payload = {
-                to: "contact@jossysola.com",
-                subject: "Scheduler: Confirm your email!",
-                text: "Please confirm your email to complete the sign up process.",
-                url: "",
-                linkText: "",
-            }
-            await expect(sendEmail(payload)).rejects.toThrow("Unsuccessful send mail action: Error: Resend API error");
         });
     });
 });
