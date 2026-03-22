734a76c88ce26389fdc3d0a18d65db91f1e128dd
Jossy Solá <jossysola@gmail.com>
2026-03-21 20:54:14 -0600

    Protects route with session

--- app/(app)/update-password/page.tsx
+++ app/(app)/update-password/page.tsx
@@ -1,6 +1,15 @@
+import { auth } from "@/auth";
 import UpdatePasswordForm from "@/features/update-password/components/update-password-form";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function UpdatePasswordPage() {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+    if (!session) {
+        redirect("/signin");
+    }    
     return (
         <section>
             <h2>Update password</h2>
