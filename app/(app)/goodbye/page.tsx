258f20dc0bbf57f90bac70e66d0c23b1dbc82919
Jossy Solá <jossysola@gmail.com>
2026-03-24 03:30:17 -0600

    Creates route

---
+++ app/(app)/goodbye/page.tsx
@@ -0,0 +1,8 @@
+export default async function GoodbyePage() {
+    return (
+        <section>
+            <h2>We are sorry to see you go 😢</h2>
+            <p>However, you are more than welcome to join again anytime and/or leave us with some feedback so we can improve.</p>
+        </section>
+    )
+}
