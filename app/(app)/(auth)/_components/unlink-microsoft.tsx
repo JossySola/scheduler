c82b369b3c72961bbad1b8e10ce0d6740b7016f9
Jossy Solá <jossysola@gmail.com>
2026-03-24 03:33:49 -0600

    Creates UI components

---
+++ app/(app)/(auth)/_components/unlink-microsoft.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { MicrosoftLogo } from "@/ui/icons/geist/icons";
+import { Button } from "react-aria-components";
+import { authClient } from "../_utils/auth-client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function UnlinkMicrosoft() {
+    const [message, setMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+    const { refetch } = authClient.useSession();
+    const handleUnlink = async () => {
+        setIsLoading(true);
+        try {
+            await authClient.unlinkAccount({ providerId: 'microsoft' });
+            await refetch();
+            router.refresh();
+            setMessage("Disconnected successfully");
+        } catch (error: any) {
+            setMessage(error.error.message || "Failed to disconnect")
+        } finally {
+            setIsLoading(false);
+        }
+    }
+    return ( 
+        <>
+        <Button 
+        type="button"
+        isDisabled={ isLoading } 
+        aria-label="Unlink Microsoft account"
+        className="provider-button"
+        onPress={ handleUnlink } >
+        <MicrosoftLogo /> { isLoading ? "Disconnecting..." : "Disconnect from Microsoft" } 
+        </Button> 
+        {
+            message && <p role="alert"> { message } </p>
+        } 
+        </>
+    );
+}
