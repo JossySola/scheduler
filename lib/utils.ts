73b899f77fd436b2d236d1b76955eb75340cf300
Jossy Solá <jossysola@gmail.com>
2026-04-14 19:09:43 -0600

    Creates util file

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+export function generateColumnName(index: number): string {
+  let name = "";
+  while (index >= 0) {
+    name = String.fromCharCode((index % 26) + 65) + name;
+    index = Math.floor(index / 26) - 1;
+  }
+  return name;
+}
