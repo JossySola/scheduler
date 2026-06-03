d5aad5bb9988c2bfa5f261c955ca0970b6ee9a2d
Jossy Solá <jossysola@gmail.com>
2026-06-03 11:49:28 -0600

    Removes Better-Auth imports/setup

--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -1,4 +1,6 @@
 
 export default async function Page() {
-    
+    return (
+        <></>
+    )
 }
--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,14 +1,4 @@
-import { auth } from "@/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-
 export default async function DashboardPage() {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    });
-    if (!session) {
-        redirect("/signin");
-    }
     return (
         <>
         <h1>Dashboard</h1>
