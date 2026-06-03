aee8cdc6ef365ea5b51437ba201403660aedab90
Jossy Solá <jossysola@gmail.com>
2026-06-03 11:47:03 -0600

    Adds parameters

--- src/app/(app)/(auth)/_utils/sign-up.ts
+++ src/app/(app)/(auth)/_utils/sign-up.ts
@@ -3,12 +3,12 @@ import { createClient } from "@/lib/supabase/client"
 const supabase = createClient();
 
 // ---cut---
-export default async function signUpNewUser() {
+export default async function signUpNewUser(email: string, password: string, emailRedirectTo: string) {
   const { data, error } = await supabase.auth.signUp({
-    email: 'valid.email@supabase.io',
-    password: 'example-password',
+    email,
+    password,
     options: {
-      emailRedirectTo: 'https://example.com/welcome',
+      emailRedirectTo,
     },
   });
   return { data, error };
