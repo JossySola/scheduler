21cc11b60455da65779e10b6ceb905dc7b2d9d9b
Jossy Solá <jossysola@gmail.com>
2026-06-04 13:34:47 -0600

    Adds redirectTo as argument

--- src/app/(app)/(auth)/_utils/reset-password.ts
+++ src/app/(app)/(auth)/_utils/reset-password.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@/lib/supabase/client"
 
 const supabase = createClient();
-export default async function resetPassword(email: string) {
-  const { data, error } = await supabase.auth.resetPasswordForEmail(email);
+export default async function resetPassword(email: string, redirectTo: string) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo,
+  });
   return { data, error };
 }
