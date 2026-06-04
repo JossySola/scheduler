f758a5e04260110e56d06ce9be6e6dc76ab77c13
Jossy Solá <jossysola@gmail.com>
2026-06-04 13:36:36 -0600

    Adds tailwindcss style

--- ui/form/form.tsx
+++ ui/form/form.tsx
@@ -2,5 +2,5 @@
 import { Form as AriaForm, FormProps } from "react-aria-components";
 
 export default function Form(props: FormProps) {
-    return <AriaForm {...props}>{ props.children }</AriaForm>
+    return <AriaForm className='flex flex-col gap-6' {...props}>{ props.children }</AriaForm>
 }
