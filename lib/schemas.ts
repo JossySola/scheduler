e766a88646ddc2753dde2c2c494084d061438888
Jossy Solá <jossysola@gmail.com>
2026-06-04 01:00:35 -0600

    Modifies signUp and signIn schemas

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,14 +1,12 @@
 import z from "zod";
 
 export const signUpSchema = z.object({
-    name: z.string({ error: "Invalid input format" }).nonempty({ error: "This field should not be empty" }),
-    username: z.string({ error: "Invalid input format" }).nonempty({ error: "This field should not be empty" }),
     email: z.email({ error: "Invalid email address" }).nonempty({ error: "This field should not be empty" }),
     password: z.string({ error: "Invalid input format" }).min(8, { error: "Password must be minimum 8 characters long" }),
     confirmPassword: z.string({ error: "Invalid input format" }).min(8, { error: "Password must be minimum 8 characters long" }),
 });
 export const signInSchema = z.object({
-    username: z.string({ error: "Invalid input format" }).nonempty({ error: "This field should not be empty" }),
+    email: z.email({ error: "Invalid email address" }).nonempty({ error: "This field should not be empty" }),
     password: z.string({ error: "Invalid input format" }).min(8, { error: "Password must be minimum 8 characters long" }),
 })
 export const sendEmailSchema = z.object({
