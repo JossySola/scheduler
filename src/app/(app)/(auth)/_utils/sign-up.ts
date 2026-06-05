fd4b70f5b9e34ceaaadcc703c3771d09e3b6a8f0
Jossy Solá <jossysola@gmail.com>
2026-06-05 10:37:51 -0600

    Implements checking if the password is leaked

--- src/app/(app)/(auth)/_utils/sign-up.ts
+++ src/app/(app)/(auth)/_utils/sign-up.ts
@@ -1,9 +1,29 @@
 import { createClient } from "@/lib/supabase/client"
+import isPasswordLeaked from "./is-password-leaked";
+import { AuthError } from "@supabase/supabase-js";
 
 const supabase = createClient();
 
 // ---cut---
 export default async function signUpNewUser(email: string, password: string, emailRedirectTo: string) {
+  const passwordLeaked = await isPasswordLeaked(password);
+  if (passwordLeaked) {
+    return {
+      data: {
+        user: null,
+        session: null,
+      },
+      error: new AuthError("The password is flagged as leaked"),
+    }
+  } else if (passwordLeaked === null) {
+    return {
+      data: {
+        user: null,
+        session: null,
+      },
+      error: new AuthError("An error occurred when checking data breaches"),
+    }
+  }
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
--- src/app/(app)/(auth)/_utils/update-password.ts
+++ src/app/(app)/(auth)/_utils/update-password.ts
@@ -1,7 +1,27 @@
 import { createClient } from "@/lib/supabase/client";
+import isPasswordLeaked from "./is-password-leaked";
+import { AuthError } from "@supabase/supabase-js";
 
 const supabase = createClient();
 export default async function updatePassword(password: string, current_password: string) {
+    const passwordLeaked = await isPasswordLeaked(password);
+    if (passwordLeaked) {
+    return {
+        data: {
+        user: null,
+        session: null,
+        },
+        error: new AuthError("The password is flagged as leaked"),
+    }
+    } else if (passwordLeaked === null) {
+    return {
+        data: {
+        user: null,
+        session: null,
+        },
+        error: new AuthError("An error occurred when checking data breaches"),
+    }
+    }
     const { data, error } = await supabase.auth.updateUser({
         password,
         current_password,
