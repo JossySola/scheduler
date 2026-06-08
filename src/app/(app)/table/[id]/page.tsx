eccbffeabe8d7779df5583bcf4c531fd998ad0d2
Jossy Solá <jossysola@gmail.com>
2026-06-07 21:36:39 -0600

    Adds authentication protection

--- src/app/(app)/table/[id]/page.tsx
+++ src/app/(app)/table/[id]/page.tsx
@@ -1,3 +1,20 @@
-export default async function TablePage() {
-    
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function ProtectedPage() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getClaims()
+  if (error || !data?.claims) {
+    redirect('/login')
+  }
+
+  return (
+    <div className="flex h-svh w-full items-center justify-center gap-2">
+      <p>
+        Table by <span>{data.claims.email}</span>
+      </p>
+      <section></section>
+    </div>
+  )
 }
