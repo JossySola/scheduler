a82abd36ebcc963eb8dfcc031a28dd6abf0514ee
Jossy Solá <jossysola@gmail.com>
2026-06-07 21:35:29 -0600

    Adds new subroute

---
+++ src/app/(app)/table/new/page.tsx
@@ -0,0 +1,28 @@
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase/server'
+import AiPromptOption from '../[id]/_components/ai-prompt-option'
+
+/*
+    Sorting, filtering, faceting, grouping,
+    aggregation, expansion, selection, sizing,
+    pinning, visibility, ordering and pagination
+*/
+export default async function ProtectedPage() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getClaims()
+  if (error || !data?.claims) {
+    redirect('/login')
+  }
+
+  return (
+    <div className="flex flex-col h-svh w-full items-center justify-center gap-2">
+      <p>
+        Table by <span>{data.claims.email}</span>
+      </p>
+      <section>
+        <AiPromptOption />
+      </section>
+    </div>
+  )
+}
