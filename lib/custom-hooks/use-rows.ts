4cd54b366f20a52caac1f694a3a8ac07c8d6e55a
Jossy Solá <jossysola@gmail.com>
2026-06-11 21:00:49 -0600

    Refactors custom hook

--- lib/custom-hooks/use-rows.ts
+++ lib/custom-hooks/use-rows.ts
@@ -1,51 +1,109 @@
 "use client"
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { generateColumnName } from "../utils";
-import { RowsState } from "../definitions";
+import { HeaderType, TableState } from "../definitions";
 
 export default function useRows(
-    initialState: RowsState = new Map()) {
-    const [rows, setRows] = useState<RowsState>(initialState);
-    // Check how many columns will be added in a new row
-    const maxCols = rows.values().find(value => value !== undefined)?.size ?? 1;
-    const columnNames = useMemo(() => {
-        return Array.from({length: maxCols}, (_, i) => generateColumnName(i));
-    }, [maxCols]);
+    initialState: TableState = []) {
+    const [rows, setRows] = useState<TableState>(initialState);
+    // Check how many columns will be added in a new row as an initial load
+    const maxCols = rows[0] ? rows[0].length : 1;
     
-    const addRow = (id: string) => {
-        const newMap = new Map(
-            columnNames.slice(0, maxCols).map(name => [name as string, ""])
-        );
+    const addRow = () => {
         setRows(prev => {
-            const previous = prev;
-            previous.set(id, newMap);
-            return previous;
+            const newArray = prev;
+            if (prev.length === 0) {
+                const row = new Array(maxCols);
+                row.fill({
+                    value: "",
+                    type: "text",
+                    isVisible: true,
+                });
+                newArray.push(row);
+                return newArray;
+            }
+            const row = new Array(maxCols);
+            row.fill({
+                value: "",
+                type: "text",
+                isVisible: true,
+            }, 0, 1);
+            row.fill("", 1, maxCols - 1);
+            newArray.push(row);
+            return newArray;
         });
     }
     const addCol = () => {
         setRows(prev => {
-            const previous = prev;
-            previous.forEach(row => row.set(generateColumnName(maxCols), ""));
-            return previous;
+            const newArray = prev.map((row, index) => {
+                if (index === 0) {
+                    const newRow = row;
+                    newRow.push({
+                        value: "",
+                        type: "text",
+                        isVisible: true,
+                    })
+                    return newRow;
+                }
+                const newRow = row;
+                newRow.push("");
+                return newRow;
+            });
+            return newArray;
         });
     }
-    const deleteRow = (id: string) => {
+    const deleteRow = (index?: number) => {
         setRows(prev => {
-            const previous = prev;
-            previous.delete(id);
-            return previous;
-        });
-    }
-    const editRow = (rowId: string, colLetter: string, value: string) => {
-        setRows(prev => {
-            const newRow = prev.get(rowId);
-            if (newRow) {
-                newRow.set(colLetter, value);
-                const newRows = prev;
-                newRows.set(rowId, newRow);
-                return newRows;
+            if (index) {
+                return prev.toSpliced(index, 1);
             }
-            return prev;
+            const newArray = prev;
+            newArray.pop();
+            return newArray;
+        });
+    }
+    const deleteColumn = (index?: number) => {
+        setRows(prev => {
+            if (index) {
+                return prev.map((row, _) => {
+                    return row.toSpliced(index, 1);
+                });
+            }
+            return prev.map((row, _) => {
+                row.pop();
+                return row;
+            });
+        });
+    }
+    const editCell = ({rowIndex, colIndex, value, type, isVisible}: {
+        rowIndex: number,
+        colIndex: number,
+        value?: string,
+        type?: "text" | "time" | "date",
+        isVisible?: boolean,
+    }) => {
+        setRows(prev => {
+            const newRows = prev;
+            if (value) {
+                const cell = newRows[rowIndex][colIndex];
+                if (typeof cell !== "string") {
+                    (newRows[rowIndex][colIndex] as HeaderType).value = value;
+                } else {
+                    newRows[rowIndex][colIndex] = value;
+                }
+            } else if (type) {
+                const cell = newRows[rowIndex][colIndex];
+                if (typeof cell !== "string") {
+                    (newRows[rowIndex][colIndex] as HeaderType).type = type;
+                }
+            } else if (isVisible !== undefined) {
+                const cell = newRows[rowIndex][colIndex];
+                if (typeof cell !== "string") {
+                    (newRows[rowIndex][colIndex] as HeaderType).isVisible = isVisible;
+                }
+            }
+
+            return newRows;
         });
     }
     return {
@@ -53,6 +111,7 @@ export default function useRows(
         addRow,
         addCol,
         deleteRow,
-        editRow,
+        deleteColumn,
+        editCell,
     }
 }
