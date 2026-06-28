4e0d868b8151fc505b5a8be6d816de1aff84d4d3
Jossy Solá <jossysola@gmail.com>
2026-06-28 00:23:06 -0600

    Completes component's functionality

--- src/app/(app)/table/_components/rows-values-tabs.tsx
+++ src/app/(app)/table/_components/rows-values-tabs.tsx
@@ -1,38 +1,40 @@
 'use client'
-import { TableState } from "@/lib/definitions";
-import { ListView, ListViewItem } from "@react-spectrum/s2/ListView";
-import { NumberField } from "@react-spectrum/s2/NumberField";
+import { HeaderType, TableState } from "@/lib/definitions";
+import { generateCellId } from "@/lib/utils";
 import { Tab, TabList, TabPanel, Tabs } from "@react-spectrum/s2/Tabs";
 import { useMemo } from "react";
+import RowCountSetting from "./row-count-setting";
 
-export default function RowsValuesTabs({ rows, values }: {
+export default function RowsValuesTabs({ rows, values, valuesInRows, editCountInRow }: {
     rows: TableState,
     values: Set<string>,
+    valuesInRows: Map<string, Map<string, number>>,
+    editCountInRow: (row: string, value: string, count: number) => void,
 }) {
     const rowsHeaders = useMemo(() => rows && rows.length > 0
         ? rows.map((row, index) => {
             if (index === 0) {
-                const column = row[0];
-                if (typeof column === 'string') {
-                    return column;
-                } else {
-                    return column.value;
-                }
-            } else {
                 return;
+            } else {
+                const column = row[0];
+                return [generateCellId(index, 0), column];
             }
-        })
+        }).filter(element => element !== undefined)
         : []
     , [rows]);
+    
     return (
-        <Tabs aria-label="Rows' values tabs">
+        <Tabs aria-label="Rows' values tabs" orientation="vertical">
             <TabList>
                 {
                     rowsHeaders
-                    ? rowsHeaders.map((header, index) => {
+                    ? rowsHeaders.map(([coordinate, header], index) => {
                         return (
-                            <Tab id={`${header}.${index}.tab`} key={`${header}.${index}.tab`}>
-                                { header }
+                            <Tab 
+                            id={coordinate as string} 
+                            key={`${coordinate as string}.${index}.tab`}
+                            aria-label={`Setting for the row ${coordinate as string}`}>
+                                { coordinate as string }
                             </Tab>
                         )
                     })
@@ -41,26 +43,29 @@ export default function RowsValuesTabs({ rows, values }: {
             </TabList>
             {
                 rowsHeaders
-                ? rowsHeaders.map((header, index) => {
+                ? rowsHeaders.map(([coordinate, header]) => {
                     return (
-                        <TabPanel id={`${header}.${index}.panel`} key={`${header}.${index}.panel`}>
-                            <ListView>
-                                {
-                                    values
-                                    ? Array.from(values).map((value, index) => {
-                                        return (
-                                            <ListViewItem id={value} key={`${value}.${index}`}>
-                                                { value }
-                                                <NumberField
-                                                label="The amount of times the value can be used in this row"
-                                                minValue={0}
-                                                maxValue={rowsHeaders.length ?? 0} />
-                                            </ListViewItem>
-                                        )
-                                    })
-                                    : null
-                                }
-                            </ListView>
+                        <TabPanel id={`${coordinate as string}`} key={`${coordinate as string}.panel`}>
+                            <h5 className="text-center text-2xl my-3">" { (header as HeaderType).value } "</h5>
+                            {
+                                values
+                                ? Array.from(values).map((value, index) => {
+                                    return (
+                                        <div
+                                        id={value}
+                                        key={`${value}.${index}`}
+                                        className="w-full flex flex-row justify-around py-2">
+                                            <RowCountSetting
+                                            value={value}
+                                            row={coordinate as string}
+                                            colsLength={rows && rows.length > 0 ? rows[0].length : 0}
+                                            valuesInRows={valuesInRows}
+                                            editCountInRow={editCountInRow} />
+                                        </div>
+                                    )
+                                })
+                                : null
+                            }
                         </TabPanel>
                     )
                 })
