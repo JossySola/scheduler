f5801efe749e2da4705a9a0e6dfd7ba5b0548383
Jossy Solá <jossysola@gmail.com>
2026-06-28 20:45:26 -0600

    Implements new components

--- src/app/(app)/table/_components/cell.tsx
+++ src/app/(app)/table/_components/cell.tsx
@@ -1,41 +1,34 @@
 'use client'
-import { HeaderType } from "@/lib/definitions";
-import { generateCellId } from "@/lib/utils";
-import { TextField } from "@react-spectrum/s2/TextField";
+import DateCell from "./date-cell";
+import TimeCell from "./time-cell";
+import TextCell from "./text-cell";
 
-export default function Cell({ value, rowIndex, colIndex, editCell }: {
-    value: string | HeaderType,
+export default function Cell({ value, rowHeadersType, colHeadersType, rowIndex, colIndex, editCell }: {
+    value: string,
+    rowHeadersType: string,
+    colHeadersType: string,
     rowIndex: number,
     colIndex: number,
-    editCell: ({ rowIndex, colIndex, value, type, isVisible}: {
+    editCell: ({ rowIndex, colIndex, value }: {
         rowIndex: number,
         colIndex: number,
-        value?: string | undefined,
-        type?: "text" | "time" | "date" | undefined,
-        isVisible?: boolean | undefined,
+        value: string,
     }) => void,
 }) {
-    const text = (
-        typeof value === 'string'
-        ? value
-        : value.value
-    );
-    const cellId = generateCellId(rowIndex, colIndex);
-    const handleChange = (event: string) => {
-        const newValue = event;
-        const payload = {
-            rowIndex,
-            colIndex,
-            value: newValue,
+    if (rowIndex === 0 && colIndex > 0) {
+        // Logic for column headers excluding the first column
+        if (colHeadersType === "date") {
+            return <DateCell value={value} rowIndex={rowIndex} colIndex={colIndex} editCell={editCell} />
+        } else if (colHeadersType === "time") {
+            return <TimeCell value={value} rowIndex={rowIndex} colIndex={colIndex} editCell={editCell} />
         }
-        editCell(payload);
+    } else if (rowIndex > 0 && colIndex === 0) {
+        // Logic for the row headers excluding the first row
+        if (rowHeadersType === "date") {
+            return <DateCell value={value} rowIndex={rowIndex} colIndex={colIndex} editCell={editCell} />
+        } else if (rowHeadersType === "time") {
+            return <TimeCell value={value} rowIndex={rowIndex} colIndex={colIndex} editCell={editCell} />
+        }
     }
-    return (
-        <TextField 
-        id={cellId}
-        aria-label={`Cell field for ${cellId}`}
-        value={text} 
-        onChange={handleChange}
-        autoComplete="off" />
-    )
+    return <TextCell value={value} rowIndex={rowIndex} colIndex={colIndex} editCell={editCell} />
 }
