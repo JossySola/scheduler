10aaa2e7e6a4097db13c8e879da163dd331afee3
Jossy Solá <jossysola@gmail.com>
2026-06-28 20:50:14 -0600

    Implements delete methods from useSoftConstraints. Implements HeaderTypesModal

--- src/app/(app)/table/_components/panel.tsx
+++ src/app/(app)/table/_components/panel.tsx
@@ -1,14 +1,17 @@
 'use client'
-import useHardConstraints from "@/lib/custom-hooks/use-hard-constraints";
+import useHardConstraints from "@/lib/custom-hooks/constraints/use-hard-constraints";
 import useRows from "@/lib/custom-hooks/use-rows";
-import useSoftConstraints from "@/lib/custom-hooks/use-soft-constraints";
-import useValues from "@/lib/custom-hooks/use-table-values";
+import useSoftConstraints from "@/lib/custom-hooks/constraints/use-soft-constraints";
+import useValues from "@/lib/custom-hooks/constraints/use-table-values";
 import { PanelInitialState } from "@/lib/definitions";
 import Table from "./table";
 import { Button } from "react-aria-components";
 import HardConstraintsModal from "./hard-constraints-modal";
 import SoftConstraintsModal from "./soft-constraints-modal";
 import ValuesModal from "./values-modal";
+import useHeaderType from "@/lib/custom-hooks/constraints/use-header-type";
+import useStructuralConstraints from "@/lib/custom-hooks/constraints/use-structural-constraints";
+import HeaderTypesModal from "./header-types-modal";
 
 export default function Panel({ initialState }: {
     initialState?: PanelInitialState,
@@ -48,10 +51,24 @@ export default function Panel({ initialState }: {
         deleteValueInColumn,
         deleteValueInRow,
     } = useSoftConstraints(initialState?.softConstraints);
+    const {
+        rowsType,
+        colsType,
+        changeColsType,
+        changeRowsType,
+    } = useHeaderType(initialState?.headersType);
+    const {
+        addColumnSpec,
+        addRowSpec,
+        deleteColumnSpec,
+        deleteRowSpec,
+        columnsSpecificity,
+        rowsSpecificity,
+    } = useStructuralConstraints(initialState?.structuralConstraints);
 
     return (
         <main id="panel">
-            <section id="settings">
+            <section id="constraints-settings" aria-label="Constraints Settings">
                 <HardConstraintsModal 
                 rows={rows} 
                 replaceColsList={replaceColsList} 
@@ -77,6 +94,12 @@ export default function Panel({ initialState }: {
                     deleteValueInRow,
                 }} />
 
+                <HeaderTypesModal
+                rowsType={rowsType}
+                colsType={colsType}
+                changeColsType={changeColsType}
+                changeRowsType={changeRowsType} />
+
                 <ValuesModal
                 addValue={addValue}
                 replaceValues={replaceValues}
@@ -87,9 +110,20 @@ export default function Panel({ initialState }: {
             <section id="table">
                 <Button type="button" onClick={() => addRow()}>Add Row</Button>
                 <Button type="button" onClick={() => addCol()}>Add Column</Button>
-                <Button type="button" onClick={() => deleteRow()}>Delete Row</Button>
-                <Button type="button" onClick={() => deleteColumn()}>Delete Column</Button>
-                <Table rows={rows} editCell={editCell} />
+                <Button type="button" onClick={() => {
+                    deleteRowInMap();
+                    deleteRow();
+                }}>Delete Row</Button>
+                <Button type="button" onClick={() => {
+                    deleteColumnInMap();
+                    deleteColumn();
+                }}>Delete Column</Button>
+
+                <Table 
+                rows={rows} 
+                rowHeadersType={rowsType}
+                colHeadersType={colsType}
+                editCell={editCell} />
             </section>
         </main>
     )
