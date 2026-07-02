57c5c5ed75e3d4e22c5ed199108e496543a9b2cf
Jossy Solá <jossysola@gmail.com>
2026-07-01 19:19:50 -0600

    Creates new two methods

--- lib/custom-hooks/constraints/use-structural-constraints.ts
+++ lib/custom-hooks/constraints/use-structural-constraints.ts
@@ -10,6 +10,22 @@ export default function useStructuralConstraints(initialState: {
     // <In Y row> fill X columns
     const [rowsSpecificity, setRowsSpecificity] = useState<Map<string, Set<string>>>(initialState.rowsSpecificity);
 
+    const setColumnSpec = useCallback((column: string, newArray: Array<string>) => {
+        const arrayToSet = new Set(newArray);
+        setColumnsSpecificity(prev => {
+            const newState = new Map(Array.from(prev));
+            newState.set(column, arrayToSet);
+            return newState;
+        });
+    }, []);
+    const setRowSpec = useCallback((row: string, newArray: Array<string>) => {
+        const arrayToSet = new Set(newArray);
+        setRowsSpecificity(prev => {
+            const newState = new Map(Array.from(prev));
+            newState.set(row, arrayToSet);
+            return newState;
+        });
+    }, []);
     const addColumnSpec = useCallback((column: string, newRow: string) => {
         if (columnsSpecificity.has(column)) {
             setColumnsSpecificity(prev => {
@@ -48,7 +64,7 @@ export default function useStructuralConstraints(initialState: {
             });
         }
     }, []);
-    const deleteColumnSpec = useCallback((column: string, rowToDelete: string) => {
+    const deleteRowInColumnSpec = useCallback((column: string, rowToDelete: string) => {
         setColumnsSpecificity(prev => {
             const newState = new Map(Array.from(prev));
             if (newState.has(column)) {
@@ -59,7 +75,7 @@ export default function useStructuralConstraints(initialState: {
             return newState;
         });
     }, []);
-    const deleteRowSpec = useCallback((row: string, colToDelete: string) => {
+    const deleteColumnInRowSpec = useCallback((row: string, colToDelete: string) => {
         setRowsSpecificity(prev => {
             const newState = new Map(Array.from(prev));
             if (newState.has(row)) {
@@ -70,12 +86,40 @@ export default function useStructuralConstraints(initialState: {
             return newState;
         });
     }, []);
+    const deleteRowFromStructuralMap = useCallback((row?: string) => {
+        setRowsSpecificity(prev => {
+            let newState = new Map(Array.from(prev));
+            if (row && newState.has(row)) {
+                newState.delete(row);
+            } else {
+                const mapToArray = Array.from(prev).toSpliced(-1, 1);
+                return new Map(mapToArray);
+            }
+            return newState;
+        });
+    }, []);
+    const deleteColumnFromStructuralMap = useCallback((column?: string) => {
+        setColumnsSpecificity(prev => {
+            const newState = new Map(Array.from(prev));
+            if (column && newState.has(column)) {
+                newState.delete(column);
+            } else {
+                const mapToArray = Array.from(prev).toSpliced(-1, 1);
+                return new Map(mapToArray);
+            }
+            return newState;
+        });
+    }, []);
 
     return {
+        setColumnSpec,
+        setRowSpec,
         addColumnSpec,
         addRowSpec,
-        deleteColumnSpec,
-        deleteRowSpec,
+        deleteRowInColumnSpec,
+        deleteColumnInRowSpec,
+        deleteRowFromStructuralMap,
+        deleteColumnFromStructuralMap,
         columnsSpecificity,
         rowsSpecificity,
     }
